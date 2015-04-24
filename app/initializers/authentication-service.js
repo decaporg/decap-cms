6f583192dd7e550eafea419498e0d7f23c878be5
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-04-24 15:44:58 -0700

    get rid of old authentication service

--- app/initializers/authentication-service.js
+++
@@ -1,8 +0,0 @@
-export function initialize(container, application) {
-  application.inject('route', 'authentication', 'service:authentication');
-}
-
-export default {
-  name: 'authentication-service',
-  initialize: initialize
-};
