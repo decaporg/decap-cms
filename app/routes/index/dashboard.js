5c0e76bf34dff1860defb33794ef9b9e5226013a
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-06-03 11:53:56 -0700

    Redirect index route to first collection

--- app/routes/index/dashboard.js
+++ app/routes/index/dashboard.js
@@ -4,10 +4,8 @@ export default AuthenticatedRoute.extend({
   model: function(params) {
     return this.get("config").collections[0];
   },
-  actions: {
-    activate: function(transition) {
-      console.log("Hello");
-      this.transitionTo("index.list", this.get("model"));
-    }
+  afterModel: function(model, transition) {
+    console.log("Hello %o", model);
+    this.transitionTo("index.list", model);
   }
 });
