7165b2c785a8d45eb67825af3e02d1aa466c9f56
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-06-09 16:08:38 -0700

    Smoother scrolling for preview pane

--- app/components/cms-preview-pane.js
+++ app/components/cms-preview-pane.js
@@ -3,18 +3,29 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['cms', 'cms-preview-pane'],
+  lastMutationEl: null,
 
   scrollTo: function(mutation) {
-    var target = mutation.target;
-    while ((target && target.parentNode) && !target.scrollIntoView) {
+    var target;
+    switch(mutation.type) {
+      case "childList":
+        target = mutation.addedNodes[0];
+        break;
+      default:
+        target = mutation.target;
+        break;
+    }
+    while ((target && target.parentNode) && !target.offsetTop) {
       target = target.parentNode;
     }
-    target && target.scrollIntoView({smooth: true});
+    if (target) {
+      this.$().animate({scrollTop: target.offsetTop - 100});
+    }
   },
 
   didInsertElement: function() {
     this.observer = new MutationObserver((mutations) => {
-      Ember.run.debounce(this, () => { this.scrollTo(mutations[0]); }, 200);
+      Ember.run.debounce(this, () => { this.scrollTo(mutations[0]); }, 50);
     });
     var config = { attributes: true, childList: true, characterData: true, subtree: true};
     this.observer.observe(this.$()[0], config);
