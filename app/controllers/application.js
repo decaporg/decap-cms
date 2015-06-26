2906270871390b5fa71b4f308c7513a300bafe4d
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-06-25 21:37:37 -0700

    Login via modal on 401

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  init: function() {
+    this._super.apply(this, arguments);
+    Ember.$(document).ajaxError((event, jqXHR, ajaxSettings, thrownError) => {
+      // You should include additional conditions to the if statement so that this
+      // only triggers when you're absolutely certain it should
+      if (jqXHR.status === 401) {
+        this.set("showLoginModal", true);
+      }
+    });
+  },
+  showLoginModal: false,
+  authorizationError: null,
+  authenticator: function() {
+    return "cms-authenticators/" + (this.get("config.backend.authenticator") || this.get("config.backend.name"));
+  }.property("config.authenticator.name"),
   templateName: "application",
   breadcrumbs: [{
     label: "Content",
@@ -17,6 +32,19 @@ export default Ember.Controller.extend({
     newEntry: function(collection) {
       this.set("showCollections", false);
       this.transitionToRoute("create", collection);
+    },
+    login: function(data) {
+      this.get("repository").authorize(data).then(
+        () => {
+          this.get("authstore").store(data);
+          if (this.get("showLoginModal")) {
+            this.set("showLoginModal", false);
+          } else {
+            this.transitionToRoute("index");
+          }
+        },
+        (err) => { this.set("controller.authorizationError", err); }
+      );
     }
   }
 });
