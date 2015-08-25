7c1bd308dfa13675df1fc81462c5b8bc3590e31f
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-08-24 22:08:48 -0700

    Support for environments in config.yml, closes #17

--- app/initializers/cms-config.js
+++ app/initializers/cms-config.js
@@ -1,6 +1,7 @@
 import Ember from "ember";
 import Config from "../models/config";
  /* global jsyaml */
+ /* global CMS_ENV */
 
 function loadYamlConfig() {
   if (Ember.$("#cms-yaml-config").length) {
@@ -12,8 +13,18 @@ function loadYamlConfig() {
 
 export function initialize(container, application) {
   application.deferReadiness();
-  loadYamlConfig().then(function(data) {
-    var config = Config.create(jsyaml.safeLoad(data));
+  loadYamlConfig().then(function(yaml) {
+    var data = jsyaml.safeLoad(yaml);
+
+    if (typeof CMS_ENV === "string" && data[CMS_ENV]) {
+      for (var key in data[CMS_ENV]) {
+        if (data[CMS_ENV].hasOwnProperty(key)) {
+          data[key] = data[CMS_ENV][key];
+        }
+      }
+    }
+
+    var config = Config.create(data);
     application.register('cms:config', config, { instantiate: false });
     application.inject('route', 'config', 'cms:config');
     application.inject('controller', 'config', 'cms:config');
