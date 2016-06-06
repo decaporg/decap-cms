139b86c26810ac84d0c5831b9560f1f49514557b
Cássio Zen <cassiozen@gmail.com>
2016-06-06 10:41:55 -0300

    render props.value straight

--- src/components/Widgets/ImagePreview.js
+++ src/components/Widgets/ImagePreview.js
@@ -3,16 +3,10 @@ import React from 'react';
 export default class ImagePreview extends React.Component {
   constructor(props) {
     super(props);
-    this.handleImageLoaded = this.handleImageLoaded.bind(this);
-  }
-
-  handleImageLoaded() {
-    window.URL.revokeObjectURL(this.props.value);
   }
 
   render() {
-    console.log(this.props)
     const { value } = this.props;
-    return value ? <img src={window.URL.createObjectURL(value)} onLoad={this.handleImageLoaded} /> : null;
+    return value ? <img src={value} /> : null;
   }
 }
