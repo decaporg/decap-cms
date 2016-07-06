45de85d693e0e20f8e3c722ee55b524a51d5b702
Cássio Zen <cassiozen@gmail.com>
2016-07-06 19:07:42 -0300

    Named export for unextended FindBar component

--- src/containers/stories/FindBar.js
+++ src/containers/stories/FindBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 
-import FindBar from '../FindBar';
+import { FindBar } from '../FindBar';
 
 const commands = [
   { pattern: 'Create new Collection(:collectionName)' },
