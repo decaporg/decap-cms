c7033a614e53e14cc01749572bc231e14fd90b42
Cássio Zen <cassiozen@gmail.com>
2016-07-08 07:17:53 -0300

    Command ids

--- src/containers/stories/FindBar.js
+++ src/containers/stories/FindBar.js
@@ -4,13 +4,13 @@ import { storiesOf, action } from '@kadira/storybook';
 import { FindBar } from '../FindBar';
 
 const commands = [
-  { pattern: 'Create new Collection(:collectionName)' },
-  { pattern: 'Create new Post(:postName)' },
-  { pattern: 'Create new Article(:articleName)' },
-  { pattern: 'Create new FAQ item(:faqName as FAQ item name)' },
-  { pattern: 'Add news item(:headline)' },
-  { pattern: 'Add new User(:userName as User name)' },
-  { pattern: 'Go to Settings' },
+  { id:'CREATE_COLLECTION', pattern: 'Create new Collection(:collectionName)' },
+  { id:'CREATE_POST', pattern: 'Create new Post(:postName)' },
+  { id:'CREATE_ARTICLE', pattern: 'Create new Article(:articleName)' },
+  { id:'CREATE_FAQ', pattern: 'Create new FAQ item(:faqName as FAQ item name)' },
+  { id:'ADD_NEWS', pattern: 'Add news item(:headline)' },
+  { id:'ADD_USER', pattern: 'Add new User(:userName as User name)' },
+  { id:'OPEN_SETTINGS', pattern: 'Go to Settings' },
 ];
 
 const style = {
