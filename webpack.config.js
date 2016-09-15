b2d4714c903b403eb4c5cbe0d0d218769408188f
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-09-15 18:53:24 +0200

    Integrated material-ui

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,14 @@ module.exports = {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url-loader?limit=100000'
       },
-      { test: /\.json$/, loader: 'json-loader' },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css?modules!sass',
+      },
       {
         test: /\.css$/,
         loader: 'style!css?modules&importLoaders=1&&localIdentName=cms__[name]__[local]!postcss',
