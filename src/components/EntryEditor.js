d09e9b40e4fbe08b7dfc838953966573b138fcb9
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-09-28 12:24:17 +0200

    Updated styles for entry page layout.
    This removes the need for resize handlers and height calc anymore.
    Re-written the component in a functional way.

--- src/components/EntryEditor.js
+++ src/components/EntryEditor.js
@@ -4,57 +4,35 @@ import ControlPane from './ControlPane';
 import PreviewPane from './PreviewPane';
 import styles from './EntryEditor.css';
 
-export default class EntryEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleResize = this.handleResize.bind(this);
-  }
+export default function EntryEditor(props) {
+  const { collection, entry, getMedia, onChange, onAddMedia, onRemoveMedia, onPersist } = props;
 
-  componentDidMount() {
-    this.calculateHeight();
-    window.addEventListener('resize', this.handleResize, false);
-  }
-
-  componengWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
-  }
-
-  handleResize() {
-    this.calculateHeight();
-  }
-
-  calculateHeight() {
-    const height = window.innerHeight - 54;
-    console.log('setting height to %s', height);
-    this.setState({ height });
-  }
-
-  render() {
-    const { collection, entry, getMedia, onChange, onAddMedia, onRemoveMedia, onPersist } = this.props;
-    const { height } = this.state;
-
-    return <div className={styles.entryEditor} style={{ height }}>
+  return (
+    <div className={styles.root}>
       <div className={styles.container}>
         <div className={styles.controlPane}>
           <ControlPane
-              collection={collection}
-              entry={entry}
-              getMedia={getMedia}
-              onChange={onChange}
-              onAddMedia={onAddMedia}
-              onRemoveMedia={onRemoveMedia}
+            collection={collection}
+            entry={entry}
+            getMedia={getMedia}
+            onChange={onChange}
+            onAddMedia={onAddMedia}
+            onRemoveMedia={onRemoveMedia}
           />
         </div>
         <div className={styles.previewPane}>
-          <PreviewPane collection={collection} entry={entry} getMedia={getMedia} />
+          <PreviewPane
+            collection={collection}
+            entry={entry}
+            getMedia={getMedia}
+          />
         </div>
       </div>
       <div className={styles.footer}>
         <button onClick={onPersist}>Save</button>
       </div>
-    </div>;
-  }
+    </div>
+  );
 }
 
 EntryEditor.propTypes = {
