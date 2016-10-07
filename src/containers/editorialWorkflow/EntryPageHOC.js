7016cc8e38ce19a3c6ff50c7b275320a75599436
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-10-07 10:44:04 +0200

    Converted to functional components + fixed other ESLint errors

--- src/containers/editorialWorkflow/EntryPageHOC.js
+++ src/containers/editorialWorkflow/EntryPageHOC.js
@@ -1,27 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { EDITORIAL_WORKFLOW } from '../../constants/publishModes';
 import { selectUnpublishedEntry } from '../../reducers';
 import { loadUnpublishedEntry, persistUnpublishedEntry } from '../../actions/editorialWorkflow';
-import { connect } from 'react-redux';
 
 export default function EntryPageHOC(EntryPage) {
-  class EntryPageHOC extends React.Component {
-    render() {
-      return <EntryPage {...this.props} />;
-    }
+  function WrappedComponent(props) {
+    return <EntryPage {...props} />;
   }
 
   function mapStateToProps(state, ownProps) {
-    const publish_mode = state.config.get('publish_mode');
-    const isEditorialWorkflow = (publish_mode === EDITORIAL_WORKFLOW);
+    const publishMode = state.config.get('publish_mode');
+    const isEditorialWorkflow = (publishMode === EDITORIAL_WORKFLOW);
     const unpublishedEntry = ownProps.route && ownProps.route.unpublishedEntry === true;
 
     const returnObj = {};
     if (isEditorialWorkflow && unpublishedEntry) {
       const status = ownProps.params.status;
       const slug = ownProps.params.slug;
-      const entry = selectUnpublishedEntry(state, status, slug);
-      returnObj.entry = entry;
+      returnObj.entry = selectUnpublishedEntry(state, status, slug);
     }
     return returnObj;
   }
@@ -43,5 +40,5 @@ export default function EntryPageHOC(EntryPage) {
     return returnObj;
   }
 
-  return connect(mapStateToProps, mapDispatchToProps)(EntryPageHOC);
+  return connect(mapStateToProps, mapDispatchToProps)(WrappedComponent);
 }
