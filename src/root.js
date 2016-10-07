4ebe0248ddaa0c8f5a1bd1cecaa5a04d6f28a4ff
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-10-07 11:43:19 +0200

    Added missing history dependency. Refactored syncHistory to be a pure function.

--- src/root.js
+++ src/root.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import routes from './routing/routes';
-import history, { syncHistory } from './routing/history';
+import history, { getSyncedHistoryInstance } from './routing/history';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
 
 // Create an enhanced history that syncs navigation events with the store
-syncHistory(store);
+const syncedHistory = getSyncedHistoryInstance(history, store);
 
 const Root = () => (
   <Provider store={store}>
-    <Router history={history}>
+    <Router history={syncedHistory}>
       {routes}
     </Router>
   </Provider>
