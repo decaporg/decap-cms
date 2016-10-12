fee2d8e7316ca463c902a465deabba2a045bbb9b
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-10-12 14:32:53 +0200

    Updated Jest config to work with webpack and CSS-modules

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,24 +1,40 @@
+/* eslint global-require: 0 */
+/* eslint import/no-extraneous-dependencies: 0 */
+
 process.env.BABEL_ENV = 'test';
 
 module.exports = wallaby => ({
   files: [
-    { pattern: 'src/**/*.js' },
-    { pattern: 'src/**/*.js.snap' },
-    { pattern: 'src/**/*.spec.js', ignore: true }
+    'package.json',
+    'src/**/*.js',
+    'src/**/*.js.snap',
+    '!src/**/*.spec.js',
   ],
 
-  tests: [
-    { pattern: 'src/**/*.spec.js' }
-  ],
+  tests: ['src/**/*.spec.js'],
 
   compilers: {
-    'src/**/*.js': wallaby.compilers.babel()
+    'src/**/*.js': wallaby.compilers.babel(),
   },
 
   env: {
     type: 'node',
-    runner: 'node'
+    runner: 'node',
+    params: {
+      runner: '--harmony_proxies',
+    },
   },
 
-  testFramework: 'jest'
+  testFramework: 'jest',
+
+  setup: () => {
+    wallaby.testFramework.configure({
+      moduleNameMapper: {
+        '^.+\\.(png|eot|woff|woff2|ttf|svg|gif)$': require('path').join(wallaby.localProjectDir, '__mocks__', 'fileLoaderMock.js'),
+        '^.+\\.scss$': require('path').join(wallaby.localProjectDir, '__mocks__', 'styleLoaderMock.js'),
+        '^.+\\.css$': require('identity-obj-proxy'),
+      },
+    });
+  },
+
 });
