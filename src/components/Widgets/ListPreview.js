6aacd0f0a57efb7d271bd20d91cb05e828d0670a
Cássio Zen <cassiozen@gmail.com>
2016-10-21 20:35:42 -0200

    check if value exists before trying to iterate over

--- src/components/Widgets/ListPreview.js
+++ src/components/Widgets/ListPreview.js
@@ -1,9 +1,9 @@
 import React, { PropTypes } from 'react';
 
 export default function ListPreview({ value }) {
-  return <ul>
-    { value.map(item => <li key={item}>{item}</li>) }
-  </ul>;
+  return (<ul>
+    { value && value.map(item => <li key={item}>{item}</li>) }
+  </ul>);
 }
 
 ListPreview.propTypes = {
