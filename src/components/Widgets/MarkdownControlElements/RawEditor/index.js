401cceb0b48e616036052b710d5a2af4365ce6e7
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2016-10-25 18:35:37 +0200

    Handle rich paste events

--- src/components/Widgets/MarkdownControlElements/RawEditor/index.js
+++ src/components/Widgets/MarkdownControlElements/RawEditor/index.js
@@ -1,5 +1,8 @@
 import React, { PropTypes } from 'react';
 import { fromJS } from 'immutable';
+import MarkupIt from 'markup-it';
+import markdownSyntax from 'markup-it/syntaxes/markdown';
+import htmlSyntax from 'markup-it/syntaxes/html';
 import CaretPosition from 'textarea-caret-position';
 import registry from '../../../../lib/registry';
 import MediaProxy from '../../../../valueObjects/MediaProxy';
@@ -8,6 +11,9 @@ import BlockMenu from './BlockMenu';
 import styles from './index.css';
 
 const HAS_LINE_BREAK = /\n/m;
+
+const markdown = new MarkupIt(markdownSyntax);
+const html = new MarkupIt(htmlSyntax);
 
 function processUrl(url) {
   if (url.match(/^(https?:\/\/|mailto:|\/)/)) {
@@ -21,6 +27,42 @@ function processUrl(url) {
 
 function preventDefault(e) {
   e.preventDefault();
+}
+
+function cleanupPaste(paste) {
+  const content = html.toContent(paste);
+  return markdown.toText(content);
+}
+
+function getCleanPaste(e) {
+  const transfer = e.clipboardData;
+  return new Promise((resolve) => {
+    const isHTML = !!Array.from(transfer.types).find(type => type === 'text/html');
+
+    if (isHTML) {
+      const data = transfer.getData('text/html');
+      // Avoid trying to clean up full HTML documents with head/body/etc
+      if (!data.match(/^\s*<!doctype/i)) {
+        e.preventDefault();
+        resolve(cleanupPaste(data));
+      } else {
+        // Handle complex pastes by stealing focus with a contenteditable div
+        const div = document.createElement('div');
+        div.contentEditable = true;
+        div.setAttribute('style', 'opacity: 0; overflow: hidden; width: 1px; height: 1px; position: fixed; top: 50%; left: 0;');
+        document.body.appendChild(div);
+        div.focus();
+        setTimeout(() => {
+          resolve(cleanupPaste(div.innerHTML));
+          document.body.removeChild(div);
+        }, 50);
+        return null;
+      }
+    }
+
+    e.preventDefault();
+    return resolve(transfer.getData(transfer.types[0]));
+  });
 }
 
 const buildtInPlugins = fromJS([{
@@ -64,6 +106,7 @@ export default class RawEditor extends React.Component {
     this.element.addEventListener('dragenter', preventDefault, false);
     this.element.addEventListener('dragover', preventDefault, false);
     this.element.addEventListener('drop', this.handleDrop, false);
+    this.element.addEventListener('paste', this.handlePaste, false);
   }
 
   componentDidUpdate() {
@@ -256,6 +299,20 @@ export default class RawEditor extends React.Component {
     }
     this.replaceSelection(data);
   };
+
+  handlePaste = (e) => {
+    const { value, onChange } = this.props;
+    const selection = this.getSelection();
+    const beforeSelection = value.substr(0, selection.start);
+    const afterSelection = value.substr(selection.end);
+
+    getCleanPaste(e).then((paste) => {
+      const newSelection = Object.assign({}, selection);
+      newSelection.start = newSelection.end = beforeSelection.length + paste.length;
+      this.newSelection = newSelection;
+      onChange(beforeSelection + paste + afterSelection);
+    });
+  }
 
   render() {
     const { onAddMedia, onRemoveMedia, getMedia } = this.props;
