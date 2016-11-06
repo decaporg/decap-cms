f6439b9a7b191980a5d5ab061a640b06cb004983
Cássio Zen <cassiozen@gmail.com>
2016-11-06 20:38:28 -0200

    fixed size cards

--- src/components/EntryListing.js
+++ src/components/EntryListing.js
@@ -1,7 +1,5 @@
 import React, { PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import { throttle } from 'lodash';
-import bricks from 'bricks.js';
 import Waypoint from 'react-waypoint';
 import history from '../routing/history';
 import { selectFields, selectInferedField } from '../reducers/collections';
@@ -17,65 +15,8 @@ export default class EntryListing extends React.Component {
     page: PropTypes.number,
   };
 
-  constructor(props) {
-    super(props);
-    this.bricksInstance = null;
-
-    this.bricksConfig = {
-      packed: 'data-packed',
-      sizes: [
-        { columns: 1, gutter: 15 },
-        { mq: '495px', columns: 2, gutter: 15 },
-        { mq: '750px', columns: 3, gutter: 15 },
-        { mq: '1005px', columns: 4, gutter: 15 },
-        { mq: '1515px', columns: 5, gutter: 15 },
-        { mq: '1770px', columns: 6, gutter: 15 },
-      ],
-    };
-
-    this.updateBricks = throttle(this.updateBricks.bind(this), 30);
-    this.handleLoadMore = this.handleLoadMore.bind(this);
-  }
-
-  componentDidMount() {
-    this.bricksInstance = bricks({
-      container: this.containerNode,
-      packed: this.bricksConfig.packed,
-      sizes: this.bricksConfig.sizes,
-    });
-
-    this.bricksInstance.resize(true);
-
-    if (this.props.entries && this.props.entries.size > 0) {
-      setTimeout(() => {
-        this.bricksInstance.pack();
-      }, 0);
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if ((prevProps.entries === undefined || prevProps.entries.size === 0)
-    && this.props.entries.size === 0) {
-      return;
-    }
-
-    this.bricksInstance.pack();
-  }
-
-  componengWillUnmount() {
-    this.bricksInstance.resize(false);
-  }
-
-  updateBricks() {
-    this.bricksInstance.pack();
-  }
-
-  handleLoadMore() {
+  handleLoadMore = () => {
     this.props.onPaginate(this.props.page + 1);
-  }
-
-  handleRef = (node) => {
-    this.containerNode = node;
   };
 
   renderCards = () => {
@@ -93,7 +34,7 @@ export default class EntryListing extends React.Component {
       const path = `/collections/${ collectionName }/entries/${ entry.get('slug') }`;
       const image = entry.getIn(['data', imageField]);
       return (
-        <Card key={entry.get('slug')} onClick={history.push.bind(this, path)}>
+        <Card key={entry.get('slug')} onClick={history.push.bind(this, path)} className={styles.card}>
           { image &&
           <header className={styles.cardImage} style={{ backgroundImage: `url(${ image })` }} />
           }
@@ -116,7 +57,7 @@ export default class EntryListing extends React.Component {
     return (
       <div>
         <h1>{children}</h1>
-        <div ref={this.handleRef}>
+        <div className={styles.cardsGrid}>
           { this.renderCards() }
           <Waypoint onEnter={this.handleLoadMore} />
         </div>
