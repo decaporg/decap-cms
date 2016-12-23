83f08e8eb8a00717757b5c4c69cf0b58e5b168d6
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2016-12-23 08:59:51 -0800

    Revert "Bump patch version"
    This reverts commit 8f6f1a0fcabfeb423ca8af2f9f6d55ace21e45bb.

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -5,7 +5,6 @@ import DashboardPage from '../containers/DashboardPage';
 import CollectionPage from '../containers/CollectionPage';
 import EntryPage from '../containers/EntryPage';
 import SearchPage from '../containers/SearchPage';
-import ConfigurationPage from '../containers/ConfigurationPage';
 import NotFoundPage from '../containers/NotFoundPage';
 
 export default (
@@ -32,10 +31,6 @@ export default (
     <Route
       path="/search/:searchTerm"
       component={SearchPage}
-    />
-    <Route
-      path="/configuration"
-      component={ConfigurationPage}
     />
     <Route
       path="*"
