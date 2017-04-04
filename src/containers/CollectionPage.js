2a8162a99d61c3fcb6749c58ffe28bda6a15e717
Benaiah Mischenko <benaiah@mischenko.com>
2017-04-04 14:41:01 -0700

    Stop showing loader forever when there are no entries

--- src/containers/CollectionPage.js
+++ src/containers/CollectionPage.js
@@ -5,6 +5,7 @@ import { loadEntries } from '../actions/entries';
 import { selectEntries } from '../reducers';
 import { Loader } from '../components/UI';
 import EntryListing from '../components/EntryListing/EntryListing';
+import styles from "./CollectionPage.css";
 
 class CollectionPage extends React.Component {
 
@@ -15,6 +16,7 @@ class CollectionPage extends React.Component {
     dispatch: PropTypes.func.isRequired,
     page: PropTypes.number,
     entries: ImmutablePropTypes.list,
+    isFetching: PropTypes.bool.isRequired,
   };
 
   componentDidMount() {
@@ -37,25 +39,28 @@ class CollectionPage extends React.Component {
   };
 
   render() {
-    const { collections, collection, publicFolder, page, entries } = this.props;
+    const { collections, collection, publicFolder, page, entries, isFetching } = this.props;
     if (collections == null) {
       return <h1>No collections defined in your config.yml</h1>;
     }
-    return (<div>
-      {entries ?
-        <EntryListing
-          collections={collection}
-          entries={entries}
-          publicFolder={publicFolder}
-          page={page}
-          onPaginate={this.handleLoadMore}
-        >
-          {collection.get('label')}
-        </EntryListing>
-        :
-        <Loader active>{['Loading Entries', 'Caching Entries', 'This might take several minutes']}</Loader>
-      }
-    </div>);
+
+    const entriesContent = (<EntryListing
+      collections={collection}
+      entries={entries}
+      publicFolder={publicFolder}
+      page={page}
+      onPaginate={this.handleLoadMore}
+    >
+      {collection.get('label')}
+    </EntryListing>);
+
+    const fetchingEntriesContent = (<Loader active>
+        {['Loading Entries', 'Caching Entries', 'This might take several minutes']}
+    </Loader>);
+    const noEntriesContent = <div className={styles.noEntries}>No Entries</div>;
+    const fallbackContent = isFetching ? fetchingEntriesContent : noEntriesContent;
+
+    return (<div>{entries ? entriesContent : fallbackContent}</div>);
   }
 }
 
@@ -68,8 +73,9 @@ function mapStateToProps(state, ownProps) {
   const page = state.entries.getIn(['pages', collection.get('name'), 'page']);
 
   const entries = selectEntries(state, collection.get('name'));
+  const isFetching = state.entries.getIn(['pages', collection.get('name'), 'isFetching'], false);
 
-  return { slug, publicFolder, collection, collections, page, entries };
+  return { slug, publicFolder, collection, collections, page, entries, isFetching };
 }
 
 export default connect(mapStateToProps)(CollectionPage);
