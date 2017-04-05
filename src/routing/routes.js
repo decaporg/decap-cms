dc03410870da0f44f9a2f6a27b1389315073341f
Bobby <steinbach.rj@gmail.com>
2017-04-04 23:20:04 -0400

    added support for editing of files in nested folders

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -20,7 +20,7 @@ export default (
       newRecord
     />
     <Route
-      path="/collections/:name/entries/:slug"
+      path="/collections/:name/entries/:slug*"
       component={EntryPage}
     />
     <Route
