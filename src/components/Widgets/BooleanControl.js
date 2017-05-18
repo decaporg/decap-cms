d6f2abcc361164d878e452e072422201855a4acd
Shawn Erquhart <shawn@shawn.works>
2017-05-18 10:03:47 -0400

    simplify boolean widget

--- src/components/Widgets/BooleanControl.js
+++ src/components/Widgets/BooleanControl.js
@@ -3,19 +3,15 @@ import ImmutablePropTypes from "react-immutable-proptypes";
 import Switch from 'react-toolbox/lib/switch';
 
 export default class BooleanControl extends React.Component {
-
-  handleChange = (e) => {
-    this.props.onChange(e);
-  };
-
   render() {
-    const finalValue = (this.props.value !== undefined) ? this.props.value : this.props.field.get('defaultValue', false);
-
-    return (<Switch
-      id={this.props.forID}
-      checked={finalValue}
-      onChange={this.handleChange}
-    />);
+    const { value, field, forId, onChange } = this.props;
+    return (
+      <Switch
+        id={forId}
+        checked={value === undefined ? field.get('defaultValue', false) : value}
+        onChange={onChange}
+      />
+    );
   }
 }
 
