257465782258cc4be4f948995ad47e59c899c3d6
Dominic Talbot <dominic@wearedamage.com>
2017-07-24 17:03:43 +0100

    save the content, need to save the selected block too

--- src/components/Widgets/BlockControl.js
+++ src/components/Widgets/BlockControl.js
@@ -1,11 +1,26 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import registry from '../../lib/registry';
 import { resolveWidget } from '../Widgets';
 import { Map } from 'immutable';
+import controlStyles from '../ControlPanel/ControlPane.css';
 
-export default class BlockControl extends React.Component {
+export default class BlockControl extends Component {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    onAddAsset: PropTypes.func.isRequired,
+    onRemoveAsset: PropTypes.func.isRequired,
+    getAsset: PropTypes.func.isRequired,
+    value: PropTypes.oneOfType([
+      PropTypes.node,
+      PropTypes.object,
+      PropTypes.bool,
+    ]),
+    field: PropTypes.object,
+    forID: PropTypes.string,
+    className: PropTypes.string,
+  };
 
   constructor(props) {
     super(props);
@@ -15,7 +30,7 @@ export default class BlockControl extends React.Component {
   }
 
   handleChange = (e) => {
-    this.props.onChange(e.target.value);
+    // this.props.onChange(e.target.value);
     if (!e.target.value) {
       this.setState({
         widget: null,
@@ -28,7 +43,8 @@ export default class BlockControl extends React.Component {
   };
 
   render() {
-    const { field, value, forID } = this.props;
+    const { field, value, forID, onChange, onAddAsset, onRemoveAsset, getAsset } = this.props;
+    // const { onAddAsset, onRemoveAsset, getAsset, value, onChange } = this.props;
     const { widget } = this.state;
     const fieldValue = value && Map.isMap(value) ? value.get(field.get('name')) : value;
 
@@ -57,18 +73,25 @@ export default class BlockControl extends React.Component {
         <div>
           {
             widget ?
-              React.createElement(widget.control, {
-                id: field.get('name'),
-                field,
-                value: fieldValue,
-                onChange: (val, metadata) => {
-                  onChange((value || Map()).set(field.get('name'), val), metadata);
-                },
-                // onAddAsset,
-                // onRemoveAsset,
-                // getAsset,
-                // forID: field.get('name'),
-              })
+              <div className={controlStyles.widget} key={field.get('name')}>
+                <div className={controlStyles.control} key={field.get('name')}>
+                  <label className={controlStyles.label} htmlFor={field.get('name')}>{field.get('label')}</label>
+                  {
+                    React.createElement(widget.control, {
+                      id: field.get('name'),
+                      field,
+                      value: fieldValue,
+                      onChange: (val, metadata) => {
+                        onChange((value || Map()).set(field.get('name'), val), metadata);
+                      },
+                      onAddAsset,
+                      onRemoveAsset,
+                      getAsset,
+                      forID: field.get('name'),
+                    })
+                  }
+                </div>
+              </div>
             :
               ''
           }
@@ -77,18 +100,3 @@ export default class BlockControl extends React.Component {
     );
   }
 }
-
-BlockControl.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  onAddAsset: PropTypes.func.isRequired,
-  onRemoveAsset: PropTypes.func.isRequired,
-  getAsset: PropTypes.func.isRequired,
-  value: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.object,
-    PropTypes.bool,
-  ]),
-  field: PropTypes.object,
-  forID: PropTypes.string,
-  className: PropTypes.string,
-};
