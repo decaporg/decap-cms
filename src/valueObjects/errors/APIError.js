e2623dd5db7fc63e18e3ea246945225ca78cfd3c
Benaiah Mischenko <benaiah@mischenko.com>
2017-07-27 22:04:13 -0700

    Add an optional `meta` field to `APIError` in `src/valueObjects/errors/APIError.js`

--- src/valueObjects/errors/APIError.js
+++ src/valueObjects/errors/APIError.js
@@ -1,11 +1,12 @@
 export const API_ERROR = 'API_ERROR';
 
 export default class APIError extends Error {
-  constructor(message, status, api) {
+  constructor(message, status, api, meta={}) {
     super(message);
     this.message = message;
     this.status = status;
     this.api = api;
     this.name = API_ERROR;
+    this.meta = meta;
   }
 }
