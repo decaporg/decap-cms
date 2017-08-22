d43697ccf13186a0a6d46b24b329b47cedc84ac4
Bryce Gibson <bryce@gibson-consulting.com.au>
2017-08-23 09:00:44 +1000

    Code style.

--- src/components/Widgets/NumberControl.js
+++ src/components/Widgets/NumberControl.js
@@ -3,23 +3,30 @@ import React, { PropTypes } from 'react';
 export default class NumberControl extends React.Component {
   handleChange = (e) => {
     const valueType = this.props.field.get('valueType');
-    let value;
+    const { onChange } = this.props;
     if(valueType === 'int') {
-      value = parseInt(e.target.value, 10);
+      onChange(parseInt(e.target.value, 10));
     } else if(valueType === 'float') {
-      value = parseFloat(e.target.value);
+      onChange(parseFloat(e.target.value));
     } else {
-      value = e.target.value;
+      onChange(e.target.value);
     }
-    this.props.onChange(value);
   };
 
   render() {
     const { field, value, forID } = this.props;
-    const min = field.get('min');
-    const max = field.get('max');
-    const step = field.get('step');
-    return <input type="number" id={forID} value={value || ''} step={step == null ? (field.get('valueType') === 'int' ? 1 : '') : step} min={min == null ? '' : min} max={max == null ? '' : max} onChange={this.handleChange} />;
+    const min = field.get('min', '');
+    const max = field.get('max', '');
+    const step = field.get('step', field.get('valueType') === 'int' ? 1 : '');
+    return <input
+      type="number"
+      id={forID}
+      value={value || ''}
+      step={step}
+      min={min}
+      max={max}
+      onChange={this.handleChange}
+    />;
   }
 }
 
