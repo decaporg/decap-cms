1eff7da25651125746bf467c83f8bad517586339
Bryce Gibson <bryce@gibson-consulting.com.au>
2017-08-22 14:53:05 +1000

    Enhance NumberControl.
    Adding support for `min`, `max`, `step` on the input element and adding
    `valueType` for specifying the return type.

--- src/components/Widgets/NumberControl.js
+++ src/components/Widgets/NumberControl.js
@@ -2,11 +2,24 @@ import React, { PropTypes } from 'react';
 
 export default class NumberControl extends React.Component {
   handleChange = (e) => {
-    this.props.onChange(parseInt(e.target.value, 10));
+    const valueType = this.props.field.get('valueType')
+    let value
+    if(valueType === 'int') {
+      value = parseInt(e.target.value, 10)
+    } else if(valueType === 'float') {
+      value = parseFloat(e.target.value)
+    } else {
+      value = e.target.value
+    }
+    this.props.onChange(value);
   };
 
   render() {
-    return <input type="number" id={this.props.forID} value={this.props.value || ''} onChange={this.handleChange} />;
+    const { field, value, forID } = this.props
+    const min = field.get('min')
+    const max = field.get('max')
+    const step = field.get('step')
+    return <input type="number" id={forID} value={value || ''} step={step == null ? (field.get('valueType') === 'int' ? 1 : '') : step} min={min == null ? '' : min} max={max == null ? '' : max} onChange={this.handleChange} />;
   }
 }
 
@@ -14,4 +27,8 @@ NumberControl.propTypes = {
   onChange: PropTypes.func.isRequired,
   value: PropTypes.node,
   forID: PropTypes.string,
+  valueType: PropTypes.string,
+  step: PropTypes.number,
+  min: PropTypes.number,
+  max: PropTypes.number,
 };
