890ee3d7e3ff75cbad66e793524be26facaa989f
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-09-05 13:24:16 -0700

    Update old netlify-auth-js to gotrue-js

--- src/lib/urlHelper.js
+++ src/lib/urlHelper.js
@@ -1,3 +1,5 @@
+import url from 'url';
+
 function getUrl(url, direct) {
   return `${ direct ? '/#' : '' }${ url }`;
 }
@@ -8,4 +10,33 @@ export function getCollectionUrl(collectionName, direct) {
 
 export function getNewEntryUrl(collectionName, direct) {
   return getUrl(`/collections/${ collectionName }/entries/new`, direct);
+}
+
+export function urlize(string) {
+  const sanitized = makePathSanitized(string);
+  const parsedURL = url.parse(sanitized);
+
+  return url.format(parsedURL);
+}
+
+function makePathSanitized(string) {
+  return makePath(string.toLowerCase());
+}
+
+function makePath(string) {
+  return unicodeSanitize(string).trim().replace(/[\s]/g, '-').replace(/-+/g, '-');
+}
+
+function unicodeSanitize(string) {
+  let target = [];
+  const runes = string.split('');
+  for (let i=0; i < string.length; i++) {
+    const r = runes[i];
+    if (r == '%' && i+2 < string.length && string.substr(i+1, 2).match(/^[0-9a-f]+$/)) {
+      target = target.concat([r, runes[i+1], runes[i+2]]);
+    } else if (r.match(/[\w .\/\\_#\+-]/u)) {
+      target.push(r);
+    }
+  }
+  return target.join('');
 }
