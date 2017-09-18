2c8e8bece043120580691aa1f0085ab99c166062
Caleb <inchristalway-tech4him@yahoo.com>
2017-09-18 08:27:49 -0600

    Fix collections breaking if folder has trailing slash.

--- src/reducers/collections.js
+++ src/reducers/collections.js
@@ -42,7 +42,7 @@ const selectors = {
       return collection.get('fields');
     },
     entryPath(collection, slug) {
-      return `${ collection.get('folder') }/${ slug }.${ this.entryExtension(collection) }`;
+      return `${ collection.get('folder').replace(/\/$/, '') }/${ slug }.${ this.entryExtension(collection) }`;
     },
     entrySlug(collection, path) {
       return path.split('/').pop().replace(/\.[^\.]+$/, '');
