51b89cc84b08c4c104addb52a343eec290565ebf
Caleb <inchristalway-tech4him@yahoo.com>
2017-09-29 14:56:15 -0600

    Switch from `slug` to `sanitize-filename`.

--- src/backends/backend.js
+++ src/backends/backend.js
@@ -5,7 +5,7 @@ import GitGatewayBackend from "./git-gateway/implementation";
 import { resolveFormat } from "../formats/formats";
 import { selectListMethod, selectEntrySlug, selectEntryPath, selectAllowNewEntries, selectFolderEntryExtension } from "../reducers/collections";
 import { createEntry } from "../valueObjects/Entry";
-import slug from 'slug';
+import sanitize from 'sanitize-filename';
 
 class LocalStorageAuthStore {
   storageKey = "netlify-cms-user";
@@ -51,9 +51,9 @@ const slugFormatter = (template = "{{slug}}", entryData) => {
       case "day":
         return (`0${ date.getDate() }`).slice(-2);
       case "slug":
-        return slug(getIdentifier(entryData).trim(), {lower: true});
+        return sanitize(getIdentifier(entryData).trim().toLowerCase(), {replacement: "-"});
       default:
-        return slug(entryData.get(field, "").trim(), {lower: true});
+        return sanitize(entryData.get(field, "").trim().toLowerCase(), {replacement: "-"});
     }
   });
 };
