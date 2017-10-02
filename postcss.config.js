4c7eaf1bba63c197531d2087164e8db1423cbdfe
Caleb <inchristalway-tech4him@yahoo.com>
2017-10-02 16:02:55 -0600

    Override react-toolbox font setting.

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,13 @@ module.exports = {
     require('postcss-import')({ addDependencyTo: webpack }),
     require('postcss-cssnext')({
       browsers: ['last 2 versions', 'IE > 10'],
+      features: {
+        customProperties: {
+          variables: {
+            "preferred-font": 'inherit', // Override react-toolbox font setting
+          },
+        },
+      },
     }),
   ],
 };
