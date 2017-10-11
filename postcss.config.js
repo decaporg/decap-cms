b49e607d2ec43ab3fb813d02d0d1c2f90b691394
Caleb <inchristalway-tech4him@yahoo.com>
2017-10-10 20:12:21 -0600

    Move supported browser list to package.json.
    That way all build tools can use it.

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,6 @@ module.exports = {
   plugins: [
     require('postcss-import')({ addDependencyTo: webpack }),
     require('postcss-cssnext')({
-      browsers: ['last 2 versions', 'IE > 10'],
       features: {
         customProperties: {
           variables: {
