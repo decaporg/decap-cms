c15dea78e9a60fd72be85e50a2bfd517617727f8
Shawn Erquhart <shawn@erquh.art>
2017-10-18 10:46:18 -0400

    update to cssnano v4 so preset is used

--- webpack.base.js
+++ webpack.base.js
@@ -43,7 +43,7 @@ module.exports = {
         }),
       },
       {
-        /* We use CSS-modules and PostCSS for CMS styles */
+        /* We use PostCSS for CMS styles */
         test: /\.css$/,
         exclude: [/node_modules/],
         use: ExtractTextPlugin.extract({
