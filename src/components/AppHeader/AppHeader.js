WittyPlex/netlify-cms

Replace prefixer with literal class names

src/components/AppHeader/AppHeader.js
@@ -62,16 +62,16 @@ export default class AppHeader extends React.Component {
     };
 
     const theme = {
-      appBar: styles("appBar"),
-      homeLink: styles("homeLink"),
-      iconMenu: styles("iconMenu"),
-      icon: styles("icon"),
-      leftIcon: styles("leftIcon"),
-      base: themeStyles("base"),
-      container: themeStyles("container"),
-      rounded: themeStyles("rounded"),
-      depth: themeStyles("depth"),
-      clearfix: themeStyles("clearfix"),
+      appBar: 'nc-appHeader-appBar',
+      homeLink: 'nc-appHeader-homeLink',
+      iconMenu: 'nc-appHeader-iconMenu',
+      icon: 'nc-appHeader-icon',
+      leftIcon: 'nc-appHeader-leftIcon',
+      base: 'nc-theme-base',
+      container: 'nc-theme-container',
+      rounded: 'nc-theme-rounded',
+      depth: 'nc-theme-depth',
+      clearfix: 'nc-theme-clearfix',
     };
 
     return (
@@ -82,8 +82,8 @@ export default class AppHeader extends React.Component {
         onLeftIconClick={toggleDrawer}
         onRightIconClick={this.handleRightIconClick}
       >
-        <IndexLink to="/" className={styles("homeLink")}>
-          <FontIcon value="home" className={styles("icon")} />
+        <IndexLink to="/" className="nc-appHeader-homeLink">
+          <FontIcon value="home" className="nc-appHeader-icon" />
         </IndexLink>
         <IconMenu
           theme={theme}

src/containers/App.js
@@ -136,26 +136,26 @@ class App extends React.Component {
 
     const sidebarContent = (
       <div>
-        <Navigation type="vertical" className={sidebarStyles("nav")}>
+        <Navigation type="vertical" className="nc-sidebar-nav">
           {
             publishMode === SIMPLE ? null :
             <section>
-              <h1 className={sidebarStyles("heading")}>Publishing</h1>
-              <div className={sidebarStyles("linkWrapper")}>
-                <IndexLink to="/" className={sidebarStyles("viewEntriesLink")}>Editorial Workflow</IndexLink>
+              <h1 className="nc-sidebar-heading">Publishing</h1>
+              <div className="nc-sidebar-linkWrapper">
+                <IndexLink to="/" className="nc-sidebar-viewEntriesLink">Editorial Workflow</IndexLink>
               </div>
             </section>
           }
           <section>
-            <h1 className={sidebarStyles("heading")}>Collections</h1>
+            <h1 className="nc-sidebar-heading">Collections</h1>
             {
               collections.valueSeq().map((collection) => {
                 const collectionName = collection.get('name');
                 return (
-                  <div key={collectionName} className={sidebarStyles("linkWrapper")}>
+                  <div key={collectionName} className="nc-sidebar-linkWrapper">
                     <a
                       href={getCollectionUrl(collectionName, true)}
-                      className={sidebarStyles("viewEntriesLink")}
+                      className="nc-sidebar-viewEntriesLink"
                       onClick={e => this.handleLinkClick(e, navigateToCollection, collectionName)}
                     >
                       {collection.get('label')}
@@ -164,7 +164,7 @@ class App extends React.Component {
                       collection.get('create') ? (
                         <a
                           href={getNewEntryUrl(collectionName, true)}
-                          className={sidebarStyles("createEntryLink")}
+                          className="nc-sidebar-createEntryLink"
                           onClick={e => this.handleLinkClick(e, createNewEntryInCollection, collectionName)}
                         >
                           <FontIcon value="add_circle_outline" />
@@ -192,7 +192,7 @@ class App extends React.Component {
             onLogoutClick={logoutUser}
             toggleDrawer={toggleSidebar}
           />
-          <div className={styles("entriesPanel")}>
+          <div className="nc-app-entriesPanel">
             { isFetching && <TopBarProgress /> }
             <div>
               {children}

decaporg/decap-cms

src/components/AppHeader/AppHeader.js
@@ -62,16 +62,16 @@ export default class AppHeader extends React.Component {
     };
 
     const theme = {
-      appBar: styles("appBar"),
-      homeLink: styles("homeLink"),
-      iconMenu: styles("iconMenu"),
-      icon: styles("icon"),
-      leftIcon: styles("leftIcon"),
-      base: themeStyles("base"),
-      container: themeStyles("container"),
-      rounded: themeStyles("rounded"),
-      depth: themeStyles("depth"),
-      clearfix: themeStyles("clearfix"),
+      appBar: 'nc-appHeader-appBar',
+      homeLink: 'nc-appHeader-homeLink',
+      iconMenu: 'nc-appHeader-iconMenu',
+      icon: 'nc-appHeader-icon',
+      leftIcon: 'nc-appHeader-leftIcon',
+      base: 'nc-theme-base',
+      container: 'nc-theme-container',
+      rounded: 'nc-theme-rounded',
+      depth: 'nc-theme-depth',
+      clearfix: 'nc-theme-clearfix',
     };
 
     return (
@@ -82,8 +82,8 @@ export default class AppHeader extends React.Component {
         onLeftIconClick={toggleDrawer}
         onRightIconClick={this.handleRightIconClick}
       >
-        <Link to="/" className={styles("homeLink")}>
-          <FontIcon value="home" className={styles("icon")} />
+        <Link to="/" className="nc-appHeader-homeLink">
+          <FontIcon value="home" className="nc-appHeader-icon" />
         </Link>
         <IconMenu
           theme={theme}

src/containers/App.js
@@ -139,26 +139,26 @@ class App extends React.Component {
 
     const sidebarContent = (
       <div>
-        <Navigation type="vertical" className={sidebarStyles("nav")}>
+        <Navigation type="vertical" className="nc-sidebar-nav">
           {
             publishMode === SIMPLE ? null :
             <section>
-              <h1 className={sidebarStyles("heading")}>Publishing</h1>
-              <div className={sidebarStyles("linkWrapper")}>
-                <Link to="/" className={sidebarStyles("viewEntriesLink")}>Editorial Workflow</Link>
+              <h1 className="nc-sidebar-heading">Publishing</h1>
+              <div className="nc-sidebar-linkWrapper">
+                <Link to="/" className="nc-sidebar-viewEntriesLink">Editorial Workflow</Link>
               </div>
             </section>
           }
           <section>
-            <h1 className={sidebarStyles("heading")}>Collections</h1>
+            <h1 className="nc-sidebar-heading">Collections</h1>
             {
               collections.valueSeq().map((collection) => {
                 const collectionName = collection.get('name');
                 return (
-                  <div key={collectionName} className={sidebarStyles("linkWrapper")}>
+                  <div key={collectionName} className="nc-sidebar-linkWrapper">
                     <a
                       href={getCollectionUrl(collectionName, true)}
-                      className={sidebarStyles("viewEntriesLink")}
+                      className="nc-sidebar-viewEntriesLink"
                       onClick={e => this.handleLinkClick(e, navigateToCollection, collectionName)}
                     >
                       {collection.get('label')}
@@ -167,7 +167,7 @@ class App extends React.Component {
                       collection.get('create') ? (
                         <a
                           href={getNewEntryUrl(collectionName, true)}
-                          className={sidebarStyles("createEntryLink")}
+                          className="nc-sidebar-createEntryLink"
                           onClick={e => this.handleLinkClick(e, createNewEntryInCollection, collectionName)}
                         >
                           <FontIcon value="add_circle_outline" />
@@ -195,7 +195,7 @@ class App extends React.Component {
             onLogoutClick={logoutUser}
             toggleDrawer={toggleSidebar}
           />
-          <div className={styles("entriesPanel")}>
+          <div className="nc-app-entriesPanel">
             { isFetching && <TopBarProgress /> }
             <div>
               <Switch>