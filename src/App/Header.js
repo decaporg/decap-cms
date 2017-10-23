6aa5c25e8d187d5bdebe049f8de01253f63083f2
Shawn Erquhart <shawn@erquh.art>
2017-10-23 18:57:57 -0400

    improve global layout

--- src/App/Header.js
+++ src/App/Header.js
@@ -48,26 +48,28 @@ export default class Header extends React.Component {
 
     return (
       <div className="nc-appHeader-container">
-        <nav>
-          <NavLink to="/" className="nc-appHeader-button" activeClassName="nc-appHeader-button-active">
-            <Icon type="page"/>
-            Content
-          </NavLink>
-          <button onClick={openMediaLibrary} className="nc-appHeader-button">
-            <Icon type="media-alt"/>
-            Media
-          </button>
-        </nav>
-        <div className="nc-appHeader-actions">
-          <button className="nc-appHeader-button nc-appHeader-quickNew">
-            Quick new
-          </button>
-          <a className="nc-appHeader-siteLink" href="http://olddvdscreensaver.com" target="_blank">
-            olddvdscreensaver.com
-          </a>
-          <button className="nc-appHeader-avatar">
-            <img src={user.get('avatar_url')}/>
-          </button>
+        <div className="nc-appHeader-content">
+          <nav>
+            <NavLink to="/" className="nc-appHeader-button" activeClassName="nc-appHeader-button-active">
+              <Icon type="page"/>
+              Content
+            </NavLink>
+            <button onClick={openMediaLibrary} className="nc-appHeader-button">
+              <Icon type="media-alt"/>
+              Media
+            </button>
+          </nav>
+          <div className="nc-appHeader-actions">
+            <button className="nc-appHeader-button nc-appHeader-quickNew">
+              Quick new
+            </button>
+            <a className="nc-appHeader-siteLink" href="http://olddvdscreensaver.com" target="_blank">
+              olddvdscreensaver.com
+            </a>
+            <button className="nc-appHeader-avatar">
+              <img src={user.get('avatar_url')}/>
+            </button>
+          </div>
         </div>
       </div>
     );
