692ca7a84c82b67f7e74a7a3d2b1e5986d190e27
Caleb <inchristalway-tech4him@yahoo.com>
2017-10-25 18:43:02 -0600

    Move `sortKeys` into a helper function.

---
+++ src/formats/helpers.js
@@ -0,0 +1,13 @@
+export const sortKeys = (sortedKeys = []) => (a, b) => {
+  const idxA = sortedKeys.indexOf(a);
+  const idxB = sortedKeys.indexOf(b);
+  if (idxA === -1 || idxB === -1) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  }
+
+  if (idxA > idxB) return 1;
+  if (idxA < idxB) return -1;
+  return 0;
+};
