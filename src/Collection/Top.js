dfcd04af099b12c3a62d2b20c21b1d904443bb59
Shawn Erquhart <shawn@erquh.art>
2017-11-03 15:17:25 -0400

    enable new entry button

--- src/Collection/Top.js
+++ src/Collection/Top.js
@@ -1,14 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Top = ({ collectionName }) =>
-  <div className="nc-collectionPage-top">
-    <h1 className="nc-collectionPage-topHeading">{collectionName}</h1>
-    <button className="nc-collectionPage-topNewButton">{`New ${collectionName}`}</button>
-  </div>;
+const Top = ({ collectionLabel, newEntryUrl }) => {
+  const newEntryLink = (
+    <a className="nc-collectionPage-topNewButton" href={newEntryUrl}>{`New ${collectionLabel}`}</a>
+  );
+
+  return (
+    <div className="nc-collectionPage-top">
+      <h1 className="nc-collectionPage-topHeading">{collectionLabel}</h1>
+      { newEntryUrl ? newEntryLink : null }
+    </div>
+  );
+};
 
 Top.propTypes = {
-  collectionName: PropTypes.string.isRequired,
+  collectionLabel: PropTypes.string.isRequired,
+  newEntryUrl: PropTypes.string
 };
 
 export default Top;
