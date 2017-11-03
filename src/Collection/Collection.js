d0de6c7bc00ceb8ef83f9d8ed48539fd3c007a7c
Shawn Erquhart <shawn@erquh.art>
2017-11-03 13:42:47 -0400

    merge search page into collection page

--- src/Collection/Collection.js
+++ src/Collection/Collection.js
@@ -1,83 +1,45 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { connect } from 'react-redux';
-import { loadEntries } from '../actions/entries';
-import { showCollection } from '../actions/collections';
-import { selectEntries } from '../reducers';
-import { Loader } from '../components/UI';
 import Sidebar from './Sidebar';
 import Top from './Top';
-import Entries from './Entries';
-import EntryListing from '../components/EntryListing/EntryListing';
+import EntriesCollection from './Entries/EntriesCollection';
+import EntriesSearch from './Entries/EntriesSearch';
 
 class Collection extends React.Component {
-
   static propTypes = {
     collection: ImmutablePropTypes.map.isRequired,
     collections: ImmutablePropTypes.orderedMap.isRequired,
-    publicFolder: PropTypes.string.isRequired,
-    dispatch: PropTypes.func.isRequired,
-    page: PropTypes.number,
-    entries: ImmutablePropTypes.list,
-    isFetching: PropTypes.bool.isRequired,
   };
 
-  state = { query: '' };
+  renderEntriesCollection = () => {
+    const { name, collection } = this.props;
+    return <EntriesCollection collection={collection} name={name} />
+  };
 
-  componentDidMount() {
-    const { collection, dispatch } = this.props;
-    if (collection) {
-      dispatch(loadEntries(collection));
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { collection, dispatch } = this.props;
-    if (nextProps.collection !== collection) {
-      dispatch(loadEntries(nextProps.collection));
-    }
-  }
-
-  handleLoadMore = (page) => {
-    const { collection, dispatch } = this.props;
-    dispatch(loadEntries(collection, page));
+  renderEntriesSearch = () => {
+    const { searchTerm, collections } = this.props;
+    return <EntriesSearch collections={collections} searchTerm={searchTerm} />
   };
 
   render() {
-    const { collections, collection, publicFolder, page, entries, isFetching } = this.props;
-    const { query } = this.state;
-
+    const { collection, collections, isSearchResults, searchTerm } = this.props;
     return (
       <div className="nc-collectionPage-container">
-        <Sidebar collections={collections}/>
-        <Top collectionName={collection.get('label')}/>
-        <Entries
-          collections={collection}
-          entries={entries}
-          publicFolder={publicFolder}
-          page={page}
-          onPaginate={this.handleLoadMore}
-          isFetching={isFetching}
-          collectionName={collection.get('label')}
-        />
+        <Sidebar collections={collections} searchTerm={searchTerm}/>
+        { isSearchResults ? null : <Top collectionName={collection.get('label')}/> }
+        { isSearchResults ? this.renderEntriesSearch() : this.renderEntriesCollection() }
       </div>
     );
   }
 }
 
-
 function mapStateToProps(state, ownProps) {
-  const { collections, config } = state;
-  const { name } = ownProps.match.params;
-  const publicFolder = config.get('public_folder');
+  const { collections } = state;
+  const { isSearchResults, match } = ownProps;
+  const { name, searchTerm } = match.params;
   const collection = name ? collections.get(name) : collections.first();
-  const page = state.entries.getIn(['pages', collection.get('name'), 'page']);
-
-  const entries = selectEntries(state, collection.get('name'));
-  const isFetching = state.entries.getIn(['pages', collection.get('name'), 'isFetching'], false);
-
-  return { publicFolder, collection, collections, page, entries, isFetching };
+  return { collection, collections, isSearchResults, searchTerm };
 }
 
 export default connect(mapStateToProps)(Collection);
