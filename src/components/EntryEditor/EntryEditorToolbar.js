4336aee3ccbb6f1932abcd92109487b3f637bb24
Shawn Erquhart <shawn@erquh.art>
2017-11-06 16:58:18 -0500

    wip

--- src/components/EntryEditor/EntryEditorToolbar.js
+++ src/components/EntryEditor/EntryEditorToolbar.js
@@ -13,7 +13,14 @@ const EntryEditorToolbar = (
   }) => {
   const disabled = !enableSave || isPersisting;
   return (
-    <div>
+    <div className="nc-entryEditor-toolbar">
+      <div onClick={onCancelEdit} className="nc-entryEditor-toolbar-backSection">
+        <div className="nc-entryEditor-toolbar-backArrow">←</div>
+        <div>
+          <div className="nc-entryEditor-toolbar-backCollection">Writing in Posts collection</div>
+          <div className="nc-entryEditor-toolbar-backStatus">Changes saved &#10003;</div>
+        </div>
+      </div>
       <Button
         primary
         raised
@@ -29,9 +36,6 @@ const EntryEditorToolbar = (
            </Button>)
         : '' }
       { showDelete ? ' ' : '' }
-      <Button onClick={onCancelEdit}>
-        Cancel
-      </Button>
     </div>
   );
 };
