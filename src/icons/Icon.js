baa062a91120463b8d43c66175d0065c37259c0f
Shawn Erquhart <shawn@erquh.art>
2017-11-07 23:44:38 -0500

    finished basic editor toolbar

--- src/icons/Icon.js
+++ src/icons/Icon.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import iconAdd from './icon-add.svg';
+import iconArrow from './icon-arrow.svg';
 import iconCircle from './icon-circle.svg';
 import iconFolder from './icon-folder.svg';
 import iconGrid from './icon-grid.svg';
@@ -14,29 +15,75 @@ import iconSettings from './icon-settings.svg';
 import iconWorkflow from './icon-workflow.svg';
 import iconWrite from './icon-write.svg';
 
-const svgIcons = {
-  'add': iconAdd,
-  'circle': iconCircle,
-  'folder': iconFolder,
-  'grid': iconGrid,
-  'home': iconHome,
-  'list': iconList,
-  'media': iconMedia,
-  'media-alt': iconMediaAlt,
-  'page': iconPage,
-  'pages': iconPages,
-  'pages-alt': iconPagesAlt,
-  'settings': iconSettings,
-  'workflow': iconWorkflow,
-  'write': iconWrite,
+const icons = {
+  'add': {
+    image: iconAdd,
+  },
+  'arrow': {
+    image: iconArrow,
+    direction: 'left',
+  },
+  'circle': {
+    image: iconCircle,
+  },
+  'folder': {
+    image: iconFolder,
+  },
+  'grid': {
+    image: iconGrid,
+  },
+  'home': {
+    image: iconHome,
+  },
+  'list': {
+    image: iconList,
+  },
+  'media': {
+    image: iconMedia,
+  },
+  'media-alt': {
+    image: iconMediaAlt,
+  },
+  'page': {
+    image: iconPage,
+  },
+  'pages': {
+    image: iconPages,
+  },
+  'pages-alt': {
+    image: iconPagesAlt,
+  },
+  'settings': {
+    image: iconSettings,
+  },
+  'workflow': {
+    image: iconWorkflow,
+  },
+  'write': {
+    image: iconWrite,
+  },
 };
 
+const getRotation = (iconDirection, newDirection) => {
+  if (!iconDirection || !newDirection) {
+    return '0deg';
+  }
+  const rotations = { right: 90, down: 180, left: 270, up: 360 };
+  const degrees = rotations[newDirection] - rotations[iconDirection];
+  return `${degrees}deg`;
+}
+
 const Icon = props => {
-  const { type, className = '', ...remainingProps } = props;
+  const { type, direction, size = 'medium', className = '', ...remainingProps } = props;
+  const icon = icons[type];
+  const rotation = getRotation(icon.direction, direction)
+  const transform = `rotate(${rotation})`;
+  const style = { transform };
   return (
     <span
-      dangerouslySetInnerHTML={{ __html: svgIcons[type] }}
-      className={`nc-icon ${className}`}
+      dangerouslySetInnerHTML={{ __html: icon.image }}
+      className={`nc-icon nc-icon-${size} ${className}`}
+      style={style}
       {...remainingProps}
     ></span>
   );
