ee4d445ad2f64f487ae6be3867eddf11843ce5eb
Shawn Erquhart <shawn@erquh.art>
2017-11-07 23:44:38 -0500

    finished basic editor toolbar

--- src/components/EntryEditor/EntryEditorToolbar.js
+++ src/components/EntryEditor/EntryEditorToolbar.js
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import React from 'react';
-import { Button } from 'react-toolbox/lib/button';
 import {
   Wrapper as Dropdown,
   Button as DropdownButton,
   Menu as DropdownList,
   MenuItem as DropdownItem,
 } from 'react-aria-menubutton';
+import Icon from '../../icons/Icon';
 
 const EntryEditorToolbar = (
   {
@@ -40,16 +40,24 @@ const EntryEditorToolbar = (
             ? <button className="nc-entryEditor-toolbar-deleteButton" onClick={onDelete}>Delete</button>
             : null
         }
-        <button className="nc-entryEditor-toolbar-saveButton" onClick={onPersist} disabled={disabled}>
-        </button>
-        <Dropdown className="nc-entryEditor-toolbar-saveDropdown">
+        <Dropdown className="nc-entryEditor-toolbar-saveDropdown" onSelection={handler => handler()}>
           <DropdownButton className="nc-entryEditor-toolbar-saveDropdownButton">
             { isPersisting ? 'Saving...' : 'Save and Publish' }
           </DropdownButton>
           <DropdownList>
             <ul className="nc-entryEditor-toolbar-saveDropdownList">
-              <DropdownItem className="nc-entryEditor-toolbar-saveDropdownItem">Save and publish</DropdownItem>
-              <DropdownItem className="nc-entryEditor-toolbar-saveDropdownItem">Save, publish, and create new</DropdownItem>
+              <DropdownItem className="nc-entryEditor-toolbar-saveDropdownItem" value={onPersist}>
+                <span>Save and publish</span>
+                <span className="nc-entryEditor-toolbar-saveDropdownItemIcon">
+                  <Icon type="folder" direction="right" size="small"/>
+                </span>
+              </DropdownItem>
+              <DropdownItem className="nc-entryEditor-toolbar-saveDropdownItem" value={onPersist}>
+                <span>Save, publish, and create new</span>
+                <span className="nc-entryEditor-toolbar-saveDropdownItemIcon">
+                  <Icon type="add" size="small"/>
+                </span>
+              </DropdownItem>
             </ul>
           </DropdownList>
         </Dropdown>
