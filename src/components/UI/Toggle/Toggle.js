36ab63c04f4cbef2d8c5bbf42bb36eff210950dd
Shawn Erquhart <shawn@erquh.art>
2017-11-10 16:31:24 -0500

    finish markdown toggle styling

--- src/components/UI/Toggle/Toggle.js
+++ src/components/UI/Toggle/Toggle.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Toggle from 'react-toggled';
 import c from 'classnames';
 
-const Switch = ({ active, onChange }) => (
+const Switch = ({ active, onChange, className }) => (
   <Toggle on={active} onToggle={onChange}>
     {({on, getElementTogglerProps}) => (
       <span
-        className={c('nc-toggle', { 'nc-toggle-active': on })}
+        className={c('nc-toggle', className, { 'nc-toggle-active': on })}
         role="switch"
         aria-checked={on.toString()}
         {...getElementTogglerProps()}
