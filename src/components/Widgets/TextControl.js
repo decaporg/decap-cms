0b404cc45648d009eec197ff8c532f4ef687fa17
Shawn Erquhart <shawn@erquh.art>
2017-11-13 20:57:38 -0500

    widget design updates, basic list/object design update

--- src/components/Widgets/TextControl.js
+++ src/components/Widgets/TextControl.js
@@ -9,7 +9,19 @@ export default class TextControl extends React.Component {
     value: PropTypes.node,
   };
 
+  /**
+   * Always update to ensure `react-textarea-autosize` properly calculates
+   * height. Certain situations, such as this widget being nested in a list
+   * item that gets rearranged, can leave the textarea in a minimal height
+   * state. Always updating should generally be low cost, but this should be
+   * optimized in the future.
+   */
+  shouldComponentUpdate(nextProps) {
+    return true;
+  }
+
   render() {
+    console.log('rendering');
     const {forID, value = '', onChange } = this.props;
 
     return (
