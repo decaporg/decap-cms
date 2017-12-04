bfb8fc3701e4c6a3329d8aac7b7f9f9ab0da7458
Jessica Parsons <verythorough@gmail.com>
2017-12-04 11:25:49 -0800

    Migrate website from netlify-cms-www
    Some modifications, including most of the changes in https://github.com/netlify/netlify-cms-www/pull/58 (previously reverted).
    
    Also updated the readme and added hugo-bin for quicker onboarding of new docs contributors.

---
+++ website/config/variables.js
@@ -0,0 +1,45 @@
+// if you change these you must restart the server
+
+module.exports = {
+
+  // colors
+  lightestGrey: '#E6E6E6',
+  lighterGrey: '#F7F8F8',
+  lightGrey: '#F6F6F6',
+  grey: '#313D3E',
+  darkGrey: '#2F3132',
+  darkerGrey: '#1C1E1E',
+  lightGreen: '#97bf2f',
+  green: '#C9FA4B',
+  darkGreen: '#7CA511',
+
+  // typography
+  thin: 100,
+  light: 300,
+  regular: 400,
+  semibold: 500,
+  bold: 700,
+  black: 900,
+
+  // fonts
+  roboto: "'Roboto', -apple-system, BlinkMacSystemFont, Helvetica, Arial, sans-serif",
+
+  // padding
+  micro: '8px',
+  tiny: '16px',
+  small: '24px',
+  medium: '40px',
+  large: '64px',
+  xl: '104px',
+  xxl: '168px',
+
+  // border radius
+  borderRadius: '4px',
+  largeBorderRadius: '10px',
+
+  // responsive breakpoints
+  mobile: '480px',
+  tablet: '768px',
+  desktop: '960px',
+  display: '1200px'
+}
