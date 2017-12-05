6500a20dbe162258184fe79a80b0f5a3818ed1c2
Shawn Erquhart <shawn@erquh.art>
2017-12-05 10:19:04 -0500

    add configurable collection descriptions

--- src/components/Collection/Top.js
+++ src/components/Collection/Top.js
@@ -1,21 +1,29 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Top = ({ collectionLabel, newEntryUrl }) => {
+const Top = ({ collectionLabel, collectionDescription, newEntryUrl }) => {
   const newEntryLink = (
     <a className="nc-collectionPage-topNewButton" href={newEntryUrl}>{`New ${collectionLabel}`}</a>
   );
 
   return (
     <div className="nc-collectionPage-top">
-      <h1 className="nc-collectionPage-topHeading">{collectionLabel}</h1>
-      { newEntryUrl ? newEntryLink : null }
+      <div className="nc-collectionPage-top-row">
+        <h1 className="nc-collectionPage-topHeading">{collectionLabel}</h1>
+        { newEntryUrl ? newEntryLink : null }
+      </div>
+      {
+        collectionDescription
+          ? <p className="nc-collectionPage-top-description">{collectionDescription}</p>
+          : null
+      }
     </div>
   );
 };
 
 Top.propTypes = {
   collectionLabel: PropTypes.string.isRequired,
+  collectionDescription: PropTypes.string,
   newEntryUrl: PropTypes.string
 };
 
