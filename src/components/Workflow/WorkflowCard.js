705262cf702dd2153736b37911e195472e921052
Shawn Erquhart <shawn@erquh.art>
2017-12-06 00:28:41 -0500

    fix workflow card button handlers

--- src/components/Workflow/WorkflowCard.js
+++ src/components/Workflow/WorkflowCard.js
@@ -16,11 +16,13 @@ const WorkflowCard = ({
   canPublish,
   onPublish,
 }) => (
-  <Link to={editLink} className="nc-workflow-card">
-    <div className="nc-workflow-card-collection">{collectionName}</div>
-    <h2 className="nc-workflow-card-title">{title}</h2>
-    <div className="nc-workflow-card-date">{timestamp} by {authorLastChange}</div>
-    <p className="nc-workflow-card-body">{body}</p>
+  <div className="nc-workflow-card">
+    <Link to={editLink} className="nc-workflow-link">
+      <div className="nc-workflow-card-collection">{collectionName}</div>
+      <h2 className="nc-workflow-card-title">{title}</h2>
+      <div className="nc-workflow-card-date">{timestamp} by {authorLastChange}</div>
+      <p className="nc-workflow-card-body">{body}</p>
+    </Link>
     <div className="nc-workflow-card-button-container">
       <button className="nc-workflow-card-buttonDelete" onClick={onDelete}>
         {isModification ? 'Delete changes' : 'Delete new entry'}
@@ -29,7 +31,7 @@ const WorkflowCard = ({
         {isModification ? 'Publish changes' : 'Publish new entry'}
       </button>
     </div>
-  </Link>
+  </div>
 );
 
 export default WorkflowCard;
