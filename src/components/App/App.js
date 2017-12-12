nnyath/netlify-cms

Fix loosing user state during re-auth.
We were clearing out any old user state when sending an authentication
request (or failure), which makes sense, but we have to keep the old
user object while re-authenticating or it will ruin our app's current
state.  This doesn't change our current (initial) auth at all.

src/containers/App.js
@@ -221,8 +221,8 @@ class App extends React.Component {
 
 function mapStateToProps(state) {
   const { auth, config, collections, globalUI } = state;
-  const user = auth && auth.get('user');
-  const authPopup = (auth && auth.get('popup')) || false;
+  const user = auth.get('user', null);
+  const authPopup = auth.get('popup', false);
   const isFetching = globalUI.get('isFetching');
   const publishMode = config && config.get('publish_mode');
   return { auth, config, collections, user, isFetching, publishMode, authPopup };

decaporg/decap-cms

src/components/App/App.js
@@ -160,8 +160,8 @@ class App extends React.Component {
 
 function mapStateToProps(state, ownProps) {
   const { auth, config, collections, globalUI } = state;
-  const authModalOpen = (auth && auth.get('modal')) || false;
-  const user = auth && auth.get('user');
+  const user = auth.get('user', null);
+  const authModalOpen = auth.get('modal', false);
   const isFetching = globalUI.get('isFetching');
   const publishMode = config && config.get('publish_mode');
   return { auth, config, collections, user, isFetching, publishMode, authModalOpen };