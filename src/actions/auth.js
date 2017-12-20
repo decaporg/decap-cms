nnyath/netlify-cms

Gentle re-authentication modal/popup.

src/actions/auth.js
@@ -7,11 +7,18 @@ export const AUTH_REQUEST = 'AUTH_REQUEST';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_FAILURE = 'AUTH_FAILURE';
 export const AUTH_REQUEST_DONE = 'AUTH_REQUEST_DONE';
+export const SHOW_AUTH_POPUP = 'SHOW_AUTH_POPUP';
 export const LOGOUT = 'LOGOUT';
 
 export function authenticating() {
   return {
     type: AUTH_REQUEST,
+  };
+}
+
+export function reAuth() {
+  return {
+    type: SHOW_AUTH_POPUP,
   };
 }
 
@@ -48,7 +55,7 @@ export function authenticateUser() {
     const state = getState();
     const backend = currentBackend(state.config);
     dispatch(authenticating());
-    return backend.currentUser()
+    return backend.currentUser(() => dispatch(reAuth()))
       .then((user) => {
         if (user) {
           dispatch(authenticate(user));
@@ -69,7 +76,7 @@ export function loginUser(credentials) {
     const backend = currentBackend(state.config);
 
     dispatch(authenticating());
-    return backend.authenticate(credentials)
+    return backend.authenticate(credentials, () => dispatch(reAuth()))
       .then((user) => {
         dispatch(authenticate(user));
       })

decaporg/decap-cms

src/actions/auth.js
@@ -7,11 +7,18 @@ export const AUTH_REQUEST = 'AUTH_REQUEST';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_FAILURE = 'AUTH_FAILURE';
 export const AUTH_REQUEST_DONE = 'AUTH_REQUEST_DONE';
+export const REAUTHENTICATE = 'REAUTHENTICATE';
 export const LOGOUT = 'LOGOUT';
 
 export function authenticating() {
   return {
     type: AUTH_REQUEST,
+  };
+}
+
+export function reAuth() {
+  return {
+    type: REAUTHENTICATE,
   };
 }
 
@@ -48,7 +55,7 @@ export function authenticateUser() {
     const state = getState();
     const backend = currentBackend(state.config);
     dispatch(authenticating());
-    return backend.currentUser()
+    return backend.currentUser(() => dispatch(reAuth()))
       .then((user) => {
         if (user) {
           dispatch(authenticate(user));
@@ -69,7 +76,7 @@ export function loginUser(credentials) {
     const backend = currentBackend(state.config);
 
     dispatch(authenticating());
-    return backend.authenticate(credentials)
+    return backend.authenticate(credentials, () => dispatch(reAuth()))
       .then((user) => {
         dispatch(authenticate(user));
       })