26c638571c41bf22a9e4a84a20a5ebf2608b231d
Luís Miguel <luism@marzeelabs.org>
2018-01-09 11:53:56 +0000

    Fix proptype warning

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -36,7 +36,7 @@ class Collection extends React.Component {
 
   render() {
     const { collection, collections, collectionName, isSearchResults, searchTerm } = this.props;
-    const newEntryUrl = collection.get('create') && getNewEntryUrl(collectionName);
+    const newEntryUrl = collection.get('create') ? getNewEntryUrl(collectionName) : '';
     return (
       <div className="nc-collectionPage-container">
         <Sidebar collections={collections} searchTerm={searchTerm}/>
