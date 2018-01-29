d5b7bd062d091879f6e50ef3d42ebb713de9225d
Owen Hoskins <owen.hoskins@gmail.com>
2018-01-29 17:18:02 +0200

    testing pica library

--- src/components/EditorWidgets/Gallery/GalleryPreview.js
+++ src/components/EditorWidgets/Gallery/GalleryPreview.js
@@ -1,110 +1,10 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
-/*
-import Gallery from 'react-photo-gallery'
-export default class GalleryPreview extends Component {
+// import pica from 'pica'
 
-  static propTypes = {
-    value: PropTypes.node,
-  }
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      images: [],
-      loaded: []
-    }
-  }
-
-  componentDidMount() {
-    this.loadImages()
-  }
-
-  componentDidUpdate() {
-    this.loadImages()
-  }
-
-  loadImages = () => {
-    const { value, getAsset } = this.props
-    const self = this
-    value.forEach(function(val, index) {
-      const src = val.getIn(['image'])
-      // console.log('valueMap: ', val);
-      // console.log('imageMap src: ', src)
-      const asset = getAsset(src)
-      // console.log('getAsset: ', asset, asset && asset.path)
-
-      if (asset) {
-
-        var found = self.state.loaded.findIndex(element => {
-          return element === asset.path
-        });
-
-        // console.log('found? ', found)
-
-        if (found !== -1) {
-          // https://stackoverflow.com/questions/6011378/how-to-add-image-to-canvas
-
-          self.setState(prevState => ({
-            loaded: [
-              ...prevState.loaded,
-              asset.path
-            ]
-          }));
-
-          var image = new Image();
-          image.src = asset.path;
-          image.onload = function(event) {
-            const img = event.currentTarget
-
-            // https://stackoverflow.com/questions/586182/how-to-insert-an-item-into-an-array-at-a-specific-index
-            // arr.splice(index, 0, item);
-
-            // https://stackoverflow.com/questions/26253351/correct-modification-of-state-arrays-in-reactjs
-
-            self.setState(prevState => ({
-              images: [
-                ...prevState.images,
-                {
-                  src: img.src,
-                  width: img.naturalWidth,
-                  height: img.naturalHeight,
-                  order: index
-                }
-              ]
-            }))
-
-
-          }
-        } else {
-          // it is found.
-          // is it's order correct?
-          console.log('found index: ', found)
-
-        }
-      }
-    })
-  }
-
-  get photos() {
-    if (this.state.loaded.length > 0) {
-      const ordered = this.state.images.concat().sort(function(a, b){
-        return a.order > b.order;
-      });
-      return ordered
-    } else {
-      return []
-    }
-  }
-
-  render() {
-    console.log('this.photos: ', this.photos)
-    return <Gallery photos={this.photos} />
-  }
-}*/
-
+// https://github.com/nodeca/pica
+const pica = require('pica')();
 
 // https://stackoverflow.com/a/7557690
 
@@ -131,11 +31,20 @@ export class ImageCanvas extends Component {
     const image = new Image()
     image.src = src
     image.onload = function(event){
+
       const aspectRatio = image.width / image.height
       canvas.width = aspectRatio * 210
       canvas.height = 210
-      console.log('aspectRatio, width: ', aspectRatio, ctx.width, ctx.height)
-      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      pica.resize(image, canvas, {
+        unsharpAmount: 80,
+        unsharpRadius: 0.6,
+        unsharpThreshold: 2
+      })
+      .then(result => console.log('resize done!', result));
+
+      //console.log('aspectRatio, width: ', aspectRatio, ctx.width, ctx.height)
+      //ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
     }
 
   }
