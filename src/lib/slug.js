081ef6b8dcbc2f2e0a3e597b8c50e5479df1e7c2
vencax <vencax@centrum.cz>
2018-02-17 08:31:41 +0100

    slugformatter configurable

--- src/lib/slug.js
+++ src/lib/slug.js
@@ -1,4 +1,15 @@
-import slug from 'slug';
+import registry from './registry';
+
+const SLUGFORMATTERKEY = 'slugformatter';
+
+function defaultSlugFormatter (value) {
+  return value;
+}
+
+function slug (value) {
+  const registerred = registry.getWidgetValueSerializer(SLUGFORMATTERKEY);
+  return registerred ? registerred(value) : defaultSlugFormatter(value);
+}
 
 export function toSlug (str) {
   return slug(str.trim(), {lower: true});
