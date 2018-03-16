f2ee2776e74123ee491b9be70327afc3a6cbd42c
talves <tony@3alves.com>
2018-03-16 16:37:09 -0700

    allow for single bundle init

--- src/index.js
+++ src/index.js
@@ -3,23 +3,47 @@
  * the `window` object.
  */
 import React from 'react';
-import bootstrap from './bootstrap';
+import bootstrap, { ROOT_ID } from './bootstrap';
 import registry from 'Lib/registry';
 import createReactClass from 'create-react-class';
 
+let initialized = false;
+
+/**
+ * Allow init of the CMS.
+ */
+function init(opts = {}) {
+  if (initialized) {
+    console.error('Bootstrap attempted, but Netlify CMS is already initialized!');
+    return;
+  }
+  initialized = bootstrap(opts);
+  return initialized;
+}
+
+/**
+ * Allow reset of the CMS to allow render after unmount
+ */
+function reset() {
+  initialized = false;
+}
+
 const CMS = (function startApp() {
   /**
-   * Load the app, bail if bootstrapping fails. This will most likely occur
-   * if both automatic and manual initialization are attempted.
+   * Load the app, bail if root element exists or no-auto element.
+   * Use <div id="no-auto" /> in your app if you are not persisting the root element.
+   *  (in example: as a React Route)
    */
-  if (!bootstrap()) {
-    return;
+  if (document.getElementById('no-auto') || document.getElementById(ROOT_ID)) {
+    console.log('CMS is running in manual mode. [using init() to initialize]');
+  } else {
+    init();
   }
 
   /**
    * Add extension hooks to global scope.
    */
-  const api = { ...registry };
+  const api = { init, reset, ...registry };
   if (typeof window !== 'undefined') {
     window.CMS = api;
     window.createClass = window.createClass || createReactClass;
@@ -32,3 +56,8 @@ const CMS = (function startApp() {
  * Export the registry for projects that import the CMS.
  */
 export default CMS;
+
+/**
+ * Export the init, reset and registry standalone. (optional)
+ */
+export { init, reset, registry };
