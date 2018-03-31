e21359b54a13093fbdad10524f8d5f4d0c78ccf0
Caleb <inchristalway-tech4him@yahoo.com>
2018-03-31 07:36:36 -0600

    Drop implicit parsing.
    The types should serialize to a string by default.

--- src/formats/yaml.js
+++ src/formats/yaml.js
@@ -1,39 +1,5 @@
 import yaml from 'js-yaml';
-import moment from 'moment';
-import AssetProxy from 'ValueObjects/AssetProxy';
 import { sortKeys } from './helpers';
-
-const MomentType = new yaml.Type('date', {
-  kind: 'scalar',
-  predicate(value) {
-    return moment.isMoment(value);
-  },
-  represent(value) {
-    return value.format(value._f);
-  },
-  resolve(value) {
-    return moment.isMoment(value) && value._f;
-  },
-});
-
-const ImageType = new yaml.Type('image', {
-  kind: 'scalar',
-  instanceOf: AssetProxy,
-  represent(value) {
-    return `${ value.path }`;
-  },
-  resolve(value) {
-    if (value === null) return false;
-    if (value instanceof AssetProxy) return true;
-    return false;
-  },
-});
-
-
-const OutputSchema = new yaml.Schema({
-  include: [yaml.CORE_SCHEMA],
-  implicit: [MomentType, ImageType],
-});
 
 export default {
   fromFile(content) {
@@ -41,6 +7,6 @@ export default {
   },
 
   toFile(data, sortedKeys = []) {
-    return yaml.safeDump(data, { schema: OutputSchema, sortKeys: sortKeys(sortedKeys) });
+    return yaml.safeDump(data, { schema: yaml.CORE_SCHEMA, sortKeys: sortKeys(sortedKeys) });
   }
 }
