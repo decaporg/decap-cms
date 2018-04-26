a361f85ba5ba0006cfbf7899c7b686f384f89065
Damien Duhamel <d.damien@live.fr>
2018-04-26 22:23:44 +0200

    Add and removes icons

--- src/components/UI/Icon/images/_index.js
+++ src/components/UI/Icon/images/_index.js
@@ -35,6 +35,8 @@ import iconQuote from './quote.svg';
 import iconScroll from './scroll.svg';
 import iconSearch from './search.svg';
 import iconSettings from './settings.svg';
+import iconStrikethrough from './strikethrough.svg';
+import iconText from './text.svg';
 import iconUser from './user.svg';
 import iconWorkflow from './workflow.svg';
 import iconWrite from './write.svg';
@@ -77,6 +79,8 @@ const images = {
   'scroll': iconScroll,
   'search': iconSearch,
   'settings': iconSettings,
+  'text': iconText,
+  'strikethrough': iconStrikethrough,
   'user': iconUser,
   'workflow': iconWorkflow,
   'write': iconWrite,
