95facb39c184659227d46c06bdc467da4d200bdc
Zeb Pykosz <zeb.apy@gmail.com>
2018-05-12 18:23:46 -0400

    website: use react-headroom to add sticky nav functionality

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'gatsby-link';
 import classnames from 'classnames';
+import Headroom from 'react-headroom';
 
 import DocSearch from './docsearch';
 
@@ -10,45 +11,47 @@ const Header = ({ location }) => {
   const isDocs = location.pathname.includes('docs');
 
   return (
-    <header
-      id="header"
-      className={classnames({
-        docs: isDocs
-      })}
-    >
-      <div className="contained">
-        <div className="logo-container">
-          <Link to="/" className="logo">
-            <img src={logo} />
-          </Link>
-          {isDocs && <DocSearch />}
+    <Headroom>
+      <header
+        id="header"
+        className={classnames({
+          docs: isDocs
+        })}
+      >
+        <div className="contained">
+          <div className="logo-container">
+            <Link to="/" className="logo">
+              <img src={logo} />
+            </Link>
+            {isDocs && <DocSearch />}
+          </div>
+          <div className="nav-container">
+            <Link className="nav-link docs-link" to="/docs/intro">
+              Docs
+            </Link>
+            <Link
+              className="nav-link contributing-link"
+              to="/docs/contributor-guide"
+            >
+              Contributing
+            </Link>
+            <Link className="nav-link" to="/community">
+              Community
+            </Link>
+            <a
+              id="ghstars"
+              className="github-button"
+              href="https://github.com/netlify/netlify-cms"
+              data-icon="octicon-star"
+              data-show-count="true"
+              aria-label="Star netlify/netlify-cms on GitHub"
+            >
+              Star
+            </a>
+          </div>
         </div>
-        <div className="nav-container">
-          <Link className="nav-link docs-link" to="/docs/intro">
-            Docs
-          </Link>
-          <Link
-            className="nav-link contributing-link"
-            to="/docs/contributor-guide"
-          >
-            Contributing
-          </Link>
-          <Link className="nav-link" to="/community">
-            Community
-          </Link>
-          <a
-            id="ghstars"
-            className="github-button"
-            href="https://github.com/netlify/netlify-cms"
-            data-icon="octicon-star"
-            data-show-count="true"
-            aria-label="Star netlify/netlify-cms on GitHub"
-          >
-            Star
-          </a>
-        </div>
-      </div>
-    </header>
+      </header>
+    </Headroom>
   );
 };
 
