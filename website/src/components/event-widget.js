2c9c2b1cd2c3267ccb908380f3c22ee2d9a16fbc
Zeb Pykosz <zeb.apy@gmail.com>
2018-05-19 16:53:16 -0400

    revert event widget to use XMLHttpRequest since fetch is not polyfilled

--- website/src/components/event-widget.js
+++ website/src/components/event-widget.js
@@ -3,11 +3,13 @@ import moment from 'moment';
 
 class EventWidget extends Component {
   state = {
+    errorMessage: '',
     loading: false,
     eventDate: ''
   };
 
   componentDidMount() {
+    const eventRequest = new XMLHttpRequest();
     const eventbriteToken = 'C5PX65CJBVIXWWLNFKLO';
     const eventbriteOrganiser = '14281996019';
 
@@ -17,29 +19,41 @@ class EventWidget extends Component {
       loading: true
     });
 
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
+    eventRequest.open('GET', url, true);
+
+    eventRequest.onload = () => {
+      if (eventRequest.status >= 200 && eventRequest.status < 400) {
+        // Success!
+        const data = JSON.parse(eventRequest.responseText);
+
         const eventDate = data.events[0].start.utc;
 
         this.setState({
           loading: false,
           eventDate
         });
-      })
-      .catch(err => {
-        console.log(err);
-        // TODO: set state to show error message
-        this.setState({
-          loading: false
-        });
+      }
+    };
+
+    eventRequest.onerror = () => {
+      this.setState({
+        errorMessage:
+          'The event info could not be loaded at this time, please try again later.',
+        loading: false
       });
+    };
+
+    eventRequest.send();
   }
   render() {
-    const { loading, eventDate } = this.state;
+    const { errorMessage, loading, eventDate } = this.state;
 
     if (loading) {
       return <span>Loading...</span>;
+    }
+
+    if (errorMessage) {
+      return <span>{errorMessage}</span>;
     }
 
     const eventDateMoment = moment(eventDate);
