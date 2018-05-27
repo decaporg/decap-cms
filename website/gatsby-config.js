754139d1813613ce6a6864baa265dcdc43443571
Zeb Pykosz <zeb.apy@gmail.com>
2018-05-27 12:38:23 -0400

    remove unused gatsby plugin comments

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -64,23 +64,10 @@ module.exports = {
     {
       resolve: 'gatsby-transformer-remark',
       options: {
+        // prettier-ignore
         plugins: [
           'gatsby-remark-autolink-headers',
-          // {
-          //   resolve: `gatsby-remark-images`,
-          //   options: {
-          //     maxWidth: 590
-          //   }
-          // },
-          // {
-          //   resolve: `gatsby-remark-responsive-iframe`,
-          //   options: {
-          //     wrapperStyle: `margin-bottom: 1.0725rem`
-          //   }
-          // },
           'gatsby-remark-prismjs'
-          // 'gatsby-remark-copy-linked-files',
-          // 'gatsby-remark-smartypants'
         ]
       }
     },
@@ -90,10 +77,6 @@ module.exports = {
         postCssPlugins
       }
     },
-    // `gatsby-transformer-sharp`,
-    // `gatsby-plugin-sharp`,
-    // `gatsby-plugin-feed`,
-    // `gatsby-plugin-offline`,
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-next',
     'gatsby-plugin-catch-links',
