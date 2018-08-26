9164b81b182751b275a73010aeb2d02e737c78ec
Tony Zhang <tonyz@uber.com>
2018-08-25 20:08:33 -0700

    Change to warn

--- packages/netlify-cms-lib-util/src/localForage.js
+++ packages/netlify-cms-lib-util/src/localForage.js
@@ -10,7 +10,7 @@ function localForageTest() {
     .catch(err => {
       if (err.code === 22) {
         const message = 'Unable to set localStorage key. Quota exceeded! Full disk?';
-        console.log(message);
+        console.warn(message);
       }
       console.log(err);
     });
