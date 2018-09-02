f03ee016a7d70b81a756e98d85bc8feb93e91c8f
Zeb Pykosz <zeb.apy@gmail.com>
2018-09-02 17:45:59 -0400

    use single quotes

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -80,7 +80,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'NetlifyCMS',
         short_name: 'NetlifyCMS',
