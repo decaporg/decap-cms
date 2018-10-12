f8683ce5241230cb4bd7e2a8faee96bc4a29f6fc
Rudolf Rakos <rudolf.rakos@zivver.com>
2018-10-12 19:08:57 +0200

    GitLab - Editorial Workflow

--- packages/netlify-cms-backend-gitlab/src/implementation.js
+++ packages/netlify-cms-backend-gitlab/src/implementation.js
@@ -3,6 +3,7 @@ import semaphore from 'semaphore';
 import { CURSOR_COMPATIBILITY_SYMBOL } from 'netlify-cms-lib-util';
 import AuthenticationPage from './AuthenticationPage';
 import API from './API';
+import { CMS_BRANCH_PREFIX } from './API';
 
 const MAX_CONCURRENT_DOWNLOADS = 10;
 
@@ -15,10 +16,6 @@ export default class GitLab {
       ...options,
     };
 
-    if (this.options.useWorkflow) {
-      throw new Error('The GitLab backend does not support the Editorial Workflow.');
-    }
-
     if (!this.options.proxied && config.getIn(['backend', 'repo']) == null) {
       throw new Error('The GitLab backend needs a "repo" in the backend configuration.');
     }
@@ -29,6 +26,7 @@ export default class GitLab {
     this.branch = config.getIn(['backend', 'branch'], 'master');
     this.api_root = config.getIn(['backend', 'api_root'], 'https://gitlab.com/api/v4');
     this.token = '';
+    this.squash_merges = config.getIn(['backend', 'squash_merges']);
   }
 
   authComponent() {
@@ -46,6 +44,8 @@ export default class GitLab {
       branch: this.branch,
       repo: this.repo,
       api_root: this.api_root,
+      squash_merges: this.squash_merges,
+      initialWorkflowStatus: this.options.initialWorkflowStatus,
     });
     return this.api.user().then(user =>
       this.api.hasWriteAccess(user).then(isCollab => {
@@ -68,21 +68,31 @@ export default class GitLab {
   }
 
   entriesByFolder(collection, extension) {
-    return this.api.listFiles(collection.get('folder')).then(({ files, cursor }) =>
-      this.fetchFiles(files.filter(file => file.name.endsWith('.' + extension))).then(
-        fetchedFiles => {
+    return this.api
+      .listFiles(collection.get('folder'))
+      .then(({ files, cursor }) => ({
+        files: files.filter(file => file.name.endsWith('.' + extension)),
+        cursor,
+      }))
+      .then(({ files, cursor }) =>
+        this.fetchFiles(files).then(fetchedFiles => {
           const returnedFiles = fetchedFiles;
           returnedFiles[CURSOR_COMPATIBILITY_SYMBOL] = cursor;
           return returnedFiles;
-        },
-      ),
-    );
+        }),
+      );
   }
 
   allEntriesByFolder(collection, extension) {
     return this.api
       .listAllFiles(collection.get('folder'))
-      .then(files => this.fetchFiles(files.filter(file => file.name.endsWith('.' + extension))));
+      .then(files => files.filter(file => file.name.endsWith('.' + extension)))
+      .then(files =>
+        this.fetchFiles(files).then(fetchedFiles => {
+          const returnedFiles = fetchedFiles;
+          return returnedFiles;
+        }),
+      );
   }
 
   entriesByFiles(collection) {
@@ -107,13 +117,12 @@ export default class GitLab {
               .readFile(file.path, file.id)
               .then(data => {
                 resolve({ file, data });
-                sem.leave();
               })
               .catch((error = true) => {
-                sem.leave();
                 console.error(`failed to load file from GitLab: ${file.path}`);
                 resolve({ error });
-              }),
+              })
+              .finally(() => sem.leave()),
           ),
         ),
       );
@@ -133,7 +142,6 @@ export default class GitLab {
 
   getMedia() {
     const sem = semaphore(MAX_CONCURRENT_DOWNLOADS);
-
     return this.api.listAllFiles(this.config.get('media_folder')).then(files =>
       files.map(({ id, name, path }) => {
         const getBlobPromise = () =>
@@ -145,24 +153,23 @@ export default class GitLab {
                 .finally(() => sem.leave()),
             ),
           );
-
         return { id, name, getBlobPromise, path };
       }),
     );
   }
 
-  async persistEntry(entry, mediaFiles, options = {}) {
-    return this.api.persistFiles([entry], options);
+  persistEntry(entry, mediaFiles = [], options = {}) {
+    return this.api.persistFiles(entry, mediaFiles, options);
   }
 
   async persistMedia(mediaFile, options = {}) {
-    await this.api.persistFiles([mediaFile], options);
+    await this.api.persistFiles(null, [mediaFile], options);
     const { value, path, fileObj } = mediaFile;
     const getBlobPromise = () => Promise.resolve(fileObj);
     return { name: value, size: fileObj.size, getBlobPromise, path: trimStart(path, '/') };
   }
 
-  deleteFile(path, commitMessage, options) {
+  deleteFile(path, commitMessage, options = {}) {
     return this.api.deleteFile(path, commitMessage, options);
   }
 
@@ -173,5 +180,62 @@ export default class GitLab {
       ),
       cursor: newCursor,
     }));
+  }
+
+  // Editorial Workflow
+
+  unpublishedEntries() {
+    return this.api.listUnpublishedBranches().then(branches => {
+      const sem = semaphore(MAX_CONCURRENT_DOWNLOADS);
+      const promises = [];
+      branches.forEach(branch => {
+        promises.push(
+          new Promise(resolve => {
+            // Strip the `CMS_BRANCH_PREFIX` from the `branch.name` to get the `slug`.
+            const slug = branch.name.split(CMS_BRANCH_PREFIX).pop();
+            return sem.take(() =>
+              this.unpublishedEntry(null, slug)
+                .then(resolve)
+                .catch((error = true) => {
+                  console.error(`failed to load unpublished file from GitLab: ${slug}`);
+                  resolve({ error });
+                })
+                .finally(() => sem.leave()),
+            );
+          }),
+        );
+      });
+      return Promise.all(promises).then(loadedEntries =>
+        loadedEntries.filter(loadedEntry => !loadedEntry.error),
+      );
+    });
+  }
+
+  unpublishedEntry(collection, slug) {
+    return this.api.readUnpublishedBranchFile(slug).then(data => {
+      if (!data) {
+        return null;
+      } else {
+        return {
+          slug,
+          file: { path: data.metaData.objects.entry.path },
+          data: data.fileData,
+          metaData: data.metaData,
+          isModification: data.isModification,
+        };
+      }
+    });
+  }
+
+  updateUnpublishedEntryStatus(collection, slug, newStatus) {
+    return this.api.updateUnpublishedEntryStatus(collection, slug, newStatus);
+  }
+
+  deleteUnpublishedEntry(collection, slug) {
+    return this.api.deleteUnpublishedEntry(collection, slug);
+  }
+
+  publishUnpublishedEntry(collection, slug) {
+    return this.api.publishUnpublishedEntry(collection, slug);
   }
 }
