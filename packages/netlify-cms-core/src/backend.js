33e20ad751fea5e456d152f86e744943d448c4f8
Ranie Santos <raniesantos@users.noreply.github.com>
2018-10-24 10:24:17 +0800

    Remove single quotes before creating slug (#1781)

--- packages/netlify-cms-core/src/backend.js
+++ packages/netlify-cms-core/src/backend.js
@@ -69,6 +69,9 @@ const slugFormatter = (collection, entryData, slugConfig) => {
     // Convert slug to lower-case
     .toLocaleLowerCase()
 
+    // Remove single quotes.
+    .replace(/[']/g, '')
+
     // Replace periods with dashes.
     .replace(/[.]/g, '-');
 
