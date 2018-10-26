0b661eb0b09a13ebc1305342748d607b44a909ca
Rudolf Rakos <rudolf.rakos@zivver.com>
2018-10-26 16:37:52 +0200

    GitLab - Editorial Workflow Branches - Included `collection` and `slug` in branch name, to avoid conflicts between collections Merge Requests - Changes to the branch are allowed, so HEAD is not checked against metadata Edge - Replaced `finally` with `then` and `recover`, as it is not (yet) supported

--- packages/netlify-cms-backend-gitlab/src/implementation.js
+++ packages/netlify-cms-backend-gitlab/src/implementation.js
@@ -1,9 +1,9 @@
+import { Map } from 'immutable';
 import trimStart from 'lodash/trimStart';
 import semaphore from 'semaphore';
 import { CURSOR_COMPATIBILITY_SYMBOL } from 'netlify-cms-lib-util';
 import AuthenticationPage from './AuthenticationPage';
 import API from './API';
-import { CMS_BRANCH_PREFIX } from './API';
 
 const MAX_CONCURRENT_DOWNLOADS = 10;
 
@@ -117,12 +117,13 @@ export default class GitLab {
               .readFile(file.path, file.id)
               .then(data => {
                 resolve({ file, data });
+                sem.leave();
               })
               .catch((error = true) => {
                 console.error(`failed to load file from GitLab: ${file.path}`);
                 resolve({ error });
-              })
-              .finally(() => sem.leave()),
+                sem.leave();
+              }),
           ),
         ),
       );
@@ -149,8 +150,14 @@ export default class GitLab {
             sem.take(() =>
               this.api
                 .readFile(path, id, { parseText: false })
-                .then(resolve, reject)
-                .finally(() => sem.leave()),
+                .then(data => {
+                  resolve(data);
+                  sem.leave();
+                })
+                .catch(error => {
+                  reject(error);
+                  sem.leave();
+                }),
             ),
           );
         return { id, name, getBlobPromise, path };
@@ -191,16 +198,20 @@ export default class GitLab {
       branches.forEach(branch => {
         promises.push(
           new Promise(resolve => {
-            // Strip the `CMS_BRANCH_PREFIX` from the `branch.name` to get the `slug`.
-            const slug = branch.name.split(CMS_BRANCH_PREFIX).pop();
+            const [collection, slug] = this.api.deconstructBranchName(branch.name);
             return sem.take(() =>
-              this.unpublishedEntry(null, slug)
-                .then(resolve)
-                .catch((error = true) => {
-                  console.error(`failed to load unpublished file from GitLab: ${slug}`);
-                  resolve({ error });
+              this.unpublishedEntry(collection, slug)
+                .then(data => {
+                  resolve(data);
+                  sem.leave();
                 })
-                .finally(() => sem.leave()),
+                .catch((error = true) => {
+                  console.error(
+                    `failed to load unpublished file from GitLab: ${collection}/${slug}`,
+                  );
+                  resolve({ error });
+                  sem.leave();
+                }),
             );
           }),
         );
@@ -212,7 +223,9 @@ export default class GitLab {
   }
 
   unpublishedEntry(collection, slug) {
-    return this.api.readUnpublishedBranchFile(slug).then(data => {
+    // NOTE - rudolf - It looks like `collection` could be `Map` or `String`.
+    const collectionName = Map.isMap(collection) ? collection.get('name') : collection;
+    return this.api.readUnpublishedBranchFile(collectionName, slug).then(data => {
       if (!data) {
         return null;
       } else {
