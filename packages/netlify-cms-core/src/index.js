06ffbc1bc3b619d056b7881a122b7cd2aab556d7
Loïc Mahieu <mahieuloic@gmail.com>
2018-11-09 13:22:04 +0100

    Move window/UMD bridge in `netlify-cms` instead of `netlify-cms-core`
    `netlify-cms` package is responsible to make NetlifyCMS easy to use. It is bundled with the UMD wrapper and is used as script tag. It imports all widgets and backends...
    It makes sense that `netlify-cms` should handle whole aspects about this "window bridge"... and keep `netlify-cms-core` as a clean ESM module.
    
    It will make `netlify-cms-core` easy to use as a standalone ESM module:
    ```
    import { init } from "netlify-cms-core"
    init({})
    ```
    
    Without this PR, a `window.CMS_MANUAL_INIT` should be set before the `import` is done.

--- packages/netlify-cms-core/src/index.js
+++ packages/netlify-cms-core/src/index.js
@@ -1,25 +1,4 @@
-import React from 'react';
 import bootstrap from './bootstrap';
 import registry from 'Lib/registry';
-import createReactClass from 'create-react-class';
-
-/**
- * Load Netlify CMS automatically if `window.CMS_MANUAL_INIT` is set.
- */
-if (!window.CMS_MANUAL_INIT) {
-  bootstrap();
-} else {
-  console.log('`window.CMS_MANUAL_INIT` flag set, skipping automatic initialization.');
-}
-
-/**
- * Add extension hooks to global scope.
- */
-if (typeof window !== 'undefined') {
-  window.CMS = registry;
-  window.initCMS = bootstrap;
-  window.createClass = window.createClass || createReactClass;
-  window.h = window.h || React.createElement;
-}
 
 export { registry as default, bootstrap as init };
--- packages/netlify-cms/src/index.js
+++ packages/netlify-cms/src/index.js
@@ -1,7 +1,28 @@
+import createReactClass from 'create-react-class';
+import React from 'react';
 import CMS, { init } from 'netlify-cms-core/src';
 import './backends';
 import './widgets';
 import './editor-components';
 import './media-libraries';
+
+/**
+ * Load Netlify CMS automatically if `window.CMS_MANUAL_INIT` is set.
+ */
+if (!window.CMS_MANUAL_INIT) {
+  init();
+} else {
+  console.log('`window.CMS_MANUAL_INIT` flag set, skipping automatic initialization.');
+}
+
+/**
+ * Add extension hooks to global scope.
+ */
+if (typeof window !== 'undefined') {
+  window.CMS = CMS;
+  window.initCMS = init;
+  window.createClass = window.createClass || createReactClass;
+  window.h = window.h || React.createElement;
+}
 
 export { CMS as default, init };
