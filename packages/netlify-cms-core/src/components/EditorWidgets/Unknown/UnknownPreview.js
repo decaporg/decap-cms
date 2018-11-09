b1a7e006f6af2b4e608e9028a4320cbecfcc7cc0
Bartholomew <mackintosh02@gmail.com>
2018-11-09 16:17:55 +0100

    fix(i18n): fix missing widget placeholder (#1861)

--- packages/netlify-cms-core/src/components/EditorWidgets/Unknown/UnknownPreview.js
+++ packages/netlify-cms-core/src/components/EditorWidgets/Unknown/UnknownPreview.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 const UnknownPreview = ({ field, t }) => {
   return (
     <div className="nc-widgetPreview">
-      {t('editor.editorWidgets.unknownPreview.noPreview', field.get('widget'))}
+      {t('editor.editorWidgets.unknownPreview.noPreview', { widget: field.get('widget') })}
     </div>
   );
 };
