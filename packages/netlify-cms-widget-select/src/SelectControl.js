3619c1bedb84e5c90c793cc0d9c9ea70e8e9fefe
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-11-22 20:35:28 +0100

    feat(netlify-cms-widget-select): add support for multiple selected items

--- packages/netlify-cms-widget-select/src/SelectControl.js
+++ packages/netlify-cms-widget-select/src/SelectControl.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import { Map } from 'immutable';
+import { Map, fromJS } from 'immutable';
 import { find } from 'lodash';
 import Select from 'react-select';
 import { colors } from 'netlify-cms-ui-default';
 
 const styles = {
-  control: provided => ({
-    ...provided,
+  control: styles => ({
+    ...styles,
     border: 0,
     boxShadow: 'none',
     padding: '9px 0 9px 12px',
   }),
-  option: (provided, state) => ({
-    ...provided,
+  option: (styles, state) => ({
+    ...styles,
     backgroundColor: state.isSelected
       ? `${colors.active}`
       : state.isFocused
@@ -22,10 +22,31 @@ const styles = {
         : 'transparent',
     paddingLeft: '22px',
   }),
-  menu: provided => ({ ...provided, right: 0 }),
-  container: provided => ({ ...provided, padding: '0 !important' }),
-  indicatorSeparator: () => ({ display: 'none' }),
-  dropdownIndicator: provided => ({ ...provided, color: `${colors.controlLabel}` }),
+  menu: styles => ({ ...styles, right: 0 }),
+  container: styles => ({ ...styles, padding: '0 !important' }),
+  indicatorSeparator: (styles, state) =>
+    state.hasValue && state.selectProps.isClearable
+      ? { ...styles, backgroundColor: `${colors.textFieldBorder}` }
+      : { display: 'none' },
+  dropdownIndicator: styles => ({ ...styles, color: `${colors.controlLabel}` }),
+  clearIndicator: styles => ({ ...styles, color: `${colors.controlLabel}` }),
+  multiValue: styles => ({
+    ...styles,
+    backgroundColor: colors.background,
+  }),
+  multiValueLabel: styles => ({
+    ...styles,
+    color: colors.textLead,
+    fontWeight: 500,
+  }),
+  multiValueRemove: styles => ({
+    ...styles,
+    color: colors.controlLabel,
+    [':hover']: {
+      color: colors.errorText,
+      backgroundColor: colors.errorBackground,
+    },
+  }),
 };
 
 export default class SelectControl extends React.Component {
@@ -54,19 +75,25 @@ export default class SelectControl extends React.Component {
   };
 
   handleChange = selectedOption => {
-    this.props.onChange(selectedOption['value']);
+    const { field, onChange } = this.props;
+
+    if (field.get('multiple')) {
+      onChange(fromJS(selectedOption));
+    } else {
+      onChange(selectedOption);
+    }
   };
 
   render() {
     const { field, value, forID, classNameWrapper, setActiveStyle, setInactiveStyle } = this.props;
     const fieldOptions = field.get('options');
+    const isMulti = field.get('multiple');
 
     if (!fieldOptions) {
       return <div>Error rendering select control for {field.get('name')}: No options</div>;
     }
 
     const options = [
-      ...(field.get('default', false) ? [] : [{ label: '', value: '' }]),
       ...fieldOptions.map(option => {
         if (typeof option === 'string') {
           return { label: option, value: option };
@@ -87,6 +114,9 @@ export default class SelectControl extends React.Component {
         onBlur={setInactiveStyle}
         options={options}
         styles={styles}
+        isMulti={isMulti}
+        placeholder=""
+        isClearable
       />
     );
   }
