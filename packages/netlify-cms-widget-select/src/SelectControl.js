955f94f72ba4d01473ba20f9e0bcf88e1e1467b9
Alexander Nanberg <alex.nanberg@hotmail.com>
2018-11-26 17:56:41 +0100

    fix(a11y): correct label "for" references to fields (#1904)

--- packages/netlify-cms-widget-select/src/SelectControl.js
+++ packages/netlify-cms-widget-select/src/SelectControl.js
@@ -79,7 +79,7 @@ export default class SelectControl extends React.Component {
 
     return (
       <Select
-        id={forID}
+        inputId={forID}
         value={selectedValue}
         onChange={this.handleChange}
         className={classNameWrapper}
