885881f7a87bb623dc8043b9312ee848254d38ec
smnh <semenh@gmail.com>
2018-12-13 12:50:06 +0200

    rolled back changes to ObjectPreview.js

--- packages/netlify-cms-widget-object/src/ObjectPreview.js
+++ packages/netlify-cms-widget-object/src/ObjectPreview.js
@@ -4,8 +4,7 @@ import { WidgetPreviewContainer } from 'netlify-cms-ui-default';
 
 const ObjectPreview = ({ field }) => (
   <WidgetPreviewContainer>
-    <div>{field.get('label', field.get('name'))}</div>
-    {(field && (field.get('types') || field.get('fields') || field.get('field'))) || null}
+    {(field && field.get('fields')) || field.get('field') || null}
   </WidgetPreviewContainer>
 );
 
