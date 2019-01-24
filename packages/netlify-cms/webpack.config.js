95b109526cc8b7cadca0f8e9e50cca044ba5a793
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-01-24 05:16:50 -0200

    :sparkles: exclude non-used widgets on build
    via webpack-conditional-loader

--- packages/netlify-cms/webpack.config.js
+++ packages/netlify-cms/webpack.config.js
@@ -4,14 +4,29 @@ const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const pkg = require('./package.json');
 const { plugins } = require('../../scripts/webpack');
 const coreWebpackConfig = require('../netlify-cms-core/webpack.config.js');
-
 const isProduction = process.env.NODE_ENV === 'production';
 const { PORT = 8080, HOST = 'localhost' } = process.env;
+
+const castArray = x => (Array.isArray(x) ? x : [x]).filter(a => a);
+
+const addConditionalLoader = rules =>
+  rules.map(rule => {
+    if (!isProduction) return rule;
+    if (!rule.test) return rule;
+    if (!(rule.test instanceof RegExp)) return rule;
+    if (!rule.test.test('.js')) return rule;
+    const use = castArray(rule.use).concat('webpack-conditional-loader');
+    return { ...rule, use };
+  });
 
 const baseConfig = {
   ...coreWebpackConfig,
   context: path.join(__dirname, 'src'),
   entry: './index.js',
+  module: {
+    ...coreWebpackConfig.module,
+    rules: addConditionalLoader(coreWebpackConfig.module.rules),
+  },
   plugins: [
     ...Object.entries(plugins)
       .filter(([key]) => key !== 'friendlyErrors')
@@ -32,6 +47,18 @@ const baseConfig = {
     quiet: true,
     host: HOST,
     port: PORT,
+  },
+  stats: {
+    builtAt: false,
+    chunks: false,
+    colors: true,
+    entrypoints: false,
+    errorDetails: false,
+    hash: false,
+    modules: false,
+    timings: false,
+    version: false,
+    warnings: false,
   },
 };
 
