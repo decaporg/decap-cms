c99c9665969717bb7a6ea815bf320808bce61be0
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-01-27 06:08:16 -0200

    :package: fix netlify-cms-entry-loader build

--- packages/netlify-cms-entry-loader/webpack.config.js
+++ packages/netlify-cms-entry-loader/webpack.config.js
@@ -1,3 +1,29 @@
-const { getConfig } = require('../../scripts/webpack.js');
+const path = require('path');
+const { getConfig, rules } = require('../../scripts/webpack.js');
 
-module.exports = getConfig();
+const baseConfig = getConfig();
+const corePath = path.resolve(__dirname, '../netlify-cms-core');
+
+module.exports = {
+  ...baseConfig,
+  context: path.join(__dirname, 'src'),
+  entry: ['./index.js'],
+  module: {
+    rules: [
+      ...Object.entries(rules)
+        .filter(([key]) => key !== 'js')
+        .map(([, rule]) => rule()),
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            configFile: path.join(corePath, 'babel.config.js'),
+          },
+        },
+      },
+    ],
+  },
+  plugins: baseConfig.plugins,
+};
