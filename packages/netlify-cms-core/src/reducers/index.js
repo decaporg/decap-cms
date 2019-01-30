cc7826b97184e26c0b46c677e6aaa83282a86ae1
Thomas Burke <tom@intelesense.net>
2019-01-30 11:29:32 +0000

    Sorry, forgot to yarn format

--- packages/netlify-cms-core/src/reducers/index.js
+++ packages/netlify-cms-core/src/reducers/index.js
@@ -61,10 +61,12 @@ export const getAsset = (state, path) => {
    * If an external media library is in use, just return the path.
    */
   if (state.mediaLibrary.get('externalLibrary')) {
-    const mediaLibraryConfig = state.config.get("media_library");
-    if(mediaLibraryConfig.get("output_filename_only") && mediaLibraryConfig.get("display_base_url"))
-    {
-      return mediaLibraryConfig.get("display_base_url") + path;
+    const mediaLibraryConfig = state.config.get('media_library');
+    if (
+      mediaLibraryConfig.get('output_filename_only') &&
+      mediaLibraryConfig.get('display_base_url')
+    ) {
+      return mediaLibraryConfig.get('display_base_url') + path;
     }
     return path;
   }
