3e80aa307b4a3cf7e0450d3ec6063ea2d2a52cc2
talves <tony@3alves.com>
2019-03-05 08:21:23 -0800

    default exports library for peers
    - The library needed to have named exports using umd in scripts

---
+++ packages/netlify-cms-default-exports/src/index.js
@@ -0,0 +1,23 @@
+import * as Emotion from 'emotion';
+import Immutable from 'immutable';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import Lodash from 'lodash';
+import Moment from 'moment';
+import PropTypes from 'prop-types';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as ReactEmotion from 'react-emotion/src/index';
+import * as UUId from 'uuid';
+
+export default {
+  Emotion,
+  Immutable,
+  ImmutablePropTypes,
+  Lodash,
+  Moment,
+  PropTypes,
+  React,
+  ReactDOM,
+  ReactEmotion,
+  UUId,
+};
