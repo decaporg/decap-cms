e783e2ace30727741786701996073ab11ec7ee28
talves <tony@3alves.com>
2019-03-28 08:11:11 -0700

    not needed config plugins and devServer

--- packages/netlify-cms-app/webpack.config.js
+++ packages/netlify-cms-app/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const pkg = require('./package.json');
 const { getConfig, plugins } = require('../../scripts/webpack');
 const baseWebpackConfig = getConfig({ baseOnly: true });
@@ -19,21 +18,8 @@ const baseConfig = {
     new webpack.DefinePlugin({
       NETLIFY_CMS_VERSION: JSON.stringify(`- app - ${pkg.version}${isProduction ? '' : '-dev'}`),
       NETLIFY_CMS_CORE_VERSION: null,
-    }),
-    new FriendlyErrorsWebpackPlugin({
-      compilationSuccessInfo: {
-        messages: ['Netlify CMS is now running at http://localhost:8080'],
-      },
-    }),
-  ],
-  devServer: {
-    contentBase: '../../dev-test',
-    watchContentBase: true,
-    publicPath: '/dist/',
-    quiet: true,
-    host: 'localhost',
-    port: 8080,
-  },
+    })
+  ]
 };
 
 module.exports = baseConfig;
