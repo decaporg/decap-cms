f496815cf4f3c967c970a6792893ad591d9a354e
talves <tony@3alves.com>
2019-03-28 08:07:07 -0700

    duplicate registry

--- packages/netlify-cms/src/index.js
+++ packages/netlify-cms/src/index.js
@@ -1,7 +1,6 @@
 import createReactClass from 'create-react-class';
 import React from 'react';
 import { NetlifyCmsApp as CMS } from 'netlify-cms-app/dist/esm';
-import './media-libraries';
 
 /**
  * Load Netlify CMS automatically if `window.CMS_MANUAL_INIT` is set.
