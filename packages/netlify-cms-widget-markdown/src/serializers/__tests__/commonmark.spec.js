1405ec7d1e6854ea8dae4c1426554f28d8a47e27
Shawn Erquhart <shawn@erquh.art>
2019-05-21 14:06:09 -0400

    hold on commonmark work

--- packages/netlify-cms-widget-markdown/src/serializers/__tests__/commonmark.spec.js
+++ packages/netlify-cms-widget-markdown/src/serializers/__tests__/commonmark.spec.js
@@ -6,17 +6,38 @@ import { markdownToSlate, slateToMarkdown, markdownToHtml } from '../index.js';
 
 const skips = [
   {
-    number: [416, 417, 424, 425, 426, 431],
+    number: [456],
+    reason: 'Remark ¯\\_(ツ)_/¯',
+  },
+  {
+    number: [416, 417, 424, 425, 426, 431, 457, 460, 462, 464, 467],
     reason: 'Remark does not support infinite (redundant) nested marks',
   },
-  { number: 602, reason: 'Remark allows autolink emails to contain backslashes' },
-  { number: 599, reason: 'Remark does not escape all expected entities' },
-  { number: 593, reason: 'Remark removes "mailto:" from autolink text' },
-  { number: 589, reason: 'Remark does not honor backslash escape of image exclamation point' },
+  {
+    number: [455, 469, 470, 471],
+    reason: 'Remark parses the initial set of identical nested delimiters first',
+  },
+  {
+    number: [473, 476, 478, 480],
+    reason: 'we convert underscores to asterisks for strong/emphasis',
+  },
+  { number: 490, reason: 'Remark strips pointy enclosing pointy brackets from link url' },
+  { number: 503, reason: 'Remark allows non-breaking space between link url and title' },
+  { number: 507, reason: 'Remark allows a space between link alt and url' },
+  {
+    number: [
+      511, 516, 525, 528, 529, 530, 532, 533, 534, 540, 541, 542, 543, 546, 548, 560, 565, 567
+    ],
+    reason: 'we convert link references to standard links, but Remark also fails these',
+  },
   {
     number: [569, 570, 571, 572, 573, 581, 585],
     reason: 'Remark does not recognize or remove marks in image alt text',
   },
+  { number: 589, reason: 'Remark does not honor backslash escape of image exclamation point' },
+  { number: 593, reason: 'Remark removes "mailto:" from autolink text' },
+  { number: 599, reason: 'Remark does not escape all expected entities' },
+  { number: 602, reason: 'Remark allows autolink emails to contain backslashes' },
 ];
 
 const onlys = [
@@ -42,7 +63,7 @@ const parse = flow([markdownToSlate, slateToMarkdown]);
  * tests, of which we're passing about 300 as of introduction of this suite. To
  * work on improving Commonmark support, update __fixtures__/commonmarkExpected.json
  */
-describe('Commonmark support', function() {
+describe.skip('Commonmark support', function() {
   const specs = onlys.length > 0
     ? commonmarkSpec.filter(({ number }) => onlys.includes(number))
     : commonmarkSpec;
@@ -75,7 +96,7 @@ ${spec.html}
     if (skip) {
       const showMessage = Array.isArray(skip.number) ? skip.number[0] === spec.number : true;
       if (showMessage) {
-        console.log(`skipping spec ${skip.number} because ${skip.reason}.\n${specUrl}`);
+        //console.log(`skipping spec ${skip.number}\n${skip.reason}\n${specUrl}`);
       }
     }
     const testFn = skip ? test.skip : test;
