03a083e5fb70e9803374730293e3c1d32e88cf0d
WJXHenry <wjxhenry@gmail.com>
2019-06-13 16:24:19 -0600

    Break from loop on invalid subfield

--- packages/netlify-cms-core/src/backend.js
+++ packages/netlify-cms-core/src/backend.js
@@ -134,6 +134,8 @@ const extractSearchFields = searchFields => entry =>
     let f = entry.data;
     for (let i = 0; i < nestedFields.length; i++) {
       f = f[nestedFields[i]];
+      if (!f)
+        break;
     }
     return f ? `${acc} ${f}` : acc;
   }, '');
