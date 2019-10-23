c444ce47a845393e33ab0973c2e9f84874aa8008
erezrokah <erezrokah@users.noreply.github.com>
2019-10-23 12:45:52 +0300

    test(cleanup): remove/reintorduce skipped tests

--- packages/netlify-cms-widget-markdown/src/serializers/__tests__/commonmark.spec.js
+++ packages/netlify-cms-widget-markdown/src/serializers/__tests__/commonmark.spec.js
@@ -1,5 +1,6 @@
 import { flow, trim } from 'lodash';
 import commonmarkSpec from './__fixtures__/commonmark.json';
+import expected from './__fixtures__/commonmarkExpected.json';
 import { markdownToSlate, slateToMarkdown, markdownToHtml } from '../index.js';
 
 /**
@@ -18,13 +19,27 @@ const process = flow([markdownToSlate, slateToMarkdown, markdownToHtml]);
 /**
  * Passing this test suite requires 100% Commonmark compliance. There are 624
  * tests, of which we're passing about 300 as of introduction of this suite. To
- * work on improving Commonmark support, remove `.skip` from this `describe`
- * and run the test suite locally.
+ * work on improving Commonmark support, update __fixtures__/commonmarkExpected.json
  */
-describe.skip('Commonmark support', () => {
+
+describe('Commonmark support', () => {
   test.each(spec)('%s', (markdown, html) => {
     // We're trimming the html from the spec as they all have trailing newlines
     // and we never output trailing newlines. This may be a compliance issue.
-    expect(process(markdown)).toEqual(trim(html));
+    const trimmedHtml = trim(html);
+
+    switch (expected[markdown]) {
+      case 'TO_EQUAL':
+        expect(process(markdown)).toEqual(trimmedHtml);
+        break;
+      case 'NOT_TO_EQUAL':
+        expect(process(markdown)).not.toEqual(trimmedHtml);
+        break;
+      case 'TO_ERROR':
+        expect(() => process(markdown)).toThrowError();
+        break;
+      default:
+        throw new Error('Unknown expected type: ' + expected[markdown]);
+    }
   });
 });
