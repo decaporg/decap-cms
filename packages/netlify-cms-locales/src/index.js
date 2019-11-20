ea40fd49bd13d508975deb44d556ef265cc2b7ec
Tom Rutgers <trutgers01@hotmail.com>
2019-11-20 20:24:37 +0100

    feat(lang): add 'nl' locale

--- packages/netlify-cms-locales/src/index.js
+++ packages/netlify-cms-locales/src/index.js
@@ -3,3 +3,4 @@ export { default as de } from './de';
 export { default as fr } from './fr';
 export { default as es } from './es';
 export { default as uk } from './uk';
+export { default as nl } from './nl';
