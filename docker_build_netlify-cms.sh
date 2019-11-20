014f501f542f4f5a98428c75ff28b70c5f43e798
Ciaran Moran <25228551+morancj@users.noreply.github.com>
2019-11-20 12:48:32 +0000

    feat: remove old Dockerfile stuff

--- docker_build_netlify-cms.sh
+++ docker_build_netlify-cms.sh
@@ -16,11 +16,6 @@ CURRENT_DIR="$(pwd)"
 # DOCKER_WORKDIR="/usr/src/app"
 DOCKER_WORKDIR="${HOME}"
 
-# Build Docker Image
-# docker build --compress --memory 4GB --pull --rm --tag "${DOCKER_IMAGE_PREFIX}"/"${DOCKER_IMAGE_NAME}":"${DOCKER_IMAGE_VERSION}" ./
-# # Build packages within image
-# docker run --cap-drop=all --memory=4GB --rm -i -t -v ./:/usr/src/app -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u="$(id -u)":"$(id -g)" -w /usr/src/app --hostname="${HOSTNAME}"-netlify-builder-13-1-0 --name netlify_builder "${DOCKER_IMAGE_PREFIX}"/"${DOCKER_IMAGE_NAME}":"${DOCKER_IMAGE_VERSION}" yarn bootstrap
-
 docker run\
  -e YARN_FOLDER="${DOCKER_WORKDIR}"/.yarn\
  -e YARN_CACHE_FOLDER="${DOCKER_WORKDIR}"/.yarn_cache\
