2c7a2c2a4cede8b4bd8651ee291ad8bbf6179574
Ciaran Moran <25228551+morancj@users.noreply.github.com>
2019-11-20 12:38:27 +0000

    feat: builds in pwd, home in container

---
+++ build_netlify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+set -x
+
+mkdir -p ".yarn" ".yarn_cache"
+yarn --global-folder "$(pwd)/.yarn" --cache-folder "$(pwd)/.yarn_cache"
+yarn bootstrap
--- docker_build_netlify-cms.sh
+++ docker_build_netlify-cms.sh
@@ -7,11 +7,14 @@ set -x
 # DOCKER_IMAGE_PREFIX="linaroits"
 # DOCKER_IMAGE_NAME="node-netlify-cmd-builder"
 # DOCKER_IMAGE_VERSION="13.1.0-stretch-slim"
-DOCKER_IMAGE_VERSION="$(git rev-parse --short HEAD)"
+DOCKER_IMAGE="node:13.1.0-stretch-slim"
+# DOCKER_IMAGE_VERSION="$(git rev-parse --short HEAD)"
 HOSTNAME="$(hostname)"
 USER="$(id -u)"
 GROUP="$(id -g)"
-DOCKER_WORKDIR="/usr/src/app"
+CURRENT_DIR="$(pwd)"
+# DOCKER_WORKDIR="/usr/src/app"
+DOCKER_WORKDIR="${HOME}"
 
 # Build Docker Image
 # docker build --compress --memory 4GB --pull --rm --tag "${DOCKER_IMAGE_PREFIX}"/"${DOCKER_IMAGE_NAME}":"${DOCKER_IMAGE_VERSION}" ./
@@ -19,22 +22,18 @@ DOCKER_WORKDIR="/usr/src/app"
 # docker run --cap-drop=all --memory=4GB --rm -i -t -v ./:/usr/src/app -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u="$(id -u)":"$(id -g)" -w /usr/src/app --hostname="${HOSTNAME}"-netlify-builder-13-1-0 --name netlify_builder "${DOCKER_IMAGE_PREFIX}"/"${DOCKER_IMAGE_NAME}":"${DOCKER_IMAGE_VERSION}" yarn bootstrap
 
 docker run\
- -e HOME="${DOCKER_WORKDIR}"\
- -e YARN_FOLDER="${HOME}"/.yarn\
- -e YARN_CACHE_FOLDER="${HOME}"/.yarn_cache\
+ -e YARN_FOLDER="${DOCKER_WORKDIR}"/.yarn\
+ -e YARN_CACHE_FOLDER="${DOCKER_WORKDIR}"/.yarn_cache\
  --cap-drop=all\
  --memory=4GB\
  --rm\
  -i\
- -t\
- -v ./:/usr/src/app\
+ -v "${CURRENT_DIR}":"${DOCKER_WORKDIR}"\
  -v /etc/group:/etc/group:ro\
  -v /etc/passwd:/etc/passwd:ro\
  -u "${USER}":"${GROUP}"\
- -w /usr/src/app\
+ -w "${DOCKER_WORKDIR}"\
  --hostname="${HOSTNAME}"-netlify-builder-13-1-0\
  --name=netlify_builder\
- "${DOCKER_IMAGE_PREFIX}"/"${DOCKER_IMAGE_NAME}":"${DOCKER_IMAGE_VERSION}"\
- mkdir -p "${HOME}/.yarn" "${HOME}/.yarn_cache" &&\
- yarn --global-folder "${YARN_FOLDER}" --cache-folder "${YARN_CACHE_FOLDER}" &&\
- yarn bootstrap
+ "${DOCKER_IMAGE}"\
+ /bin/bash -c "${DOCKER_WORKDIR}/build_netlify.sh"
