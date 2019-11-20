9022e9d31f300b813f44b00bd5fc31b359ab8e28
Oleksandr Shevtsov <oleksandr.shevtsov@90poe.io>
2019-11-20 19:49:39 +0200

    feat(lang): add 'uk' locale (#2903)

--- packages/netlify-cms-locales/src/index.js
+++ packages/netlify-cms-locales/src/index.js
@@ -2,3 +2,4 @@ export { default as en } from './en';
 export { default as de } from './de';
 export { default as fr } from './fr';
 export { default as es } from './es';
+export { default as uk } from './uk';
