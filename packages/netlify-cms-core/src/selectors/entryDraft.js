44b70cbabd8eb11ba74f0b02cfa0660bc2753a5b
erezrokah <erezrokah@users.noreply.github.com>
2019-11-26 13:51:45 +0200

    fix: show extension in path preview

--- packages/netlify-cms-core/src/selectors/entryDraft.js
+++ packages/netlify-cms-core/src/selectors/entryDraft.js
@@ -1,4 +1,5 @@
 import { slugFormatter } from 'Lib/backendHelper';
+import { selectEntryPath } from 'Reducers/collections';
 
 export const selectDraftPath = (state, collection, entry) => {
   const config = state.config;
@@ -6,7 +7,8 @@ export const selectDraftPath = (state, collection, entry) => {
   try {
     // slugFormatter throws in case an identifier is missing from the entry
     // we can safely ignore this error as this is just a preview path value
-    return slugFormatter(collection, entry.get('data'), config.get('slug'));
+    const slug = slugFormatter(collection, entry.get('data'), config.get('slug'));
+    return selectEntryPath(collection, slug);
   } catch (e) {
     return '';
   }
