4c5f013d15acd935c1a486b71e31788ab039e1f6
Erez Rokah <erezrokah@users.noreply.github.com>
2019-11-28 11:02:19 +0200

    chore: fix some dev mode console errors (#2924)

--- packages/netlify-cms-core/src/redux/index.js
+++ packages/netlify-cms-core/src/redux/index.js
@@ -7,7 +7,7 @@ const store = createStore(
   reducer,
   compose(
     applyMiddleware(thunkMiddleware, waitUntilAction),
-    window.devToolsExtension ? window.devToolsExtension() : f => f,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
   ),
 );
 
