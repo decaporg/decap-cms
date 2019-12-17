af6124536013e95b417501ef17f1144baffada72
Erez Rokah <erezrokah@users.noreply.github.com>
2019-12-17 08:30:08 +0200

    fix: don't show progress when loading preview status (#2974)

--- packages/netlify-cms-core/src/reducers/globalUI.js
+++ packages/netlify-cms-core/src/reducers/globalUI.js
@@ -1,13 +1,21 @@
 import { Map } from 'immutable';
 import { USE_OPEN_AUTHORING } from 'Actions/auth';
+
+const LOADING_IGNORE_LIST = ['DEPLOY_PREVIEW'];
+
+const ignoreWhenLoading = action => LOADING_IGNORE_LIST.some(type => action.type.includes(type));
+
 /*
  * Reducer for some global UI state that we want to share between components
  * */
 const globalUI = (state = Map({ isFetching: false, useOpenAuthoring: false }), action) => {
   // Generic, global loading indicator
-  if (action.type.indexOf('REQUEST') > -1) {
+  if (!ignoreWhenLoading(action) && action.type.includes('REQUEST')) {
     return state.set('isFetching', true);
-  } else if (action.type.indexOf('SUCCESS') > -1 || action.type.indexOf('FAILURE') > -1) {
+  } else if (
+    !ignoreWhenLoading(action) &&
+    (action.type.includes('SUCCESS') || action.type.includes('FAILURE'))
+  ) {
     return state.set('isFetching', false);
   } else if (action.type === USE_OPEN_AUTHORING) {
     return state.set('useOpenAuthoring', true);
