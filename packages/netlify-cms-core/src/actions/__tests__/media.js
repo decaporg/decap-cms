04d64141aec656986a9273afb5643ad1e26942c8
erezrokah <erezrokah@users.noreply.github.com>
2019-12-18 10:33:56 +0200

    test: add getAsset unit tests

---
+++ packages/netlify-cms-core/src/actions/__tests__/media.js
@@ -0,0 +1,138 @@
+import { Map } from 'immutable';
+import { getAsset, ADD_ASSET } from '../media';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import AssetProxy from '../../valueObjects/AssetProxy';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+jest.mock('../../reducers/entries');
+jest.mock('../mediaLibrary');
+jest.mock('../../reducers/mediaLibrary');
+
+describe('media', () => {
+  describe('getAsset', () => {
+    global.URL = { createObjectURL: jest.fn() };
+
+    const { selectMediaFilePath } = require('../../reducers/entries');
+    const { selectMediaFileByPath } = require('../../reducers/mediaLibrary');
+    const { getMediaDisplayURL, getMediaFile } = require('../mediaLibrary');
+
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+
+    it('should return empty asset for null path', () => {
+      const store = mockStore({});
+
+      const payload = { collection: null, entryPath: null, path: null };
+
+      return store.dispatch(getAsset(payload)).then(result => {
+        const actions = store.getActions();
+        expect(actions).toHaveLength(0);
+
+        expect(result).toEqual(new AssetProxy({ file: new File([], 'empty'), path: '' }));
+      });
+    });
+
+    it('should return asset from medias state', () => {
+      const path = 'static/media/image.png';
+      const asset = new AssetProxy({ file: new File([], 'empty'), path });
+      const store = mockStore({
+        config: Map(),
+        medias: Map({
+          [path]: asset,
+        }),
+      });
+
+      selectMediaFilePath.mockReturnValue(path);
+      const payload = { collection: Map(), entryPath: 'entryPath', path };
+
+      return store.dispatch(getAsset(payload)).then(result => {
+        const actions = store.getActions();
+        expect(actions).toHaveLength(0);
+
+        expect(result).toBe(asset);
+        expect(selectMediaFilePath).toHaveBeenCalledTimes(1);
+        expect(selectMediaFilePath).toHaveBeenCalledWith(
+          store.getState().config,
+          payload.collection,
+          payload.entryPath,
+          path,
+        );
+      });
+    });
+
+    it('should create asset for absolute path when not in medias state', () => {
+      const path = 'https://asset.netlify.com/image.png';
+
+      const asset = new AssetProxy({ url: path, path });
+      const store = mockStore({
+        medias: Map({}),
+      });
+
+      selectMediaFilePath.mockReturnValue(path);
+      const payload = { collection: null, entryPath: null, path };
+
+      return store.dispatch(getAsset(payload)).then(result => {
+        const actions = store.getActions();
+        expect(actions).toHaveLength(1);
+        expect(actions[0]).toEqual({
+          type: ADD_ASSET,
+          payload: asset,
+        });
+        expect(result).toEqual(asset);
+      });
+    });
+
+    it('should create asset from media file when not in medias state', () => {
+      const path = 'static/media/image.png';
+      const mediaFile = { file: new File([], '') };
+      const url = 'blob://displayURL';
+      const asset = new AssetProxy({ url, path });
+      const store = mockStore({
+        medias: Map({}),
+      });
+
+      selectMediaFilePath.mockReturnValue(path);
+      selectMediaFileByPath.mockReturnValue(mediaFile);
+      getMediaDisplayURL.mockResolvedValue(url);
+      const payload = { path };
+
+      return store.dispatch(getAsset(payload)).then(result => {
+        const actions = store.getActions();
+        expect(actions).toHaveLength(1);
+        expect(actions[0]).toEqual({
+          type: ADD_ASSET,
+          payload: asset,
+        });
+        expect(result).toEqual(asset);
+      });
+    });
+
+    it('should fetch asset media file when not in redux store', () => {
+      const path = 'static/media/image.png';
+      const url = 'blob://displayURL';
+      const asset = new AssetProxy({ url, path });
+      const store = mockStore({
+        medias: Map({}),
+      });
+
+      selectMediaFilePath.mockReturnValue(path);
+      selectMediaFileByPath.mockReturnValue(undefined);
+      getMediaFile.mockResolvedValue({ url });
+      const payload = { path };
+
+      return store.dispatch(getAsset(payload)).then(result => {
+        const actions = store.getActions();
+        expect(actions).toHaveLength(1);
+        expect(actions[0]).toEqual({
+          type: ADD_ASSET,
+          payload: asset,
+        });
+        expect(result).toEqual(asset);
+      });
+    });
+  });
+});
