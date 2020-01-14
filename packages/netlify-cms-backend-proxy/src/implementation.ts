22c6783ead289fa7fcde31bb300fcb4f2a6f9feb
erezrokah <erezrokah@users.noreply.github.com>
2020-01-14 18:04:08 +0200

    feat(backends): add proxy backend

---
+++ packages/netlify-cms-backend-proxy/src/implementation.ts
@@ -0,0 +1,160 @@
+import {
+  Entry,
+  AssetProxy,
+  PersistOptions,
+  User,
+  Config,
+  Implementation,
+  ImplementationFile,
+} from 'netlify-cms-lib-util';
+import AuthenticationPage from './AuthenticationPage';
+
+const serializeAsset = async (assetProxy: AssetProxy) => {
+  const base64content = await assetProxy.toBase64!();
+
+  return { path: assetProxy.path, content: base64content, encoding: 'base64' };
+};
+
+export default class ProxyBackend implements Implementation {
+  proxyUrl: string;
+
+  options: { initialWorkflowStatus?: string };
+
+  constructor(config: Config, options = {}) {
+    if (!config.backend.proxy_url) {
+      throw new Error('The Proxy backend needs a "proxy_url" in the backend configuration.');
+    }
+
+    this.proxyUrl = config.backend.proxy_url;
+    this.options = options;
+  }
+
+  authComponent() {
+    return AuthenticationPage;
+  }
+
+  restoreUser() {
+    return this.authenticate();
+  }
+
+  authenticate() {
+    return (Promise.resolve() as unknown) as Promise<User>;
+  }
+
+  logout() {
+    return null;
+  }
+
+  getToken() {
+    return Promise.resolve('');
+  }
+
+  request(payload: { action: string; params: Record<string, unknown> }) {
+    return fetch(this.proxyUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      body: JSON.stringify(payload),
+    }).then(r => r.json());
+  }
+
+  entriesByFolder(folder: string, extension: string, depth: number) {
+    return this.request({
+      action: 'entriesByFolder',
+      params: { folder, extension, depth },
+    });
+  }
+
+  entriesByFiles(files: ImplementationFile[]) {
+    return this.request({
+      action: 'entriesByFiles',
+      params: { files },
+    });
+  }
+
+  getEntry(path: string) {
+    return this.request({
+      action: 'getEntry',
+      params: { path },
+    });
+  }
+
+  unpublishedEntries() {
+    return this.request({
+      action: 'unpublishedEntries',
+      params: {},
+    });
+  }
+
+  unpublishedEntry(collection: string, slug: string) {
+    return this.request({
+      action: 'unpublishedEntry',
+      params: { collection, slug },
+    });
+  }
+
+  deleteUnpublishedEntry(collection: string, slug: string) {
+    return this.request({
+      action: 'deleteUnpublishedEntry',
+      params: { collection, slug },
+    });
+  }
+
+  async persistEntry(entry: Entry, assetProxies: AssetProxy[], options: PersistOptions) {
+    const assets = await Promise.all(assetProxies.map(serializeAsset));
+    return this.request({
+      action: 'persistEntry',
+      params: {
+        entry,
+        assets,
+        options: { ...options, status: options.status || this.options.initialWorkflowStatus },
+      },
+    });
+  }
+
+  updateUnpublishedEntryStatus(collection: string, slug: string, newStatus: string) {
+    return this.request({
+      action: 'updateUnpublishedEntryStatus',
+      params: { collection, slug, newStatus },
+    });
+  }
+
+  publishUnpublishedEntry(collection: string, slug: string) {
+    return this.request({
+      action: 'publishUnpublishedEntry',
+      params: { collection, slug },
+    });
+  }
+
+  getMedia() {
+    return this.request({
+      action: 'getMedia',
+      params: {},
+    });
+  }
+
+  getMediaFile(path: string) {
+    return this.request({
+      action: 'getMediaFile',
+      params: { path },
+    });
+  }
+
+  async persistMedia(assetProxy: AssetProxy) {
+    const asset = await serializeAsset(assetProxy);
+    return this.request({
+      action: 'persistMedia',
+      params: { asset },
+    });
+  }
+
+  deleteFile(path: string) {
+    return this.request({
+      action: 'deleteFile',
+      params: { path },
+    });
+  }
+
+  async getDeployPreview() {
+    return null;
+  }
+}
