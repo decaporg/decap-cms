169a2361e61de5debfb9ea58ef542960d8a6b03c
erezrokah <erezrokah@users.noreply.github.com>
2020-01-19 09:21:44 +0200

    chore: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: [
+    '../packages/netlify-cms-core/src/**/*.stories.js',
+    '../packages/netlify-cms-ui-default/src/**/*.stories.js',
+  ],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+};
