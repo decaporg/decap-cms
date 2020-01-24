cdadc5bf92c45da02e3c7e1d58dcac1c1138bdf2
Daan van der Zwaag <daan@devign.it>
2020-01-24 15:37:21 +0100

    style: new login UI

--- packages/netlify-cms-backend-test/src/AuthenticationPage.js
+++ packages/netlify-cms-backend-test/src/AuthenticationPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import { Icon, buttons, shadows, GoBackButton } from 'netlify-cms-ui-default';
+import { Icon, buttons, shadows } from 'netlify-cms-ui-default';
+// import { Button } from 'netlify-identity-widget';
+
+// Set one variable for all sizing aka the magicNumber, for more info contact @danoszz
+
+let magicNumber = 48;
 
 const StyledAuthenticationPage = styled.section`
   display: flex;
@@ -9,11 +14,54 @@ const StyledAuthenticationPage = styled.section`
   align-items: center;
   justify-content: center;
   height: 100vh;
+  background-color: #0c1e24;
+  position: relative;
+  &:after {
+    content: '';
+    width: 100%;
+    height: 50%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: #f2f5f7;
+    z-index: 0;
+  }
+`;
+
+const AuthenticationPageModal = styled.div`
+  position: relative;
+  z-index: 1;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: white;
+  background: #ffffff;
+  box-shadow: 0 2px 4px 0 rgba(14, 30, 37, 0.12);
+  border-radius: 8px;
+  padding: ${magicNumber}px;
+`;
+
+const PageModalLogoWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  max-width: calc(${magicNumber}px * 6);
 `;
 
 const PageLogoIcon = styled(Icon)`
-  color: #c4c6d2;
-  margin-top: -300px;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: calc(100% - ${magicNumber}px);
+  margin: 0 auto;
+`;
+
+const PageModalButtonsWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  width: 100%;
+  margin-top: calc(${magicNumber}px * 1.5);
 `;
 
 const LoginButton = styled.button`
@@ -22,10 +70,9 @@ const LoginButton = styled.button`
   ${buttons.default};
   ${buttons.gray};
 
-  padding: 0 30px;
-  margin-top: -40px;
   display: flex;
   align-items: center;
+  justify-content: center;
   position: relative;
 
   ${Icon} {
@@ -57,15 +104,21 @@ export default class AuthenticationPage extends React.Component {
   };
 
   render() {
-    const { config, inProgress, t } = this.props;
+    const { inProgress, t } = this.props;
 
     return (
       <StyledAuthenticationPage>
-        <PageLogoIcon size="300px" type="netlify-cms" />
-        <LoginButton disabled={inProgress} onClick={this.handleLogin}>
-          {inProgress ? t('auth.loggingIn') : t('auth.login')}
-        </LoginButton>
-        {config.site_url && <GoBackButton href={config.site_url}></GoBackButton>}
+        <AuthenticationPageModal>
+          <PageModalLogoWrapper>
+            <PageLogoIcon size="300px" type="netlify-cms" />
+          </PageModalLogoWrapper>
+          <PageModalButtonsWrapper>
+            <LoginButton disabled={inProgress} onClick={this.handleLogin}>
+              {inProgress ? t('auth.loggingIn') : t('auth.login')}
+            </LoginButton>
+            {/* {config.site_url && <GoBackButton href={config.site_url}></GoBackButton>} */}
+          </PageModalButtonsWrapper>
+        </AuthenticationPageModal>
       </StyledAuthenticationPage>
     );
   }
