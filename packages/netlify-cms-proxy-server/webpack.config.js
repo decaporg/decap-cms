899cb075826daaf98cfb2c73a120b9880e396ce2
erezrokah <erezrokah@users.noreply.github.com>
2020-01-25 17:40:12 +0200

    build(proxy_server): cleanup dependencies, add bin entry

--- packages/netlify-cms-proxy-server/webpack.config.js
+++ packages/netlify-cms-proxy-server/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const { NODE_ENV = 'production' } = process.env;
@@ -33,4 +34,5 @@ module.exports = {
       modulesDir: path.resolve(__dirname, path.join('..', '..', 'node_modules')),
     }),
   ],
+  plugins: [new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })],
 };
