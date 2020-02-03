36a7cce739829caba7646047794fbfe9e99b5c90
erezrokah <erezrokah@users.noreply.github.com>
2020-02-03 11:14:56 +0200

    chore: fix typescript error

--- packages/netlify-cms-proxy-server/src/middlewares/joi/index.ts
+++ packages/netlify-cms-proxy-server/src/middlewares/joi/index.ts
@@ -190,13 +190,11 @@ export const joi = (schema: Joi.Schema) => (
   next: express.NextFunction,
 ) => {
   const { error } = schema.validate(req.body, { allowUnknown: true });
-  const valid = error == null;
-
-  if (valid) {
-    next();
-  } else {
+  if (error) {
     const { details } = error;
     const message = details.map(i => i.message).join(',');
     res.status(422).json({ error: message });
+  } else {
+    next();
   }
 };
