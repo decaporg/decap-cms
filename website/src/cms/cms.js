a6e909050127940938fa94caea24c0a313308b11
erezrokah <erezrokah@users.noreply.github.com>
2020-02-03 18:47:04 +0200

    refactor: switch to netlify-cms-app

--- website/src/cms/cms.js
+++ website/src/cms/cms.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CMS from 'netlify-cms';
+import CMS from 'netlify-cms-app';
 import dayjs from 'dayjs';
 import Prism from 'prismjs';
 import { BlogPostTemplate } from '../templates/blog-post';
