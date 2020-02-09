b5e045d6f16304d8dbc19991e4aa27cfa03fe96d
erezrokah <erezrokah@users.noreply.github.com>
2020-02-09 16:22:51 +0200

    test: add unit tests

--- packages/netlify-cms-widget-markdown/src/MarkdownControl/VisualEditor.js
+++ packages/netlify-cms-widget-markdown/src/MarkdownControl/VisualEditor.js
@@ -49,6 +49,38 @@ const createSlateValue = (rawValue, { voidCodeBlock }) => {
   return Value.create({ document });
 };
 
+export const mergeMediaConfig = (editorComponents, field) => {
+  // merge editor media library config to image components
+  if (editorComponents.has('image')) {
+    const imageComponent = editorComponents.get('image');
+    const fields = imageComponent?.fields;
+
+    if (fields) {
+      imageComponent.fields = fields.update(
+        fields.findIndex(f => f.get('widget') === 'image'),
+        f => {
+          // merge `media_library` config
+          if (field.has('media_library')) {
+            f = f.set(
+              'media_library',
+              field.get('media_library').mergeDeep(f.get('media_library')),
+            );
+          }
+          // merge 'media_folder'
+          if (field.has('media_folder') && !f.has('media_folder')) {
+            f = f.set('media_folder', field.get('media_folder'));
+          }
+          // merge 'public_folder'
+          if (field.has('public_folder') && !f.has('public_folder')) {
+            f = f.set('public_folder', field.get('public_folder'));
+          }
+          return f;
+        },
+      );
+    }
+  }
+};
+
 export default class Editor extends React.Component {
   constructor(props) {
     super(props);
@@ -60,7 +92,7 @@ export default class Editor extends React.Component {
         ? editorComponents
         : editorComponents.set('code-block', { label: 'Code Block', type: 'code-block' });
 
-    this.mergeMediaConfig();
+    mergeMediaConfig(this.editorComponents, this.props.field);
     this.renderBlock = renderBlock({
       classNameWrapper: props.className,
       resolveWidget: props.resolveWidget,
@@ -86,38 +118,6 @@ export default class Editor extends React.Component {
     getEditorComponents: PropTypes.func.isRequired,
     t: PropTypes.func.isRequired,
   };
-
-  mergeMediaConfig() {
-    // merge editor media library config to image components
-    if (this.editorComponents.has('image')) {
-      const imageComponent = this.editorComponents.get('image');
-      const fields = imageComponent?.fields;
-
-      if (fields) {
-        imageComponent.fields = fields.update(
-          fields.findIndex(f => f.get('widget') === 'image'),
-          f => {
-            // merge `media_library` config
-            if (this.props.field.has('media_library')) {
-              f = f.set(
-                'media_library',
-                this.props.field.get('media_library').mergeDeep(f.get('media_library')),
-              );
-            }
-            // merge 'media_folder'
-            if (this.props.field.has('media_folder') && !f.has('media_folder')) {
-              f = f.set('media_folder', this.props.field.get('media_folder'));
-            }
-            // merge 'public_folder'
-            if (this.props.field.has('public_folder') && !f.has('public_folder')) {
-              f = f.set('public_folder', this.props.field.get('public_folder'));
-            }
-            return f;
-          },
-        );
-      }
-    }
-  }
 
   shouldComponentUpdate(nextProps, nextState) {
     return !this.state.value.equals(nextState.value);
