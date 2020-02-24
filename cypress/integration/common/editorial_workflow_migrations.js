fd26a54f40eb0e62b81778169e57aa8fd51be0b4
erezrokah <erezrokah@users.noreply.github.com>
2020-02-24 19:47:54 +0100

    test(e2e): shorten wait time

--- cypress/integration/common/editorial_workflow_migrations.js
+++ cypress/integration/common/editorial_workflow_migrations.js
@@ -27,13 +27,13 @@ export default function({ entries, getUser }) {
       goToWorkflow();
       updateWorkflowStatus(entries[2], workflowStatus.draft, workflowStatus.ready);
       // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(3000); // older versions of the CMS didn't wait fully for the update to be resolved
+      cy.wait(1500); // older versions of the CMS didn't wait fully for the update to be resolved
       updateWorkflowStatus(entries[1], workflowStatus.draft, workflowStatus.ready);
       // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(3000); // older versions of the CMS didn't wait fully for the update to be resolved
+      cy.wait(1500); // older versions of the CMS didn't wait fully for the update to be resolved
       updateWorkflowStatus(entries[0], workflowStatus.draft, workflowStatus.ready);
       // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(3000); // older versions of the CMS didn't wait fully for the update to be resolved
+      cy.wait(1500); // older versions of the CMS didn't wait fully for the update to be resolved
 
       cy.task('switchToVersion', {
         version: 'latest',
