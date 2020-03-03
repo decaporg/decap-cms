444837d804b1bc198b44ed6899832e9d7507babd
Shawn Erquhart <shawn@erquh.art>
2020-03-03 16:32:26 -0500

    fix: defaults

--- packages/netlify-cms-editor-component-definition-list/src/index.js
+++ packages/netlify-cms-editor-component-definition-list/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import marked from 'marked';
 
 const markdownField = {
@@ -28,16 +28,16 @@ const definitionList = {
     return list;
   },
   toBlock: (list = []) => {
-    return list.map(({ term, definition }) => `${term}\n: ${definition}`).join('\n\n');
+    return list.map(({ term = '', definition = '' }) => `${term}\n: ${definition}`).join('\n\n');
   },
   // eslint-disable-next-line react/display-name
   toPreview: (list = []) => (
     <dl>
       {list.map(({ term, definition }) => (
-        <>
+        <Fragment key={`${term}_${definition}`}>
           <dt dangerouslySetInnerHTML={{ __html: marked(term) }} />
           <dd dangerouslySetInnerHTML={{ __html: marked(definition) }} />
-        </>
+        </Fragment>
       ))}
     </dl>
   ),
