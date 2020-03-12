b92b50655481182cb79bc0c25c0810ae072f0cb9
Ferran Buireu <fbuireu@leadtech.com>
2020-03-12 14:20:49 +0100

    feat(catalan-translation): catalan translation for new fields added

--- packages/netlify-cms-locales/src/cat/index.js
+++ packages/netlify-cms-locales/src/cat/index.js
@@ -1,4 +1,18 @@
 const cat = {
+  auth: {
+    login: 'Iniciar sessió',
+    loggingIn: 'Iniciant sessió...',
+    loginWithNetlifyIdentity: 'Iniciar sessió amb l\'identitat de Netlify',
+    loginWithBitbucket: 'Iniciar sessió amb Bitbucket',
+    loginWithGitHub: 'Iniciar sessió amb GitHub',
+    loginWithGitLab: 'Iniciar sessió amb GitLab',
+    errors: {
+      email: 'Comprova que has escrit el teu email.',
+      password: 'Si us plau escriu la teva contrasenya.',
+      identitySettings:
+        'No s\'ha pogut obtenir accés a les configuracions d\'identitat. Quan feu servir backend de git-gateway, assegureu-vos que activeu el servei d’identitat i la passarel·la de Git.',
+    },
+  },
   app: {
     header: {
       content: 'Contingut',
@@ -9,7 +23,7 @@ const cat = {
     app: {
       errorHeader: 'Error al carregar la configuració del CMS',
       configErrors: 'Errors de configuració',
-      checkConfigYml: "Comprovi l'arxiu config.yml.",
+      checkConfigYml: 'Comprovi l\'arxiu config.yml.',
       loadingConfig: 'Carregant configuració....',
       waitingBackend: 'Esperant al servidor...',
     },
@@ -43,35 +57,43 @@ const cat = {
         required: '%{fieldLabel} és obligatori.',
         regexPattern: '%{fieldLabel} no coincideix amb el patró: %{pattern}.',
         processing: '%{fieldLabel} està processant.',
-        range: "%{fieldLabel} ha d'estar entre %{minValue} i %{maxValue}.",
+        range: '%{fieldLabel} ha d\'estar entre %{minValue} i %{maxValue}.',
         min: '%{fieldLabel} ha ser com a mínim %{minValue}.',
         max: '%{fieldLabel} ha de ser %{maxValue} o més.',
+        rangeCount: '%{fieldLabel} ha de tenir entre %{minCount} i %{maxCount} element(s).',
+        rangeCountExact: '%{fieldLabel} ha de tenir exactament %{count} element(s).',
+        minCount: '%{fieldLabel} ha de tenir com a mínim %{minCount} element(s).',
+        maxCount: '%{fieldLabel} ha de ser %{maxCount} o inferior.',
       },
     },
     editor: {
       onLeavePage: 'Estàs segur que vols deixar aquesta pàgina?',
       onUpdatingWithUnsavedChanges:
-        "Té canvis no guardats, si us plau, guardi'ls abans d'actualitzar l'estat.",
+        'Té canvis no guardats, si us plau, guardi\'ls abans d\'actualitzar l\'estat.',
       onPublishingNotReady: 'si us plau, actualitzi l\'estat a "Ready" abans de publicar.',
       onPublishingWithUnsavedChanges:
-        "Té canvis no guardats, siusplay, guardi'ls abans de publicar-los.",
+        'Té canvis no guardats, siusplay, guardi\'ls abans de publicar-los.',
       onPublishing: 'Està segur que vol publicar aquesta entrada?',
       onDeleteWithUnsavedChanges:
         'Està segur que vol eliminar aquesta entrada publicada, així com els canvis no guardats de la sessió actual?',
       onDeletePublishedEntry: 'Està segur que vol eliminar aquesta entrada publicada?',
       onDeleteUnpublishedChangesWithUnsavedChanges:
-        "Això eliminarà tots els canvis no publicats d'aquesta entrada així com els canvis no guardats de la sessió actual. Encara vol procedir?",
+        'Això eliminarà tots els canvis no publicats d\'aquesta entrada així com els canvis no guardats de la sessió actual. Encara vol procedir?',
       onDeleteUnpublishedChanges:
         'Tots els canvis no publicats en aquesta entrada seràn esborrats. Encara els vol eliminar?',
       loadingEntry: 'Carregant entrada...',
       confirmLoadBackup:
-        "S'ha recuperat una copia de seguretat local per aquesta entrada. La vol utilitzar?",
+        'S\'ha recuperat una copia de seguretat local per aquesta entrada. La vol utilitzar?',
     },
     editorToolbar: {
       publishing: 'Publicant...',
       publish: 'Publicar',
       published: 'Publicat',
+      unpublish: 'Despublicar',
+      duplicate: 'Duplicar',
+      unpublishing: 'Despublicant...',
       publishAndCreateNew: 'Publicar i crear de nou',
+      publishAndDuplicate: 'Publica i duplica',
       deleteUnpublishedChanges: 'Eliminar canvis no publicats',
       deleteUnpublishedEntry: 'Eliminar entrada no publicada',
       deletePublishedEntry: 'Eliminar entrada publicada',
@@ -93,6 +115,10 @@ const cat = {
       deployButtonLabel: 'Veure publicació',
     },
     editorWidgets: {
+      markdown: {
+        richText: 'Text enriquit',
+        markdown: 'Markdown',
+      },
       image: {
         choose: 'Escull una imatge',
         chooseDifferent: 'Escull una imatge diferent',
@@ -104,10 +130,10 @@ const cat = {
         remove: 'Esborrar archivo',
       },
       unknownControl: {
-        noControl: "No existeix un control per al widget '%{widget}'.",
+        noControl: 'No existeix un control per al widget \'%{widget}\'.',
       },
       unknownPreview: {
-        noPreview: "No existeix una vista prèvia per al widget '%{widget}'.",
+        noPreview: 'No existeix una vista prèvia per al widget \'%{widget}\'.',
       },
       headingOptions: {
         headingOne: 'Encapçalament 1',
@@ -125,6 +151,7 @@ const cat = {
     },
     mediaLibrary: {
       onDelete: 'Està segur de que vol eliminar el mitjà seleccionat?',
+      fileTooLarge: 'El fitxer és massa gran.\nLa configuració no permet fitxers més grans de %{size} kB.',
     },
     mediaLibraryModal: {
       loading: 'Carregant...',
@@ -143,10 +170,13 @@ const cat = {
     },
   },
   ui: {
+    default: {
+      goBackToSite: 'Torna enrere al lloc',
+    },
     errorBoundary: {
       title: 'Error',
-      details: "S'ha produït un error - si us plau ",
-      reportIt: "informa'ns d'això.",
+      details: 'S\'ha produït un error - si us plau ',
+      reportIt: 'informa\'ns d\'això.',
       detailsHeading: 'Detalls',
       recoveredEntry: {
         heading: 'Document recuperat',
@@ -159,17 +189,18 @@ const cat = {
       logOut: 'Tancar sessió',
     },
     toast: {
-      onFailToLoadEntries: "No s'ha ha pogut carregar l'entrada: %{details}",
-      onFailToLoadDeployPreview: "No s'ha pogut carregar la vista prèvia: %{details}",
-      onFailToPersist: "No s'ha pogut guardar l'entrada: %{details}",
-      onFailToDelete: "No s'ha pogut eliminar l'entrada: %{details}",
-      onFailToUpdateStatus: "No s'ha pogut actualitzar l'estat: %{details}",
-      missingRequiredField:
-        "Ups, no ha omplert un camp obligatori. Si us plau, ompli'l abans de guardar.",
+      onFailToLoadEntries: 'No s\'ha ha pogut carregar l\'entrada: %{details}',
+      onFailToLoadDeployPreview: 'No s\'ha pogut carregar la vista prèvia: %{details}',
+      onFailToPersist: 'No s\'ha pogut guardar l\'entrada: %{details}',
+      onFailToDelete: 'No s\'ha pogut eliminar l\'entrada: %{details}',
+      onFailToUpdateStatus: 'No s\'ha pogut actualitzar l\'estat: %{details}',
+      missingRequiredField: 'Ups, no ha omplert un camp obligatori. Si us plau, ompli\'l abans de guardar.',
       entrySaved: 'Entrada guardada',
       entryPublished: 'Entrada publicada',
-      onFailToPublishEntry: "No s'ha pogut publicar: %{details}",
-      entryUpdated: "Estat de l'entrada actualitzat",
+      entryUnpublished: 'Entrada despublicada',
+      onFailToPublishEntry: 'No s\'ha pogut publicar: %{details}',
+      onFailToUnpublishEntry: 'No s\'ha pogut despublicar l\'entrada: %{details}',
+      entryUpdated: 'Estat de l\'entrada actualitzat',
       onDeleteUnpublishedChanges: 'Canvis no publicats eliminats',
       onFailToAuth: '%{details}',
     },
