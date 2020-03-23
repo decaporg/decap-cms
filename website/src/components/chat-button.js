50fb5a56a886dadbad6b0562c02856f63513c8fb
erezrokah <erezrokah@users.noreply.github.com>
2020-03-23 13:18:58 +0200

    build(website): don't import styles from netlify-cms-ui-default

--- website/src/components/chat-button.js
+++ website/src/components/chat-button.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { zIndex } from 'netlify-cms-ui-default';
 
 const ChatLink = styled.a`
-  z-index: ${zIndex.zIndex100};
+  z-index: 100;
   position: fixed;
   bottom: 10px;
   right: 10px;
