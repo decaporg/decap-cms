d3aaf4ddb326efcaffdbcc5af5c0e5c8bcaa36ea
Steffen Martinsen <steffen@steffenmedia.no>
2020-05-20 11:37:52 +0200

    feat(lang): add norwegian (#3785)

--- packages/netlify-cms-locales/src/index.js
+++ packages/netlify-cms-locales/src/index.js
@@ -9,6 +9,8 @@ export { default as hu } from './hu';
 export { default as it } from './it';
 export { default as ja } from './ja';
 export { default as nl } from './nl';
+export { default as nb_no } from './nb_no';
+export { default as nn_no } from './nn_no';
 export { default as pl } from './pl';
 export { default as pt } from './pt';
 export { default as ru } from './ru';
