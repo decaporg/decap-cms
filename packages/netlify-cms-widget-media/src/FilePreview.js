384539a3e065f1115070c39b58802a13d358cab0
2Clutch <13703683+2Clutch@users.noreply.github.com>
2020-06-17 18:55:49 -0700

    feat: create media widget

---
+++ packages/netlify-cms-widget-media/src/FilePreview.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { List } from 'immutable';
+import { WidgetPreviewContainer } from 'netlify-cms-ui-default';
+
+const FileLink = styled(({ href, path }) => (
+  <a href={href} rel="noopener noreferrer" target="_blank">
+    {path}
+  </a>
+))`
+  display: block;
+`;
+
+function FileLinkList({ values, getAsset, field }) {
+  return (
+    <div>
+      {values.map(value => (
+        <FileLink key={value} path={value} href={getAsset(value, field)} />
+      ))}
+    </div>
+  );
+}
+
+function FileContent(props) {
+  const { value, getAsset, field } = props;
+  if (Array.isArray(value) || List.isList(value)) {
+    return <FileLinkList values={value} getAsset={getAsset} field={field} />;
+  }
+  return <FileLink key={value} path={value} href={getAsset(value, field)} />;
+}
+
+const FilePreview = props => (
+  <WidgetPreviewContainer>{props.value ? <FileContent {...props} /> : null}</WidgetPreviewContainer>
+);
+
+FilePreview.propTypes = {
+  getAsset: PropTypes.func.isRequired,
+  value: PropTypes.node,
+};
+
+export default FilePreview;
