a70631ed046318040a0018a5b462966cf5661a03
2Clutch <13703683+2Clutch@users.noreply.github.com>
2020-06-20 22:35:22 -0700

    feat: create media widget

--- packages/netlify-cms-app/src/extensions.js
+++ packages/netlify-cms-app/src/extensions.js
@@ -15,6 +15,7 @@ import NetlifyCmsWidgetNumber from 'netlify-cms-widget-number';
 import NetlifyCmsWidgetText from 'netlify-cms-widget-text';
 import NetlifyCmsWidgetImage from 'netlify-cms-widget-image';
 import NetlifyCmsWidgetFile from 'netlify-cms-widget-file';
+import NetlifyCmsWidgetMedia from 'netlify-cms-widget-media';
 import NetlifyCmsWidgetSelect from 'netlify-cms-widget-select';
 import NetlifyCmsWidgetMarkdown from 'netlify-cms-widget-markdown';
 import NetlifyCmsWidgetList from 'netlify-cms-widget-list';
@@ -45,6 +46,7 @@ CMS.registerWidget([
   NetlifyCmsWidgetText.Widget(),
   NetlifyCmsWidgetImage.Widget(),
   NetlifyCmsWidgetFile.Widget(),
+  NetlifyCmsWidgetMedia.Widget(),
   NetlifyCmsWidgetSelect.Widget(),
   NetlifyCmsWidgetMarkdown.Widget(),
   NetlifyCmsWidgetList.Widget(),
---
+++ packages/netlify-cms-widget-media/src/index.js
@@ -0,0 +1,15 @@
+import withFileControl from './withFileControl';
+import previewComponent from './FilePreview';
+import schema from './schema';
+
+const controlComponent = withFileControl();
+const Widget = (opts = {}) => ({
+  name: 'media',
+  controlComponent,
+  previewComponent,
+  schema,
+  ...opts,
+});
+
+export const NetlifyCmsWidgetMedia = { Widget, controlComponent, previewComponent, withFileControl };
+export default NetlifyCmsWidgetMedia;
