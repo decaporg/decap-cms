12ca602306f938311cda30abee9909c356c31688
tinfoil-knight <kunal99kundu@gmail.com>
2020-06-30 19:58:21 +0530

    feat: added-media-dialog-component

---
+++ packages/netlify-cms-ui-default/src/MediaDialog/MediaDialog.jsx
@@ -0,0 +1,292 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+
+import { Button, ButtonGroup, IconButton } from '../Button';
+import { Menu, MenuItem } from '../Menu';
+import Card from '../Card';
+import Icon from '../Icon';
+import Thumbnail, { ThumbnailGrid } from '../Thumbnail';
+import getMockData from '../utils/getMockData';
+import SearchBar from '../SearchBar';
+
+import { NavMenuItem, NavMenuGroup, NavMenu } from '../NavMenu';
+
+const MediaDialogWrap = styled(Card)`
+  color: ${({ theme }) => theme.color.text};
+  height: 80%;
+  width: auto;
+  display: flex;
+`;
+
+const SideWrap = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 1rem;
+`;
+
+const MainWrap = styled.div`
+  background-color: ${({ theme }) => theme.color.background};
+  position: relative;
+`;
+
+const ActionsTopWrap = styled.div`
+  display: flex;
+  padding: 1rem;
+  & > * + * {
+    margin-left: 1rem;
+  }
+`;
+
+const ActionsBottomWrap = styled.div`
+  background-color: ${({ theme }) => theme.color.background};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 1rem;
+  z-index: 10;
+`;
+
+const HeaderWrap = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 3rem;
+`;
+
+const SourceWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  & > a {
+    white-space: nowrap;
+  }
+`;
+
+// TODO: Fix the height
+const GalleryWrap = styled.div`
+  overflow-y: auto;
+  height: calc(100% - 10rem);
+  padding: 1rem;
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+const CloseBtn = styled(IconButton)`
+  position: absolute;
+  top: calc(50%-20px);
+  right: 0.8rem;
+`;
+
+const AddSrcBtn = styled(Button)`
+  position: absolute;
+  bottom: 1rem;
+`;
+const TitleWrap = styled.div`
+  font-weight: bold;
+`;
+
+const SideBarTitle = styled.div`
+  font-weight: bold;
+  font-size: 1.5rem;
+  white-space: nowrap;
+`;
+
+const StoryThumbnail = ({
+  previewImgSrc,
+  previewBgColor,
+  previewText,
+  title,
+  subtitle,
+  selectable,
+  previewAspectRatio,
+  horizontal,
+  titleMaxLines,
+  subtitleMaxLines,
+  width,
+  height,
+  onClick,
+}) => {
+  const [selected, setSelected] = useState(false);
+
+  return (
+    <StyledThumbnail
+      previewImgSrc={previewImgSrc}
+      previewBgColor={previewBgColor}
+      previewText={previewText}
+      title={title}
+      subtitle={subtitle}
+      selectable={selectable}
+      selected={selected}
+      horizontal={horizontal}
+      titleMaxLines={titleMaxLines}
+      subtitleMaxLines={subtitleMaxLines}
+      onSelect={() => setSelected(!selected)}
+      previewAspectRatio={previewAspectRatio}
+      width={width}
+      height={height}
+      onClick={onClick}
+    />
+  );
+};
+
+const StyledThumbnail = styled(Thumbnail)`
+  ${({ width }) => (width ? `width: ${width}` : ``)};
+  ${({ height }) => (height ? `height: ${height}` : ``)};
+`;
+
+// Currently renders photos of random size based on god knows what. Fix.
+const Gallery2 = ({ horizontal }) => {
+  const mockData = getMockData('post', 19);
+  const selectable = true;
+  const onClick = true;
+  const previewAspectRatio = '12:9';
+  const previewImgSrc = true;
+  const supertitle = true;
+  const title = true;
+  const description = true;
+  const subtitle = true;
+  const titleMaxLines = 1;
+  const subtitleMaxLines = 1;
+  return (
+    <ThumbnailGrid horizontal={horizontal}>
+      {mockData.map((thumb, i) => (
+        <StoryThumbnail
+          key={i}
+          previewImgSrc={previewImgSrc && thumb.featuredImage.regular}
+          title={title && thumb.title}
+          subtitle={subtitle && `${thumb.dateCreated}`}
+          selectable={selectable}
+          previewAspectRatio={previewAspectRatio}
+          width={'auto'}
+          horizontal={horizontal}
+          titleMaxLines={titleMaxLines}
+          subtitleMaxLines={subtitleMaxLines}
+          onClick={onClick && !selectable ? () => alert('You just clicked a thumbnail.') : null}
+        />
+      ))}
+    </ThumbnailGrid>
+  );
+};
+
+const SideBar = () => {
+  const sources = [
+    {
+      name: 'Media Library',
+      icon: 'database',
+    },
+    {
+      name: 'Unsplash',
+      icon: 'image',
+    },
+    {
+      name: 'Raw Pixel',
+      icon: 'image',
+    },
+  ];
+  return (
+    <SideWrap>
+      <SideBarTitle>Choose Image</SideBarTitle>
+      <SourceWrap>
+        {sources.map(el => (
+          <NavMenuItem icon={el.icon} key={el.name}>
+            {el.name}
+          </NavMenuItem>
+        ))}
+      </SourceWrap>
+      <AddSrcBtn icon="plus">Add Source</AddSrcBtn>
+    </SideWrap>
+  );
+};
+
+const MediaDialog = props => {
+  const [horizontal, setHorizontal] = useState(false);
+  const [open, setOpen] = useState(true);
+  const renderEnd = true;
+  const placeholder = 'Search media library';
+
+  return open ? (
+    <MediaDialogWrap>
+      <SideBar />
+      <MainWrap>
+        <HeaderWrap>
+          <TitleWrap>Media Library</TitleWrap>
+          <CloseBtn icon="cross" onClick={() => setOpen(false)} />
+        </HeaderWrap>
+        <ActionsTopWrap>
+          <SearchBar
+            placeholder={placeholder}
+            renderEnd={renderEnd ? () => <EndContent /> : null}
+            onChange={e => console.log(e.target.value)}
+          />
+          <IconButton icon="bulleted-list" onClick={() => setHorizontal(true)} />
+          <IconButton icon="grid" onClick={() => setHorizontal(false)} />
+        </ActionsTopWrap>
+        <GalleryWrap>
+          <Gallery2 horizontal={horizontal} />
+        </GalleryWrap>
+        <ActionsBottomWrap>
+          <ButtonGroup>
+            <Button icon="upload-cloud" type="default">
+              Upload New
+            </Button>
+            <Button icon="trash-2" type="danger">
+              Delete Selected
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button type="default">Cancel</Button>
+            <Button type="success" primary>
+              Choose Selected
+            </Button>
+          </ButtonGroup>
+        </ActionsBottomWrap>
+      </MainWrap>
+    </MediaDialogWrap>
+  ) : null;
+};
+
+export default MediaDialog;
+
+const EndContent = () => {
+  const [categoryMenuAnchorEl, setCategoryMenuAnchorEl] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState('Posts');
+  const categories = ['Posts', 'Media', 'Pages', 'Products', 'Authors', 'Everywhere'];
+  function handleClose() {
+    setCategoryMenuAnchorEl(null);
+  }
+  return (
+    <>
+      <Button size="sm" hasMenu onClick={e => setCategoryMenuAnchorEl(e.currentTarget)}>
+        {selectedCategory}
+      </Button>
+      <Menu
+        anchorEl={categoryMenuAnchorEl}
+        open={!!categoryMenuAnchorEl}
+        onClose={() => setCategoryMenuAnchorEl(null)}
+        anchorOrigin={{ y: 'bottom', x: 'right' }}
+      >
+        {categories.map(category => (
+          <MenuItem
+            key={category}
+            selected={selectedCategory === category}
+            onClick={() => {
+              setSelectedCategory(category);
+              handleClose();
+            }}
+          >
+            {category}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+};
---
+++ packages/netlify-cms-ui-default/src/MediaDialog/story.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MediaDialog from './MediaDialog';
+
+export default {
+  title: 'Components/MediaDialog',
+};
+
+export const _MediaDialog = () => {
+  return <MediaDialog />;
+};
