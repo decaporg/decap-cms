d67e7a586500a46f308cca5d7b057f22ebad14e3
flxbot <mail@felixboettcher.de>
2020-10-11 13:41:55 +0200

    fix: fixed clear button position

--- packages/netlify-cms-widget-color/src/ColorControl.js
+++ packages/netlify-cms-widget-color/src/ColorControl.js
@@ -52,22 +52,29 @@ export default class ColorControl extends React.Component {
         {' '}
         {!allowInput && value ? (
           <div
-            // clear button, not displayed if allowInput: true
             style={{
-              position: 'absolute',
-              right: '18px',
-              zIndex: '3',
-              padding: '8px',
-              marginTop: '11px',
+              position: 'relative',
+              width: '100%',
             }}
-            onClick={this.handleClear}
           >
-            <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false">
-              <path
-                fill="rgb(122, 130, 145)"
-                d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"
-              ></path>
-            </svg>
+            <div
+              // clear button, not displayed if allowInput: true
+              style={{
+                position: 'absolute',
+                right: '6px',
+                zIndex: '3',
+                padding: '8px',
+                marginTop: '11px',
+              }}
+              onClick={this.handleClear}
+            >
+              <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false">
+                <path
+                  fill="rgb(122, 130, 145)"
+                  d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"
+                ></path>
+              </svg>
+            </div>
           </div>
         ) : null}
         <div
