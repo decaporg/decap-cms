60f7e0bba67c6555acba3e04039c49f10cf51f6b
pz-mxu <66033561+pz-mxu@users.noreply.github.com>
2020-12-13 15:09:51 +0100

    fix(lib-util): update js-sha256 import (#4699)

--- packages/netlify-cms-lib-util/src/getBlobSHA.ts
+++ packages/netlify-cms-lib-util/src/getBlobSHA.ts
@@ -1,9 +1,9 @@
-import sha256 from 'js-sha256';
+import { sha256 } from 'js-sha256';
 
 export default (blob: Blob): Promise<string> =>
   new Promise((resolve, reject) => {
     const fr = new FileReader();
-    fr.onload = ({ target }) => resolve(sha256(target?.result));
+    fr.onload = ({ target }) => resolve(sha256(target?.result || ''));
     fr.onerror = err => {
       fr.abort();
       reject(err);
