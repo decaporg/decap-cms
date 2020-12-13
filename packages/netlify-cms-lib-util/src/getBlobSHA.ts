c052b2276f5117c0e145282952e9a90f8c7fe8dd
erezrokah <erezrokah@users.noreply.github.com>
2020-12-13 14:00:16 +0100

    chore: fix build

--- packages/netlify-cms-lib-util/src/getBlobSHA.ts
+++ packages/netlify-cms-lib-util/src/getBlobSHA.ts
@@ -3,7 +3,7 @@ import { sha256 } from 'js-sha256';
 export default (blob: Blob): Promise<string> =>
   new Promise((resolve, reject) => {
     const fr = new FileReader();
-    fr.onload = ({ target }) => resolve(sha256(target?.result));
+    fr.onload = ({ target }) => resolve(sha256(target?.result || ''));
     fr.onerror = err => {
       fr.abort();
       reject(err);
