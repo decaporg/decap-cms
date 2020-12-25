0f3b88f06e0e1bb5e8c2bc8b8bc987022471be5d
Ajmal Aboobacker <43377443+B3EF@users.noreply.github.com>
2020-12-25 22:45:01 +0530

    Update MarkdownPreview.js

--- packages/netlify-cms-widget-markdown/src/MarkdownPreview.js
+++ packages/netlify-cms-widget-markdown/src/MarkdownPreview.js
@@ -2,7 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { WidgetPreviewContainer } from 'netlify-cms-ui-default';
 import { markdownToHtml } from './serializers';
-
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
 class MarkdownPreview extends React.Component {
   static propTypes = {
     getAsset: PropTypes.func.isRequired,
@@ -16,7 +19,7 @@ class MarkdownPreview extends React.Component {
       return null;
     }
 
-    const html = markdownToHtml(value, { getAsset, resolveWidget });
+    const html = DOMPurify.sanitize(markdownToHtml(value, { getAsset, resolveWidget }));
 
     return <WidgetPreviewContainer dangerouslySetInnerHTML={{ __html: html }} />;
   }
