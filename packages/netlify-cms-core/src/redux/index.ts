9ae4cf2bdbae4a58a5a43f07f58503056c1a1e38
Slava Shkodin <smashercosmo@gmail.com>
2021-01-05 15:17:57 +0200

    refactor: use redux-devtools-extension package

--- packages/netlify-cms-core/src/redux/index.ts
+++ packages/netlify-cms-core/src/redux/index.ts
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose, AnyAction } from 'redux';
+import { createStore, applyMiddleware, AnyAction } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware, { ThunkMiddleware } from 'redux-thunk';
 import { routerMiddleware } from 'connected-react-router';
 import { waitUntilAction } from './middleware/waitUntilAction';
@@ -7,23 +8,14 @@ import history from '../routing/history';
 import { State } from '../types/redux';
 import { Reducer } from 'react';
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION__?: Function;
-  }
-}
-
 const store = createStore<State | undefined, AnyAction, unknown, unknown>(
   (createRootReducer(history) as unknown) as Reducer<State | undefined, AnyAction>,
-  compose(
+  composeWithDevTools(
     applyMiddleware(
       routerMiddleware(history),
       thunkMiddleware as ThunkMiddleware<State, AnyAction>,
       waitUntilAction,
     ),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f: Function): Function => f,
   ),
 );
 
