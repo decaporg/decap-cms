6223048fa6321cc99eb58c0628687cee92764190
Vladislav Shkodin <vladislav@umbra3d.com>
2021-02-07 22:29:32 +0200

    refactor(netlify-cms-widget-colorstring): convert function expressions to function declarations

--- packages/netlify-cms-widget-colorstring/src/index.js
+++ packages/netlify-cms-widget-colorstring/src/index.js
@@ -1,12 +1,14 @@
 import controlComponent from './ColorControl';
 import previewComponent from './ColorPreview';
 
-const Widget = (opts = {}) => ({
-  name: 'color',
-  controlComponent,
-  previewComponent,
-  ...opts,
-});
+function Widget(opts = {}) {
+  return {
+    name: 'color',
+    controlComponent,
+    previewComponent,
+    ...opts,
+  };
+}
 
 export const NetlifyCmsWidgetColorString = { Widget, controlComponent, previewComponent };
 export default NetlifyCmsWidgetColorString;
