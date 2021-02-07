45cef91571cf004c52ebf852eaca94a5a18f6048
Vladislav Shkodin <vladislav@umbra3d.com>
2021-02-07 22:34:01 +0200

    refactor(netlify-cms-widget-date): convert function expressions to function declarations

--- packages/netlify-cms-widget-date/src/index.js
+++ packages/netlify-cms-widget-date/src/index.js
@@ -1,12 +1,14 @@
 import controlComponent from './DateControl';
 import previewComponent from './DatePreview';
 
-const Widget = (opts = {}) => ({
-  name: 'date',
-  controlComponent,
-  previewComponent,
-  ...opts,
-});
+function Widget(opts = {}) {
+  return {
+    name: 'date',
+    controlComponent,
+    previewComponent,
+    ...opts,
+  };
+}
 
 export const NetlifyCmsWidgetDate = { Widget, controlComponent, previewComponent };
 export default NetlifyCmsWidgetDate;
