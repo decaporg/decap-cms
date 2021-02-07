d742abf2a3dce5055fd9cfa8abdde96e665a72c1
Vladislav Shkodin <vladislav@umbra3d.com>
2021-02-07 22:31:43 +0200

    refactor(netlify-cms-widget-object): convert function expressions to function declarations

--- packages/netlify-cms-widget-object/src/index.js
+++ packages/netlify-cms-widget-object/src/index.js
@@ -2,13 +2,15 @@ import controlComponent from './ObjectControl';
 import previewComponent from './ObjectPreview';
 import schema from './schema';
 
-const Widget = (opts = {}) => ({
-  name: 'object',
-  controlComponent,
-  previewComponent,
-  schema,
-  ...opts,
-});
+function Widget(opts = {}) {
+  return {
+    name: 'object',
+    controlComponent,
+    previewComponent,
+    schema,
+    ...opts,
+  };
+}
 
 export const NetlifyCmsWidgetObject = { Widget, controlComponent, previewComponent };
 export default NetlifyCmsWidgetObject;
