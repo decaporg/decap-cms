99528bc8964a2e1632708390befa7059a30b6fbd
Yudu Ban <banyudu@gmail.com>
2021-03-16 16:49:00 +0800

    feat: #5082 add persistMedia function in widget props

--- packages/netlify-cms-core/src/components/Editor/EditorControlPane/EditorControl.js
+++ packages/netlify-cms-core/src/components/Editor/EditorControlPane/EditorControl.js
@@ -18,6 +18,7 @@ import {
   removeInsertedMedia,
   clearMediaControl,
   removeMediaControl,
+  persistMedia,
 } from 'Actions/mediaLibrary';
 import Widget from './Widget';
 import { validateMetaField } from '../../../actions/entries';
@@ -123,6 +124,7 @@ class EditorControl extends React.Component {
     openMediaLibrary: PropTypes.func.isRequired,
     addAsset: PropTypes.func.isRequired,
     removeInsertedMedia: PropTypes.func.isRequired,
+    persistMedia: PropTypes.func.isRequired,
     onValidate: PropTypes.func,
     processControlRef: PropTypes.func,
     controlRef: PropTypes.func,
@@ -182,6 +184,7 @@ class EditorControl extends React.Component {
       removeMediaControl,
       addAsset,
       removeInsertedMedia,
+      persistMedia,
       onValidate,
       processControlRef,
       controlRef,
@@ -294,6 +297,7 @@ class EditorControl extends React.Component {
               onClearMediaControl={clearMediaControl}
               onRemoveMediaControl={removeMediaControl}
               onRemoveInsertedMedia={removeInsertedMedia}
+              onPersistMedia={persistMedia}
               onAddAsset={addAsset}
               getAsset={boundGetAsset}
               hasActiveStyle={isSelected || this.state.styleActive}
@@ -370,6 +374,7 @@ function mapDispatchToProps(dispatch) {
       clearMediaControl,
       removeMediaControl,
       removeInsertedMedia,
+      persistMedia,
       addAsset,
       query,
       clearSearch,
