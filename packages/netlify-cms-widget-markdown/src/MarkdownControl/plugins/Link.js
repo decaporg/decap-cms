dde1a9db5483912f626f13239d7a3d06d6c4e05c
Arpad Gabor <hello@arpadgabor.com>
2021-04-01 17:32:10 +0300

    fix(widget-markdown): improve UX in Markdown editor - link editing and selected heading underline (#5104)

--- packages/netlify-cms-widget-markdown/src/MarkdownControl/plugins/Link.js
+++ packages/netlify-cms-widget-markdown/src/MarkdownControl/plugins/Link.js
@@ -2,24 +2,35 @@ function Link({ type }) {
   return {
     commands: {
       toggleLink(editor, getUrl) {
+        const selection = editor.value.selection;
+        const isCollapsed = selection && selection.isCollapsed;
+
         if (editor.hasInline(type)) {
-          editor.unwrapInline(type);
+          const inlines = editor.value.inlines.toJSON();
+          const link = inlines.find(item => item.type === type);
+
+          const url = getUrl(link.data.url);
+
+          if (url) {
+            // replace the old link
+            return editor.setInlines({ data: { url } });
+          } else {
+            // remove url if it was removed by the user
+            return editor.unwrapInline(type);
+          }
         } else {
           const url = getUrl();
-          if (!url) return;
-
-          const selection = editor.value.selection;
-          const isCollapsed = selection && selection.isCollapsed;
-          if (isCollapsed) {
-            // If no text is selected, use the entered URL as text.
-            return editor.insertInline({
-              type,
-              data: { url },
-              nodes: [{ object: 'text', text: url }],
-            });
-          } else {
-            return editor.wrapInline({ type, data: { url } }).moveToEnd();
+          if (!url) {
+            return;
           }
+
+          return isCollapsed
+            ? editor.insertInline({
+                type,
+                data: { url },
+                nodes: [{ object: 'text', text: url }],
+              })
+            : editor.wrapInline({ type, data: { url } }).moveToEnd();
         }
       },
     },
