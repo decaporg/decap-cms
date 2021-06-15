cc8374a33540d2d3a17c64da98dfb0aab3164b00
Trang Le <lethutrang101@gmail.com>
2021-06-15 17:38:35 +0700

    fix(editor plugin): handle Enter key in list block items
    Skip creating default block when text is not empty

--- packages/netlify-cms-widget-markdown/src/MarkdownControl/plugins/BreakToDefaultBlock.js
+++ packages/netlify-cms-widget-markdown/src/MarkdownControl/plugins/BreakToDefaultBlock.js
@@ -1,13 +1,21 @@
 import isHotkey from 'is-hotkey';
 
+import {SLATE_LIST_BLOCK_TYPES as listType} from '../../types'
+
 function BreakToDefaultBlock({ defaultType }) {
   return {
     onKeyDown(event, editor, next) {
-      const { selection, startBlock } = editor.value;
+      const { selection, startBlock, blocks } = editor.value;
       const isEnter = isHotkey('enter', event);
       if (!isEnter) {
         return next();
       }
+      const isListItem = blocks.some((node) => node.type === listType.children);
+    if (isListItem) {
+      if (startBlock.text.length > 0) return next()
+      return editor.setNodeByKey(startBlock.key, {type: defaultType}); 
+      
+    };
       if (selection.isExpanded) {
         editor.delete();
         return next();
