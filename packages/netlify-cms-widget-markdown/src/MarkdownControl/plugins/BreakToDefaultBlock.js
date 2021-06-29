c9214c9ac93257fd834c206ef4b5522c579fdda4
Trang Le <lethutrang101@gmail.com>
2021-06-29 16:23:44 +0700

    fix(default block): skip creating new default block when target is list item

--- packages/netlify-cms-widget-markdown/src/MarkdownControl/plugins/BreakToDefaultBlock.js
+++ packages/netlify-cms-widget-markdown/src/MarkdownControl/plugins/BreakToDefaultBlock.js
@@ -3,7 +3,7 @@ import isHotkey from 'is-hotkey';
 function BreakToDefaultBlock({ defaultType }) {
   return {
     onKeyDown(event, editor, next) {
-      const { selection, startBlock } = editor.value;
+      const { selection, startBlock, document } = editor.value;
       const isEnter = isHotkey('enter', event);
       if (!isEnter) {
         return next();
@@ -12,6 +12,11 @@ function BreakToDefaultBlock({ defaultType }) {
         editor.delete();
         return next();
       }
+      /** If the block that receives the Enter key is a child of a list item block, we'll
+        * skip this plugin and create a new list item block underneath instead.
+      */
+      const parent = document.getParent(startBlock.key);
+      if (parent.type === 'list-item') return next();
       if (selection.start.isAtEndOfNode(startBlock) && startBlock.type !== defaultType) {
         return editor.insertBlock(defaultType);
       }
