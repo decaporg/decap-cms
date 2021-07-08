e0ee46c32632b6275165e8c1edb76f8074ca2de6
Brady Stroud <bradystroud@me.com>
2021-07-08 16:00:49 +1000

    fix: remove logs

--- packages/netlify-cms-core/src/components/Editor/EditorToolbar.js
+++ packages/netlify-cms-core/src/components/Editor/EditorToolbar.js
@@ -592,15 +592,10 @@ export class EditorToolbar extends React.Component {
 
   renderWorkflowPublishControls = () => {
     const { collection, currentStatus, isNewEntry, useOpenAuthoring, t } = this.props;
-    console.log(useOpenAuthoring);
 
     const canCreate = collection.get('create');
     const canPublish = collection.get('publish') && !useOpenAuthoring;
     const canDelete = collection.get('delete', true);
-
-    console.assert(useOpenAuthoring, "Not using open authoring :(")
-
-    console.table([{"Can create": canCreate,"Can publish": canPublish}])
 
     if (currentStatus) {
       return (
