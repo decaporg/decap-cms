1cf121ba5109b2a0174e81fa14ddec6fb4602726
Brady Stroud <bradystroud@me.com>
2021-07-28 08:29:47 +1000

    fix: deploy preview position

--- packages/netlify-cms-core/src/components/Editor/EditorToolbar.js
+++ packages/netlify-cms-core/src/components/Editor/EditorToolbar.js
@@ -603,7 +603,6 @@ export class EditorToolbar extends React.Component {
     if (currentStatus) {
       return (
         <>
-          {this.renderDeployPreviewControls(t('editor.editorToolbar.deployPreviewButtonLabel'))}
           {this.renderWorkflowStatusControls()}
           {this.renderNewEntryWorkflowPublishControls({ canCreate, canPublish })}
         </>
