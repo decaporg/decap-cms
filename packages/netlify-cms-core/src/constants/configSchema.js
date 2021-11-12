c68e82a9001cfb01401859ea4d48b2e9b24ce744
Taras <tarasm@gmail.com>
2021-11-12 13:56:44 -0500

    fix: generate config schema

--- packages/netlify-cms-core/src/constants/configSchema.js
+++ packages/netlify-cms-core/src/constants/configSchema.js
@@ -11,7 +11,7 @@ import uuid from 'uuid/v4';
 import { formatExtensions, frontmatterFormats, extensionFormatters } from '../formats/formats';
 import { getWidgets } from '../lib/registry';
 import { I18N_STRUCTURE, I18N_FIELD } from '../lib/i18n';
-import configSchema from './configSchema.json';
+import validateSchema from './validateSchema';
 
 const localeType = { type: 'string', minLength: 2, maxLength: 10, pattern: '^[a-zA-Z-_]+$' };
 
@@ -123,203 +123,202 @@ const viewGroups = {
  * fix a circular dependency problem for WebPack,
  * where the imports get resolved asynchronously.
  */
-export function getConfigSchema() {
-  return configSchema;
-  // return {
-  //   type: 'object',
-  //   properties: {
-  //     backend: {
-  //       type: 'object',
-  //       properties: {
-  //         name: { type: 'string', examples: ['test-repo'] },
-  //         auth_scope: {
-  //           type: 'string',
-  //           examples: ['repo', 'public_repo'],
-  //           enum: ['repo', 'public_repo'],
-  //         },
-  //         cms_label_prefix: { type: 'string', minLength: 1 },
-  //         open_authoring: { type: 'boolean', examples: [true] },
-  //       },
-  //       required: ['name'],
-  //     },
-  //     local_backend: {
-  //       oneOf: [
-  //         { type: 'boolean' },
-  //         {
-  //           type: 'object',
-  //           properties: {
-  //             url: { type: 'string', examples: ['http://localhost:8081/api/v1'] },
-  //             allowed_hosts: {
-  //               type: 'array',
-  //               items: { type: 'string' },
-  //             },
-  //           },
-  //           additionalProperties: false,
-  //         },
-  //       ],
-  //     },
-  //     locale: { type: 'string', examples: ['en', 'fr', 'de'] },
-  //     i18n: i18nRoot,
-  //     site_url: { type: 'string', examples: ['https://example.com'] },
-  //     display_url: { type: 'string', examples: ['https://example.com'] },
-  //     logo_url: { type: 'string', examples: ['https://example.com/images/logo.svg'] },
-  //     show_preview_links: { type: 'boolean' },
-  //     media_folder: { type: 'string', examples: ['assets/uploads'] },
-  //     public_folder: { type: 'string', examples: ['/uploads'] },
-  //     media_folder_relative: { type: 'boolean' },
-  //     media_library: {
-  //       type: 'object',
-  //       properties: {
-  //         name: { type: 'string', examples: ['uploadcare'] },
-  //         config: { type: 'object' },
-  //       },
-  //       required: ['name'],
-  //     },
-  //     publish_mode: {
-  //       type: 'string',
-  //       enum: ['simple', 'editorial_workflow'],
-  //       examples: ['editorial_workflow'],
-  //     },
-  //     slug: {
-  //       type: 'object',
-  //       properties: {
-  //         encoding: { type: 'string', enum: ['unicode', 'ascii'] },
-  //         clean_accents: { type: 'boolean' },
-  //       },
-  //     },
-  //     collections: {
-  //       type: 'array',
-  //       minItems: 1,
-  //       items: {
-  //         // ------- Each collection: -------
-  //         type: 'object',
-  //         properties: {
-  //           name: { type: 'string' },
-  //           label: { type: 'string' },
-  //           label_singular: { type: 'string' },
-  //           description: { type: 'string' },
-  //           folder: { type: 'string' },
-  //           files: {
-  //             type: 'array',
-  //             items: {
-  //               // ------- Each file: -------
-  //               type: 'object',
-  //               properties: {
-  //                 name: { type: 'string' },
-  //                 label: { type: 'string' },
-  //                 label_singular: { type: 'string' },
-  //                 description: { type: 'string' },
-  //                 file: { type: 'string' },
-  //                 preview_path: { type: 'string' },
-  //                 preview_path_date_field: { type: 'string' },
-  //                 fields: fieldsConfig(),
-  //               },
-  //               required: ['name', 'label', 'file', 'fields'],
-  //             },
-  //             uniqueItemProperties: ['name'],
-  //           },
-  //           identifier_field: { type: 'string' },
-  //           summary: { type: 'string' },
-  //           slug: { type: 'string' },
-  //           path: { type: 'string' },
-  //           preview_path: { type: 'string' },
-  //           preview_path_date_field: { type: 'string' },
-  //           create: { type: 'boolean' },
-  //           publish: { type: 'boolean' },
-  //           hide: { type: 'boolean' },
-  //           editor: {
-  //             type: 'object',
-  //             properties: {
-  //               preview: { type: 'boolean' },
-  //             },
-  //           },
-  //           format: { type: 'string', enum: Object.keys(formatExtensions) },
-  //           extension: { type: 'string' },
-  //           frontmatter_delimiter: {
-  //             type: ['string', 'array'],
-  //             minItems: 2,
-  //             maxItems: 2,
-  //             items: {
-  //               type: 'string',
-  //             },
-  //           },
-  //           fields: fieldsConfig(),
-  //           sortable_fields: {
-  //             type: 'array',
-  //             items: {
-  //               type: 'string',
-  //             },
-  //           },
-  //           sortableFields: {
-  //             type: 'array',
-  //             items: {
-  //               type: 'string',
-  //             },
-  //           },
-  //           view_filters: viewFilters,
-  //           view_groups: viewGroups,
-  //           nested: {
-  //             type: 'object',
-  //             properties: {
-  //               depth: { type: 'number', minimum: 1, maximum: 1000 },
-  //               summary: { type: 'string' },
-  //             },
-  //             required: ['depth'],
-  //           },
-  //           meta: {
-  //             type: 'object',
-  //             properties: {
-  //               path: {
-  //                 type: 'object',
-  //                 properties: {
-  //                   label: { type: 'string' },
-  //                   widget: { type: 'string' },
-  //                   index_file: { type: 'string' },
-  //                 },
-  //                 required: ['label', 'widget', 'index_file'],
-  //               },
-  //             },
-  //             additionalProperties: false,
-  //             minProperties: 1,
-  //           },
-  //           i18n: i18nCollection,
-  //         },
-  //         required: ['name', 'label'],
-  //         oneOf: [{ required: ['files'] }, { required: ['folder', 'fields'] }],
-  //         not: {
-  //           required: ['sortable_fields', 'sortableFields'],
-  //         },
-  //         if: { required: ['extension'] },
-  //         then: {
-  //           // Cannot infer format from extension.
-  //           if: {
-  //             properties: {
-  //               extension: { enum: Object.keys(extensionFormatters) },
-  //             },
-  //           },
-  //           else: { required: ['format'] },
-  //         },
-  //         dependencies: {
-  //           frontmatter_delimiter: {
-  //             properties: {
-  //               format: { enum: frontmatterFormats },
-  //             },
-  //             required: ['format'],
-  //           },
-  //         },
-  //       },
-  //       uniqueItemProperties: ['name'],
-  //     },
-  //     editor: {
-  //       type: 'object',
-  //       properties: {
-  //         preview: { type: 'boolean' },
-  //       },
-  //     },
-  //   },
-  //   required: ['backend', 'collections'],
-  //   anyOf: [{ required: ['media_folder'] }, { required: ['media_library'] }],
-  // };
+function getConfigSchema() {
+  return {
+    type: 'object',
+    properties: {
+      backend: {
+        type: 'object',
+        properties: {
+          name: { type: 'string', examples: ['test-repo'] },
+          auth_scope: {
+            type: 'string',
+            examples: ['repo', 'public_repo'],
+            enum: ['repo', 'public_repo'],
+          },
+          cms_label_prefix: { type: 'string', minLength: 1 },
+          open_authoring: { type: 'boolean', examples: [true] },
+        },
+        required: ['name'],
+      },
+      local_backend: {
+        oneOf: [
+          { type: 'boolean' },
+          {
+            type: 'object',
+            properties: {
+              url: { type: 'string', examples: ['http://localhost:8081/api/v1'] },
+              allowed_hosts: {
+                type: 'array',
+                items: { type: 'string' },
+              },
+            },
+            additionalProperties: false,
+          },
+        ],
+      },
+      locale: { type: 'string', examples: ['en', 'fr', 'de'] },
+      i18n: i18nRoot,
+      site_url: { type: 'string', examples: ['https://example.com'] },
+      display_url: { type: 'string', examples: ['https://example.com'] },
+      logo_url: { type: 'string', examples: ['https://example.com/images/logo.svg'] },
+      show_preview_links: { type: 'boolean' },
+      media_folder: { type: 'string', examples: ['assets/uploads'] },
+      public_folder: { type: 'string', examples: ['/uploads'] },
+      media_folder_relative: { type: 'boolean' },
+      media_library: {
+        type: 'object',
+        properties: {
+          name: { type: 'string', examples: ['uploadcare'] },
+          config: { type: 'object' },
+        },
+        required: ['name'],
+      },
+      publish_mode: {
+        type: 'string',
+        enum: ['simple', 'editorial_workflow'],
+        examples: ['editorial_workflow'],
+      },
+      slug: {
+        type: 'object',
+        properties: {
+          encoding: { type: 'string', enum: ['unicode', 'ascii'] },
+          clean_accents: { type: 'boolean' },
+        },
+      },
+      collections: {
+        type: 'array',
+        minItems: 1,
+        items: {
+          // ------- Each collection: -------
+          type: 'object',
+          properties: {
+            name: { type: 'string' },
+            label: { type: 'string' },
+            label_singular: { type: 'string' },
+            description: { type: 'string' },
+            folder: { type: 'string' },
+            files: {
+              type: 'array',
+              items: {
+                // ------- Each file: -------
+                type: 'object',
+                properties: {
+                  name: { type: 'string' },
+                  label: { type: 'string' },
+                  label_singular: { type: 'string' },
+                  description: { type: 'string' },
+                  file: { type: 'string' },
+                  preview_path: { type: 'string' },
+                  preview_path_date_field: { type: 'string' },
+                  fields: fieldsConfig(),
+                },
+                required: ['name', 'label', 'file', 'fields'],
+              },
+              uniqueItemProperties: ['name'],
+            },
+            identifier_field: { type: 'string' },
+            summary: { type: 'string' },
+            slug: { type: 'string' },
+            path: { type: 'string' },
+            preview_path: { type: 'string' },
+            preview_path_date_field: { type: 'string' },
+            create: { type: 'boolean' },
+            publish: { type: 'boolean' },
+            hide: { type: 'boolean' },
+            editor: {
+              type: 'object',
+              properties: {
+                preview: { type: 'boolean' },
+              },
+            },
+            format: { type: 'string', enum: Object.keys(formatExtensions) },
+            extension: { type: 'string' },
+            frontmatter_delimiter: {
+              type: ['string', 'array'],
+              minItems: 2,
+              maxItems: 2,
+              items: {
+                type: 'string',
+              },
+            },
+            fields: fieldsConfig(),
+            sortable_fields: {
+              type: 'array',
+              items: {
+                type: 'string',
+              },
+            },
+            sortableFields: {
+              type: 'array',
+              items: {
+                type: 'string',
+              },
+            },
+            view_filters: viewFilters,
+            view_groups: viewGroups,
+            nested: {
+              type: 'object',
+              properties: {
+                depth: { type: 'number', minimum: 1, maximum: 1000 },
+                summary: { type: 'string' },
+              },
+              required: ['depth'],
+            },
+            meta: {
+              type: 'object',
+              properties: {
+                path: {
+                  type: 'object',
+                  properties: {
+                    label: { type: 'string' },
+                    widget: { type: 'string' },
+                    index_file: { type: 'string' },
+                  },
+                  required: ['label', 'widget', 'index_file'],
+                },
+              },
+              additionalProperties: false,
+              minProperties: 1,
+            },
+            i18n: i18nCollection,
+          },
+          required: ['name', 'label'],
+          oneOf: [{ required: ['files'] }, { required: ['folder', 'fields'] }],
+          not: {
+            required: ['sortable_fields', 'sortableFields'],
+          },
+          if: { required: ['extension'] },
+          then: {
+            // Cannot infer format from extension.
+            if: {
+              properties: {
+                extension: { enum: Object.keys(extensionFormatters) },
+              },
+            },
+            else: { required: ['format'] },
+          },
+          dependencies: {
+            frontmatter_delimiter: {
+              properties: {
+                format: { enum: frontmatterFormats },
+              },
+              required: ['format'],
+            },
+          },
+        },
+        uniqueItemProperties: ['name'],
+      },
+      editor: {
+        type: 'object',
+        properties: {
+          preview: { type: 'boolean' },
+        },
+      },
+    },
+    required: ['backend', 'collections'],
+    anyOf: [{ required: ['media_folder'] }, { required: ['media_library'] }],
+  };
 }
 
 function getWidgetSchemas() {
@@ -356,16 +355,9 @@ class ConfigError extends Error {
  * the config that is passed in.
  */
 export function validateConfig(config) {
-  const ajv = new AJV({ allErrors: true, $data: true, strict: false });
-  uniqueItemProperties(ajv);
-  select(ajv);
-  instanceOf(ajv);
-  prohibited(ajv);
-  ajvErrors(ajv);
-
-  const valid = ajv.validate(getConfigSchema(), config);
+  const valid = validateSchema(config);
   if (!valid) {
-    const errors = ajv.errors.map(e => {
+    const errors = validateSchema.errors.map(e => {
       switch (e.keyword) {
         // TODO: remove after https://github.com/ajv-validator/ajv-keywords/pull/123 is merged
         case 'uniqueItemProperties': {
