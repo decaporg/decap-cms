183a0c33e71523340651d2dcee497596f98bedac
Trang Le <lethutrang101@gmail.com>
2022-04-11 16:26:47 +0700

    feat(jest config): add moduleNameMapper for GitHub and BitBucket
    Required for the test that checks setDefaultBranchName is called in lib-util to work

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,8 @@ module.exports = {
     'netlify-cms-lib-util': '<rootDir>/packages/netlify-cms-lib-util/src/index.ts',
     'netlify-cms-ui-default': '<rootDir>/packages/netlify-cms-ui-default/src/index.js',
     'netlify-cms-backend-github': '<rootDir>/packages/netlify-cms-backend-github/src/index.ts',
+    'netlify-cms-backend-gitlab': '<rootDir>/packages/netlify-cms-backend-gitlab/src/index.ts',
+    'netlify-cms-backend-bitbucket': '<rootDir>/packages/netlify-cms-backend-bitbucket/src/index.ts',
     'netlify-cms-lib-widgets': '<rootDir>/packages/netlify-cms-lib-widgets/src/index.ts',
     'netlify-cms-widget-object': '<rootDir>/packages/netlify-cms-widget-object/src/index.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
