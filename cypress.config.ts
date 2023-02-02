0219405db1b1ccc73bc6f1caf60ba2a5cfb735c3
Anze Demsar <anze.demsar@p-m.si>
2023-02-02 13:50:49 +0100

    chore(deps): update cypress

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  projectId: '1c35bs',
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:8080',
+    specPattern: 'cypress/e2e/*spec*.js',
+  },
+});
