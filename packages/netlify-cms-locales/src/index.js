ac83de3f5c1ae0220f69c8ec08e3919fe01935b0
Oleksii Brylin <brylin@moqod.com>
2023-03-18 00:30:51 +0200

    added ua locale to netlify-cms-locales

--- packages/netlify-cms-locales/src/index.js
+++ packages/netlify-cms-locales/src/index.js
@@ -28,3 +28,4 @@ export { default as hr } from './hr';
 export { default as bg } from './bg';
 export { default as zh_Hans } from './zh_Hans';
 export { default as he } from './he';
+export { default as ua } from './ua';
