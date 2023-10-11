c3fd6a4cdc4c56a68413bdda4f009a4c0c321a56
JohnJMills <john@digits.com>
2023-10-11 11:35:35 -0700

    build: update build files

---
+++ packages/decap-cms-locales/dist/decap-cms-locales.js
@@ -0,0 +1,2 @@
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define("DecapCmsLocales",[],o):"object"==typeof exports?exports.DecapCmsLocales=o():e.DecapCmsLocales=o()}(window,(()=>(()=>{"use strict";var e={};return e.DecapCmsLocales})()));
+//# sourceMappingURL=decap-cms-locales.js.map
