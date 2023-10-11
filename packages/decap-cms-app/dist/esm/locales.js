c371ff161cce3d761f65be7351f41d1c9fc7d64c
JohnJMills <john@digits.com>
2023-10-11 14:11:33 -0700

    build: add dist/ back to .gitignore

--- packages/decap-cms-app/dist/esm/locales.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _decapCmsCore = require("decap-cms-core");
-var _decapCmsLocales = require("decap-cms-locales");
-_decapCmsCore.DecapCmsCore.registerLocale('en', _decapCmsLocales.en);
--- packages/decap-cms-core/dist/esm/lib/consoleError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = consoleError;
-function consoleError(title, description) {
-  console.error(`%c ⛔ ${title}\n` + `%c${description}\n\n`, 'color: black; font-weight: bold; font-size: 16px; line-height: 50px;', 'color: black;');
-}
