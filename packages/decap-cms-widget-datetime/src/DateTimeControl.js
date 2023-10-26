988ea8dddd27491231662d821e0d4fb7b1964c89
Martin Jagodic <jagodicmartin1@gmail.com>
2023-10-26 09:39:45 +0200

    feat: import buttons from ui-4

--- packages/decap-cms-widget-datetime/src/DateTimeControl.js
+++ packages/decap-cms-widget-datetime/src/DateTimeControl.js
@@ -5,7 +5,7 @@ import { jsx, css } from '@emotion/react';
 import reactDateTimeStyles from 'react-datetime/css/react-datetime.css';
 import DateTime from 'react-datetime';
 import moment from 'moment';
-import { buttons } from 'decap-cms-ui-default';
+import { buttons } from 'decap-cms-ui-4';
 
 function NowButton({ t, handleChange }) {
   return (
