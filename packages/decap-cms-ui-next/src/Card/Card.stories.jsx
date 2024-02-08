ce405ab7cd724acdf89414b0bbaea5b9cd7e1aec
Daniel Mendes <contact@daniel-mendes.ch>
2024-02-08 17:04:56 +0100

    feat(storybook): add story for Card

---
+++ packages/decap-cms-ui-next/src/Card/Card.stories.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import Card from '.';
+
+const StyledCard = styled(Card)`
+  width: 20rem;
+  height: 20rem;
+`;
+
+export default {
+  title: 'Components/Card',
+  component: Card,
+  argTypes: {
+    elevation: {
+      control: 'select',
+      options: ['xs', 'sm', 'md', 'lg'],
+    },
+    direction: {
+      control: 'select',
+      options: ['up', 'down', 'left', 'right'],
+      table: {
+        defaultValue: { summary: 'down' },
+      },
+    },
+    rounded: {
+      control: 'select',
+      options: ['null', 'sm', 'md', 'lg'],
+      mapping: {
+        null: null,
+      },
+      table: {
+        defaultValue: { summary: 'lg' },
+      },
+    },
+  },
+  args: {
+    elevation: 'md',
+    direction: 'down',
+    rounded: 'null',
+  },
+};
+
+export function _Card(args) {
+  return <StyledCard {...args} />;
+}
