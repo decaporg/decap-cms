a855b001e1f5cbf1578d785cf3f09ea2aec0e5b1
Daniel Mendes <contact@daniel-mendes.ch>
2024-02-15 22:49:30 +0100

    refactor: refactored site_url nav section to use NavMenuLink component

--- packages/decap-cms-core/src/components/App/Nav.jsx
+++ packages/decap-cms-core/src/components/App/Nav.jsx
@@ -1,71 +1,7 @@
 import React, { useMemo } from 'react';
 import { translate } from 'react-polyglot';
 import { NavLink as ReactRouterNavLink, useLocation } from 'react-router-dom';
-import styled from '@emotion/styled';
-import color from 'color';
-import {
-  NavMenu,
-  NavMenuItem,
-  NavMenuGroup,
-  NavMenuGroupLabel,
-  ExternalLinkIcon,
-  Logo,
-} from 'decap-cms-ui-next';
-
-const StyledSiteLink = styled.a`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  height: 3rem;
-  padding: 0 12px;
-  cursor: pointer;
-`;
-
-const SiteContents = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  overflow: hidden;
-  color: ${({ theme, active }) =>
-    active ? theme.color.success['900'] : theme.color.mediumEmphasis};
-  background-color: ${({ theme, active }) =>
-    active ? color(theme.color.success['900']).alpha(0.1).string() : `transparent`};
-  border: none;
-  height: 2.5rem;
-  border-radius: 6px;
-  outline: none;
-  transition: 200ms;
-  ${StyledSiteLink}:hover & {
-    color: ${({ theme, active }) =>
-      active ? theme.color.success['900'] : theme.color.highEmphasis};
-    background-color: ${({ theme, active }) =>
-      active
-        ? color(theme.color.success['900']).alpha(0.1).string()
-        : color(theme.color.highEmphasis).alpha(0.05).string()};
-    ${({ active }) =>
-      active
-        ? `
-      cursor: default;
-    `
-        : ``}
-  }
-`;
-const StyledLogo = styled(Logo)`
-  margin: 0.375rem;
-`;
-const SiteName = styled.span`
-  margin-left: 0.75rem;
-  display: flex;
-  flex: 1;
-  overflow: hidden;
-  font-size: 0.875rem;
-  font-weight: 600;
-  opacity: ${({ collapsed }) => (collapsed ? '0' : '1')};
-  transition: opacity 200ms;
-`;
-const StyledExternalLinkIcon = styled(ExternalLinkIcon)`
-  margin-right: 0.5rem;
-`;
+import { NavMenu, NavMenuItem, NavMenuGroup, NavMenuGroupLabel, Logo } from 'decap-cms-ui-next';
 
 function Nav({
   collections,
@@ -106,13 +42,13 @@ function Nav({
 
   return (
     <NavMenu collapsable={true}>
-      <StyledSiteLink href={siteUrl} target="_blank">
-        <SiteContents>
-          <StyledLogo src={logoUrl} />
-          <SiteName>My Website</SiteName>
-          <StyledExternalLinkIcon />
-        </SiteContents>
-      </StyledSiteLink>
+      {/* <NavMenuItem href={siteUrl} target="_blank" icon={<Logo src={logoUrl} />}>
+        {siteName || displayUrl}
+      </NavMenuItem> */}
+
+      <NavMenuItem href={siteUrl} target="_blank" icon="decap">
+        My Website
+      </NavMenuItem>
 
       <NavMenuGroup>
         <NavMenuItem
