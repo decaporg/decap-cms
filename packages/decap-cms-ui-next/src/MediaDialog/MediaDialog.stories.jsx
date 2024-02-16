50ff20bf22beb674eb0ecb0fbba6a3b2810566f0
Daniel Mendes <contact@daniel-mendes.ch>
2024-02-16 11:17:36 +0100

    feat(storybook): added story for MediaDialog

---
+++ packages/decap-cms-ui-next/src/MediaDialog/MediaDialog.stories.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { useArgs } from '@storybook/preview-api';
+
+import MediaDialog from './';
+import { Button, ButtonGroup } from '../Buttons';
+
+export default {
+  title: 'Dialogs/MediaDialog',
+  component: MediaDialog,
+  args: {
+    open: false,
+    onClose: action('onClose'),
+  },
+};
+
+export function _MediaDialog(args) {
+  const [{ open }, updateArgs] = useArgs();
+
+  function toggleOpen() {
+    updateArgs({ open: !open });
+  }
+
+  return (
+    <>
+      <ButtonGroup>
+        <Button onClick={toggleOpen}>Open Dialog</Button>
+      </ButtonGroup>
+
+      <MediaDialog {...args} open={open} onClose={toggleOpen} />
+    </>
+  );
+}
