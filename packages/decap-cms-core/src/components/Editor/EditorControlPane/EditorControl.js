1480c772e0290512bc26819a0885fb9a96b2417f
Leonardo Matos <leomp120894@gmail.com>
2024-03-08 18:15:02 +0000

    chore: format

--- packages/decap-cms-core/src/components/Editor/EditorControlPane/EditorControl.js
+++ packages/decap-cms-core/src/components/Editor/EditorControlPane/EditorControl.js
@@ -102,7 +102,8 @@ function LabelComponent({ field, isActive, hasErrors, uniqueFieldId, isFieldOpti
   const label = `${field.get('label', field.get('name'))}`;
   const labelComponent = (
     <FieldLabel isActive={isActive} hasErrors={hasErrors} htmlFor={uniqueFieldId}>
-      {label}{isFieldOptional && <span>{` (${t('editor.editorControl.field.optional')})`}</span>}
+      {label}
+      {isFieldOptional && <span>{` (${t('editor.editorControl.field.optional')})`}</span>}
     </FieldLabel>
   );
 
