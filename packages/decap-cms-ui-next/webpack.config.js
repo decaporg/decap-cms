c1909a5a4d4247c41f29be64843a79a4d6c8ac86
Daniel Mendes <contact@daniel-mendes.ch>
2024-03-09 16:03:27 +0100

    refactor: cleanup webpack config

--- packages/decap-cms-ui-next/webpack.config.js
+++ packages/decap-cms-ui-next/webpack.config.js
@@ -1,23 +1,3 @@
 const { getConfig } = require('../../scripts/webpack.js');
 
-function configs() {
-  return getConfig().map(config => {
-    return {
-      ...config,
-      rules: [
-        ...config.rules,
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          type: 'asset/resource',
-          use: ['file-loader'],
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    };
-  });
-}
-
-module.exports = configs();
+module.exports = getConfig();
