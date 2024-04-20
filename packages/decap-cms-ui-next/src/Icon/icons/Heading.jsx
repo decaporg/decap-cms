085536afec8afc306b7f8dd592870e82820e40d5
Daniel Mendes <contact@daniel-mendes.ch>
2024-04-20 18:12:52 +0200

    chore: improve decap and heading icons

--- packages/decap-cms-ui-next/src/Icon/icons/Heading.jsx
+++ packages/decap-cms-ui-next/src/Icon/icons/Heading.jsx
@@ -9,9 +9,9 @@ const SVG = ({ className = '', size = '24' }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <path d="M6 12h12" />
     <path d="M6 20V4" />
