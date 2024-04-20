495e5ec2eb97611e3543a9ab40841e03594b8cdb
Daniel Mendes <contact@daniel-mendes.ch>
2024-04-20 18:10:38 +0200

    chore(style): switch collection view style

--- packages/decap-cms-core/src/components/Collection/ViewStyleControl.js
+++ packages/decap-cms-core/src/components/Collection/ViewStyleControl.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { IconButton } from 'decap-cms-ui-next';
+import { css } from '@emotion/react';
+import color from 'color';
+import { IconButton, ButtonGroup } from 'decap-cms-ui-next';
 
 import { VIEW_STYLE_LIST, VIEW_STYLE_GRID } from '../../constants/collectionViews';
 
-const ViewControlsSection = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  max-width: 500px;
-  gap: 4px;
+const ViewControlsSection = styled(ButtonGroup)`
+  ${({ theme }) => css`
+    background-color: ${color(theme.color.neutral['700']).alpha(0.2).string()};
+    border-radius: 8px;
+    margin: initial;
+  `}
 `;
 
 function ViewStyleControl({ viewStyle, onChangeViewStyle }) {
@@ -17,11 +19,13 @@ function ViewStyleControl({ viewStyle, onChangeViewStyle }) {
     <ViewControlsSection>
       <IconButton
         icon="menu"
+        size="sm"
         active={viewStyle === VIEW_STYLE_LIST}
         onClick={() => onChangeViewStyle(VIEW_STYLE_LIST)}
       />
       <IconButton
         icon="grid"
+        size="sm"
         active={viewStyle === VIEW_STYLE_GRID}
         onClick={() => onChangeViewStyle(VIEW_STYLE_GRID)}
       />
