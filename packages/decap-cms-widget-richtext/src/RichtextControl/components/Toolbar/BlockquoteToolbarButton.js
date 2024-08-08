08c14c8b32066dc6b62af1b07bd0ae69c6cc1256
Anze Demsar <anze.demsar@p-m.si>
2024-08-08 10:11:24 +0200

    feat(richtext): add blockquote toolbar button state

--- packages/decap-cms-widget-richtext/src/RichtextControl/components/Toolbar/BlockquoteToolbarButton.js
+++ packages/decap-cms-widget-richtext/src/RichtextControl/components/Toolbar/BlockquoteToolbarButton.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useEditorRef, focusEditor } from '@udecode/plate-common';
+import {
+  useEditorRef,
+  focusEditor,
+  useEditorSelector,
+  findNode,
+} from '@udecode/plate-common';
 import { unwrapList } from '@udecode/plate-list';
 import { toggleWrapNodes } from '@udecode/slate-utils';
 import { ELEMENT_BLOCKQUOTE } from '@udecode/plate-block-quote';
@@ -9,12 +14,14 @@ import ToolbarButton from './ToolbarButton';
 function BlockquoteToolbarButton(props) {
   const editor = useEditorRef();
 
+  const pressed = useEditorSelector(editor => !!findNode(editor, { match: { type: ELEMENT_BLOCKQUOTE } }), []);
+
   function handleClick() {
     unwrapList(editor);
     toggleWrapNodes(editor, ELEMENT_BLOCKQUOTE);
     focusEditor(editor);
   }
-  const pressed = false;
+
   return <ToolbarButton isActive={pressed} onClick={handleClick} {...props} />;
 }
 
