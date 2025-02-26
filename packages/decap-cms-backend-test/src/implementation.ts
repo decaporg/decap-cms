57fc57fb8ec60d6fd01e776106e3d575dbee787d
Felix Gnass <fgnass@gmail.com>
2025-02-26 10:03:18 +0100

    fix: repoFilesUnpublished is a record not an array

--- packages/decap-cms-backend-test/src/implementation.ts
+++ packages/decap-cms-backend-test/src/implementation.ts
@@ -22,7 +22,7 @@ import type {
   DataFile,
 } from 'decap-cms-lib-util';
 
-type RepoFile = { path: string; content: string | AssetProxy };
+type RepoFile = { path?: string; content?: string | AssetProxy };
 type RepoTree = { [key: string]: RepoFile | RepoTree };
 
 type Diff = {
@@ -50,7 +50,7 @@ declare global {
 }
 
 window.repoFiles = window.repoFiles || {};
-window.repoFilesUnpublished = window.repoFilesUnpublished || [];
+window.repoFilesUnpublished = window.repoFilesUnpublished || {};
 
 function getFile(path: string, tree: RepoTree) {
   const segments = path.split('/');
