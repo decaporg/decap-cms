72df72964294e2917963359a4392414a971177b1
Anze Demsar <demshy@gmail.com>
2025-03-14 14:42:46 +0100

    feat(index-file): remove logging

--- packages/decap-cms-core/src/components/Editor/EditorInterface.js
+++ packages/decap-cms-core/src/components/Editor/EditorInterface.js
@@ -160,15 +160,6 @@ function isPreviewEnabled(collection, entry) {
   }
 
   const indexFileConfig = collection.get('index_file');
-  console.log(
-    'isPreviewEnabled',
-    indexFileConfig,
-    entry.get('slug'),
-    indexFileConfig.get('pattern'),
-    !!collection.get('nested'),
-    indexFileConfig.get('editor')?.has('preview'),
-    indexFileConfig.get('editor').get('preview'),
-  );
   if (
     indexFileConfig &&
     isIndexFile(entry.get('slug'), indexFileConfig.get('pattern'), !!collection.get('nested')) &&
