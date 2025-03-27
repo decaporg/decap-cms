d1f784350821da57b37dde741efd37e60a2b8d91
hip3r <hip3r.master@gmail.com>
2025-03-27 11:49:52 +0100

    fix(markdown): extra whitespace before and after pasted content #7364

--- packages/decap-cms-widget-markdown/src/MarkdownControl/plugins/html/withHtml.js
+++ packages/decap-cms-widget-markdown/src/MarkdownControl/plugins/html/withHtml.js
@@ -37,7 +37,7 @@ const INLINE_STYLES = {
 
 function deserialize(el) {
   if (el.nodeType === 3) {
-    return el.textContent;
+    return el.textContent.trim();
   } else if (el.nodeType !== 1) {
     return null;
   } else if (el.nodeName === 'BR') {
