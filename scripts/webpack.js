a8307593c34fc84b7a47128f2e43c5561d08c0de
Martin Jagodic <jagodicmartin1@gmail.com>
2025-07-04 11:19:22 +0200

    chore: handle the polyfill mapping only in webpack config

--- scripts/webpack.js
+++ scripts/webpack.js
@@ -60,6 +60,7 @@ function plugins() {
     buffer: () =>
       new webpack.ProvidePlugin({
         Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser',
       }),
   };
 }
@@ -141,7 +142,9 @@ function baseConfig({ target = isProduction ? 'umd' : 'umddir' } = {}) {
     resolve: {
       extensions: ['.ts', '.tsx', '.js', '.json'],
       fallback: {
+        path: require.resolve('path-browserify'),
         stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer'),
       },
     },
     plugins: Object.values(plugins()).map(plugin => plugin()),
