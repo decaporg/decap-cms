98268d5ee6e4e79627fba25851985a859ca08482
Martin Jagodic <jagodicmartin1@gmail.com>
2025-07-04 10:01:26 +0200

    chore: revert for ci test 2

--- packages/decap-cms-core/src/formats/toml.ts
+++ packages/decap-cms-core/src/formats/toml.ts
@@ -1,5 +1,4 @@
-// import parseToml from '@iarna/toml/parse-string';
-import toml from '@iarna/toml';
+import parseToml from '@iarna/toml/parse-string';
 import tomlify from 'tomlify-j0.4';
 import dayjs from 'dayjs';
 
@@ -25,8 +24,7 @@ function outputReplacer(_key: string, value: unknown) {
 
 export default {
   fromFile(content: string) {
-    // return parseToml(content);
-    return toml.parse(content);
+    return parseToml(content);
   },
 
   toFile(data: object, sortedKeys: string[] = []) {
