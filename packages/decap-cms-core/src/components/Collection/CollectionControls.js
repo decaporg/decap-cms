3c2bc2a07cc0887fe6be10350d4374273a3efdff
Chris Swithinbank <swithinbank@gmail.com>
2026-05-23 18:00:33 +0200

    feat: snugger collection controls on small screens and let them wrap

--- packages/decap-cms-core/src/components/Collection/CollectionControls.js
+++ packages/decap-cms-core/src/components/Collection/CollectionControls.js
@@ -9,14 +9,16 @@ import GroupControl from './GroupControl';
 
 const CollectionControlsContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   flex-direction: row-reverse;
+  gap: 6px 0;
   margin-top: 22px;
   width: ${lengths.topCardWidth};
   max-width: 100%;
 
-  & > div {
-    margin-left: 6px;
+  @media (min-width: 500px) {
+    gap: 6px;
   }
 `;
 
