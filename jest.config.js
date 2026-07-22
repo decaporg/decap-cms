448b818f664cbfb4307de90d32b8980d03f28ff1
Mart Hesselink <21331676+marthesselink@users.noreply.github.com>
2026-07-22 17:28:33 +0200

    fix(richtext): preserve block images in list items

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setupTestFramework.js'],
   moduleNameMapper: {
+    '^platejs/react$': '<rootDir>/node_modules/platejs/dist/react/index.js',
+    '^(@platejs/.+)/react$': '<rootDir>/node_modules/$1/dist/react/index.js',
     'decap-cms-lib-auth': '<rootDir>/packages/decap-cms-lib-auth/src/index.js',
     'decap-cms-lib-util': '<rootDir>/packages/decap-cms-lib-util/src/index.ts',
     'decap-cms-ui-default': '<rootDir>/packages/decap-cms-ui-default/src/index.js',
@@ -13,7 +15,7 @@ module.exports = {
   modulePathIgnorePatterns: ['.nx', 'dist'],
   snapshotSerializers: ['@emotion/jest/serializer'],
   transformIgnorePatterns: [
-    'node_modules/(?!copy-text-to-clipboard|clean-stack|escape-string-regexp)',
+    'node_modules/(?!.*(?:copy-text-to-clipboard|clean-stack|escape-string-regexp|jotai-optics|nanoid))',
   ],
   testEnvironment: 'jsdom',
   testEnvironmentOptions: {
