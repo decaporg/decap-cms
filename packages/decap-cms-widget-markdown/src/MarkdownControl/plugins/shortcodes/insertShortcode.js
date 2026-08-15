a0d015a84d669d469ef0ff310aa070b80a501bba
Mmao <mmaoteacher@gmail.com>
2026-08-15 09:42:11 +0800

    feat(widget-markdown): support selection capture and async onInsert lifecycle for inline shortcodes

--- packages/decap-cms-widget-markdown/src/MarkdownControl/plugins/shortcodes/insertShortcode.js
+++ packages/decap-cms-widget-markdown/src/MarkdownControl/plugins/shortcodes/insertShortcode.js
@@ -1,12 +1,58 @@
-import { Transforms } from 'slate';
+import { Editor, Range, Transforms } from 'slate';
 
 import isCursorInEmptyParagraph from './locations/isCursorInEmptyParagraph';
 
-function insertShortcode(editor, pluginConfig) {
+async function insertShortcode(editor, pluginConfig, cmsContext = {}) {
+  if (pluginConfig.type === 'inline') {
+    let selectedText = '';
+    if (editor.selection && Range.isRange(editor.selection)) {
+      selectedText = Editor.string(editor, editor.selection);
+    }
+
+    let shortcodeData = {};
+
+    if (typeof pluginConfig.onInsert === 'function') {
+      try {
+        const result = await pluginConfig.onInsert({ selectedText, cmsContext });
+        if (result === null || result === undefined) {
+          return;
+        }
+        shortcodeData = result;
+      } catch (err) {
+        console.error(`Error in onInsert for inline component '${pluginConfig.id}':`, err);
+        return;
+      }
+    } else if (pluginConfig.fields) {
+      const defaultValues = pluginConfig.fields
+        .toMap()
+        .mapKeys((_, field) => field.get('name'))
+        .map(field => field.get('default', ''));
+      shortcodeData = defaultValues.toJS();
+    }
+
+    const nodeData = {
+      type: 'inline-shortcode',
+      id: pluginConfig.id,
+      data: {
+        shortcode: pluginConfig.id,
+        shortcodeNew: true,
+        shortcodeData,
+        isVoid: pluginConfig.isVoid !== false,
+      },
+      children: [{ text: '' }],
+    };
+
+    Transforms.insertNodes(editor, nodeData);
+    return;
+  }
+
   const defaultValues = pluginConfig.fields
-    .toMap()
-    .mapKeys((_, field) => field.get('name'))
-    .map(field => field.get('default', ''));
+    ? pluginConfig.fields
+        .toMap()
+        .mapKeys((_, field) => field.get('name'))
+        .map(field => field.get('default', ''))
+        .toJS()
+    : {};
 
   const nodeData = {
     type: 'shortcode',
@@ -14,7 +60,7 @@ function insertShortcode(editor, pluginConfig) {
     data: {
       shortcode: pluginConfig.id,
       shortcodeNew: true,
-      shortcodeData: defaultValues.toJS(),
+      shortcodeData: defaultValues,
     },
     children: [{ text: '' }],
   };
