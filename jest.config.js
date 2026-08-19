09940309af57b9409882205a4fb35b18335e4834
Yan <61414485+yanthomasdev@users.noreply.github.com>
2026-08-19 14:24:35 -0300

    fix: jest resolver config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setupTestFramework.js'],
+  resolver: '<rootDir>/jest.resolver.js',
   moduleNameMapper: {
-    '^platejs/react$': '<rootDir>/node_modules/platejs/dist/react/index.js',
-    '^(@platejs/.+)/react$': '<rootDir>/node_modules/$1/dist/react/index.js',
     'decap-cms-lib-auth': '<rootDir>/packages/decap-cms-lib-auth/src/index.js',
     'decap-cms-lib-util': '<rootDir>/packages/decap-cms-lib-util/src/index.ts',
     'decap-cms-ui-default': '<rootDir>/packages/decap-cms-ui-default/src/index.js',
