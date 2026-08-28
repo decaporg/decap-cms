c592c23face28a39a334b518b902cc4c495beef1
Sem Postma <sem@esstudio.nl>
2026-08-28 18:59:42 +0300

    chore: formatted files

--- packages/decap-cms-widget-markdown/src/MarkdownControl/components/Shortcode.js
+++ packages/decap-cms-widget-markdown/src/MarkdownControl/components/Shortcode.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useCallback, useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { css } from '@emotion/react';
 import { fromJS } from 'immutable';
 import omit from 'lodash/omit';
